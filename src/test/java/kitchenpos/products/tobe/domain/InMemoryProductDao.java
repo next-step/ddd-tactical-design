6696516e2b2a345744a3d6d2df38bffe7c7abc21
james <hency.yeo@gmail.com>
2020-03-08 14:56:13 +0900

    feat - ProductService 테스트를 위한 가짜 Dao - id autogeneration 문제로 가짜 DB의 키는 Product name으로 입력함

--- src/test/java/kitchenpos/products/tobe/domain/InMemoryProductDao.java
+++ src/test/java/kitchenpos/products/tobe/domain/InMemoryProductDao.java
@@ -1,16 +1,23 @@
 package kitchenpos.products.tobe.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class InMenoryProductDao implements ProductDao {
+public class InMemoryProductDao implements ProductDao {
+    private final Map<String, Product> entities = new HashMap<>();
 
     @Override
     public Product save(Product product) {
-        return null;
+        entities.put(product.getName(), product);
+        return product;
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return new ArrayList<>(entities.values());
     }
+
+
 }
