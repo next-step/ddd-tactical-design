9d9c8ad109217074571c10d4faeb66babc08f7d3
james <hency.yeo@gmail.com>
2020-03-08 11:53:32 +0900

    feat - Price 객체 생성

--- src/main/java/kitchenpos/products/tobe/domain/Price.java
+++ src/main/java/kitchenpos/products/tobe/domain/Price.java
@@ -1,4 +1,15 @@
 package kitchenpos.products.tobe.domain;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Price {
+    private BigDecimal price;
+
+    public Price(final BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0)
+            throw new IllegalArgumentException();
+
+        this.price = price;
+    }
 }
