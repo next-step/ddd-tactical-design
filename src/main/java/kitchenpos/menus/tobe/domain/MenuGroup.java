7a4fc06ec2a290b3ca913e87d80b0836581b8a72
liquidjoo <liquidjoo@gmail.com>
2020-03-11 22:25:10 +0900

    feat(menu) : menu group 객체 비교

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuGroup.java
@@ -0,0 +1,34 @@
+package kitchenpos.menus.tobe.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "menu_group")
+public class MenuGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public MenuGroup(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuGroup menuGroup = (MenuGroup) o;
+        return Objects.equals(name, menuGroup.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuGroupTest.java
@@ -0,0 +1,23 @@
+package kitchenpos.menus.tobe.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MenuGroupTest {
+
+    @Test
+    @DisplayName("메뉴 그룹 객체 비교")
+    void compareToMenuGroup() {
+        // give
+        MenuGroup expectedObject = new MenuGroup("치킨");
+        MenuGroup actualObject = new MenuGroup("치킨");
+        // when
+        boolean same = actualObject.equals(expectedObject);
+        // then
+        assertThat(same).isTrue();
+    }
+}
