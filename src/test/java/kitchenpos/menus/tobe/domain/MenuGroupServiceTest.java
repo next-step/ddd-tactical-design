c8b1aabca0a1be8bd9e529bc7a6855ff82d5cc81
liquidjoo <liquidjoo@gmail.com>
2020-03-11 23:11:10 +0900

    feat(menu) : 메뉴그룹서비스 -> 조회

--- src/test/java/kitchenpos/menus/tobe/domain/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuGroupServiceTest.java
@@ -9,6 +9,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
@@ -23,21 +26,42 @@ class MenuGroupServiceTest {
     private MenuGroupService menuGroupService;
 
     private MenuGroup expectedMenuGroup;
+    private List<MenuGroup> expectedMenuGroups;
 
     @BeforeEach
     void setUp() {
         expectedMenuGroup = new MenuGroup("추천 메뉴");
+        expectedMenuGroups = Arrays.asList(
+                new MenuGroup("두마리메뉴"),
+                new MenuGroup("한마리메뉴"),
+                new MenuGroup("순살파닭두마리메뉴"),
+                new MenuGroup("신메뉴")
+        );
     }
 
     @Test
     @DisplayName("객체 생성 테스트")
     void create() {
-        // given
+        // give
         given(menuGroupRepository.save(new MenuGroup("메뉴 그룹")))
                 .willReturn(expectedMenuGroup);
         MenuGroup actualMenuGroup = menuGroupService.create(new MenuGroup("메뉴 그룹"));
         // when
         boolean same = actualMenuGroup.equals(expectedMenuGroup);
+        // then
+        assertThat(same).isTrue();
+    }
+
+    @Test
+    @DisplayName("객체 조회")
+    void list() {
+        // give
+        given(menuGroupRepository.findAll())
+                .willReturn(expectedMenuGroups);
+
+        List<MenuGroup> actualMenuGroups = menuGroupService.list();
+        // when
+        boolean same = actualMenuGroups.containsAll(expectedMenuGroups);
         // then
         assertThat(same).isTrue();
     }
