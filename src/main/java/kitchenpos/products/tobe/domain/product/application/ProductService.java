b6a7afba0feb32f2fd5c90234a6a4a36c325d402
isisrin <isisrin@nate.com>
2020-03-12 16:41:18 +0900

    refactor (using JPA) : 로직 수정

--- src/main/java/kitchenpos/products/tobe/domain/product/application/ProductService.java
+++ src/main/java/kitchenpos/products/tobe/domain/product/application/ProductService.java
@@ -1,13 +1,11 @@
 package kitchenpos.products.tobe.domain.product.application;
 
 import kitchenpos.products.tobe.domain.product.domain.Product;
-import kitchenpos.products.tobe.domain.product.domain.ProductDomain;
 import kitchenpos.products.tobe.domain.product.repository.ProductRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class ProductService {
@@ -19,14 +17,10 @@ public class ProductService {
 
     @Transactional
     public Product create(final Product product) {
-        ProductDomain result = productRepository.save(product.toDomain());
-        return result.toProduct();
+        return productRepository.save(product);
     }
 
     public List<Product> list() {
-        return productRepository.findAll()
-                .stream()
-                .map(ProductDomain::toProduct)
-                .collect(Collectors.toList());
+        return productRepository.findAll();
     }
 }
