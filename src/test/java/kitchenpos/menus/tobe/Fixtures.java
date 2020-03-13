eb1907e98a94b8906c52bf7dd5b7d22a39888957
Hyukchan Kwon <kwonssy02@gmail.com>
2020-03-13 22:22:56 +0900

    feat(MenuGroup): domain 개발

---
+++ src/test/java/kitchenpos/menus/tobe/Fixtures.java
@@ -0,0 +1,37 @@
+package kitchenpos.menus.tobe;
+
+import kitchenpos.menus.tobe.menuGroup.domain.MenuGroup;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public class Fixtures {
+    public static final Long TWO_FRIED_CHICKENS_ID = 1L;
+    public static final Long TWO_CHICKENS_ID = 1L;
+    public static final Long FRIED_CHICKEN_ID = 1L;
+
+//    public static Menu twoFriedChickens() {
+//        final Menu menu = new Menu();
+//        menu.setId(TWO_FRIED_CHICKENS_ID);
+//        menu.setName("후라이드+후라이드");
+//        menu.setPrice(BigDecimal.valueOf(19_000L));
+//        menu.setMenuGroupId(TWO_CHICKENS_ID);
+//        menu.setMenuProducts(Arrays.asList(menuProduct()));
+//        return menu;
+//    }
+
+    public static MenuGroup twoChickens() {
+        final MenuGroup menuGroup = new MenuGroup("두마리메뉴");
+        ReflectionTestUtils.setField(menuGroup, "id", TWO_CHICKENS_ID);
+        return menuGroup;
+    }
+
+//    private static MenuProduct menuProduct() {
+//        final MenuProduct menuProduct = new MenuProduct();
+//        menuProduct.setMenuId(TWO_FRIED_CHICKENS_ID);
+//        menuProduct.setProductId(FRIED_CHICKEN_ID);
+//        menuProduct.setQuantity(2);
+//        return menuProduct;
+//    }
+}
--- src/test/java/kitchenpos/menus/tobe/menuGroup/domain/MenuGroupTest.java
+++ src/test/java/kitchenpos/menus/tobe/menuGroup/domain/MenuGroupTest.java
@@ -2,8 +2,12 @@ package kitchenpos.menus.tobe.menuGroup.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuGroupTest {
 
@@ -18,5 +22,18 @@ class MenuGroupTest {
 
         // then
         assertThat(menuGroup.getName()).isEqualTo(name);
+    }
+
+    @DisplayName("메뉴그룹 생성 시, 메뉴그룹명을 입력해야한다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {""})
+    void createFailsWhenNameIsBlank(String name) {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new MenuGroup(name);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
