ede9d5c64899c28f1d0b55c38f4e5f460d216bb6
Hyeonah <hyeonah@wantedlab.com>
2020-03-16 00:28:18 +0900

    feat(ProductPrice): 금액 validate 추가

--- src/main/java/kitchenpos/products/tobe/domain/ProductPrice.java
+++ src/main/java/kitchenpos/products/tobe/domain/ProductPrice.java
@@ -4,6 +4,7 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Embeddable
 @Access(AccessType.FIELD)
@@ -11,16 +12,24 @@ public class ProductPrice {
 
     private final BigDecimal price;
 
-
     public ProductPrice() {
         price = null;
     }
 
     public ProductPrice(BigDecimal price) {
+        validate(price);
         this.price = price;
+    }
+
+    private void validate(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public BigDecimal getPrice() {
         return price;
     }
+
+
 }
