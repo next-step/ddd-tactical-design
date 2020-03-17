42321b827d86d289076655bcb6faf551225ca714
isisrin <isisrin@nate.com>
2020-03-17 23:50:50 +0900

    refactor (remove price) : price 삭제

--- src/main/resources/db/migration/V1__Initialize_project_tables.sql
+++ src/main/resources/db/migration/V1__Initialize_project_tables.sql
@@ -33,7 +33,6 @@ CREATE TABLE menu_product (
     menu_id BIGINT(20) NOT NULL,
     product_id BIGINT(20) NOT NULL,
     quantity BIGINT(20) NOT NULL,
-    price DECIMAL(19, 2),
     PRIMARY KEY (seq)
 );
 
