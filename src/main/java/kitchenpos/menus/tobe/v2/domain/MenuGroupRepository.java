5d67f011c126b754c91c0f534534df453831cd52
dongchul <sah3122@naver.com>
2020-03-22 18:28:25 +0900

    refactor(Kitchenpos): remove V2

--- src/main/java/kitchenpos/menus/tobe/v2/domain/MenuGroupRepository.java
+++
@@ -1,6 +0,0 @@
-package kitchenpos.menus.tobe.v2.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuGroupRepository extends JpaRepository<MenuGroup, Long> {
-}
--- src/main/java/kitchenpos/menus/tobe/v2/domain/MenuProductRepository.java
+++
@@ -1,6 +0,0 @@
-package kitchenpos.menus.tobe.v2.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuProductRepository extends JpaRepository<MenuProduct, Long> {
-}
