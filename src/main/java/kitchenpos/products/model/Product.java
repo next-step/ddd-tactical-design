46146999280c2f0ab7b73c43ea386272f196a30d
하재권 <hazxz@naver.com>
2020-05-22 17:33:08 +0900

    refactoring(product): product domain 리팩토링을 위한 ProductRepository 추가

--- src/main/java/kitchenpos/products/model/Product.java
+++ src/main/java/kitchenpos/products/model/Product.java
@@ -1,8 +1,14 @@
 package kitchenpos.products.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
     private Long id;
     private String name;
     private BigDecimal price;
