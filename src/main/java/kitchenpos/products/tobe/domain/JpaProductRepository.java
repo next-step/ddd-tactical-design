9a24dde6585b881a0c7f87d531277c1f97844017
jonny-cho <andante2183@gmail.com>
2021-08-30 23:40:12 +0900

    fix(ProductRepository): tobe.domain의 객체를 사용하도록 변경

--- src/main/java/kitchenpos/products/tobe/domain/JpaProductRepository.java
+++ src/main/java/kitchenpos/products/tobe/domain/JpaProductRepository.java
@@ -1,8 +1,6 @@
 package kitchenpos.products.tobe.domain;
 
 import java.util.UUID;
-import kitchenpos.products.domain.Product;
-import kitchenpos.products.domain.ProductRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
--- src/main/java/kitchenpos/products/tobe/domain/ProductRepository.java
+++ src/main/java/kitchenpos/products/tobe/domain/ProductRepository.java
@@ -3,9 +3,9 @@ package kitchenpos.products.tobe.domain;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import kitchenpos.products.domain.Product;
 
 public interface ProductRepository {
+
     Product save(Product product);
 
     Optional<Product> findById(UUID id);
