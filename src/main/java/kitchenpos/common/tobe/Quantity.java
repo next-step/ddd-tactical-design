882025857bbae472e565ef8dab3e7c486fe838fa
skid901 <tlrudejr1993@gmail.com>
2021-09-05 05:03:01 +0900

    feat(kitchenpos/common): create quantity value object

---
+++ src/main/java/kitchenpos/common/tobe/Quantity.java
@@ -0,0 +1,21 @@
+package kitchenpos.common.tobe;
+
+public class Quantity {
+
+    private final long quantity;
+
+    public Quantity(final long quantity) {
+        validate(quantity);
+        this.quantity = quantity;
+    }
+
+    private void validate(final long quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("수량 값이 필수고, 수량은 0 미만의 수량 값을 가질 수 없습니다");
+        }
+    }
+
+    public long value() {
+        return quantity;
+    }
+}
