731066f7a7252a5dbbea7a213b9c6a354cc8eaad
JaeHan-Kim <hgal8877@gmail.com>
2021-09-04 10:22:09 +0900

    test:  리펙토링한 상품에 대한 테스트 코드

---
+++ src/test/java/kitchenpos/products/tobe/application/ProductServiceTest.java
@@ -0,0 +1,99 @@
+package kitchenpos.products.tobe.application;
+
+import kitchenpos.menus.application.InMemoryMenuRepository;
+import kitchenpos.menus.domain.MenuRepository;
+import kitchenpos.products.application.FakePurgomalumClient;
+import kitchenpos.products.infra.PurgomalumClient;
+import kitchenpos.products.tobe.domain.Product;
+import kitchenpos.products.tobe.domain.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ProductServiceTest {
+    private ProductRepository productRepository;
+    private MenuRepository menuRepository;
+    private PurgomalumClient purgomalumClient;
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productRepository = new InMemoryProductRepository();
+        menuRepository = new InMemoryMenuRepository();
+        purgomalumClient = new FakePurgomalumClient();
+        productService = new ProductService(productRepository, menuRepository, purgomalumClient);
+    }
+
+    @DisplayName("상품을 등록할 수 있다.")
+    @Test
+    void create() {
+        final Product expected = new Product("후라이드", 16000L);
+        final Product actual = productService.create(expected);
+        assertThat(actual).isNotNull();
+        assertAll(
+            () -> assertThat(actual.getId()).isNotNull(),
+            () -> assertThat(actual.getName()).isEqualTo(expected.getName()),
+            () -> assertThat(actual.getPrice()).isEqualTo(expected.getPrice())
+        );
+    }
+
+    @DisplayName("상품의 가격이 올바르지 않으면 등록할 수 없다.")
+    @ValueSource(strings = "-1000")
+    @NullSource
+    @ParameterizedTest
+    void create(final BigDecimal price) {
+        final Product expected = new Product("후라이드", price);
+        assertThatThrownBy(() -> productService.create(expected))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품의 이름이 올바르지 않으면 등록할 수 없다.")
+    @ValueSource(strings = {"비속어", "욕설이 포함된 이름"})
+    @NullSource
+    @ParameterizedTest
+    void create(final String name) {
+        final Product expected = new Product(name, 16_000L);
+        assertThatThrownBy(() -> productService.create(expected))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품의 가격을 변경할 수 있다.")
+    @Test
+    void changePrice() {
+        final UUID productId = productRepository.save(new Product("후라이드", 16_000L)).getId();
+        final Product expected = new Product(BigDecimal.valueOf(15_000L));
+        final Product actual = productService.changePrice(productId, expected);
+        assertThat(actual.getPrice()).isEqualTo(expected.getPrice());
+    }
+
+    @DisplayName("상품의 가격이 올바르지 않으면 변경할 수 없다.")
+    @ValueSource(strings = "-1000")
+    @NullSource
+    @ParameterizedTest
+    void changePrice(final BigDecimal price) {
+        final UUID productId = productRepository.save(new Product("후라이드", 16_000L)).getId();
+        final Product expected = new Product(price);
+        assertThatThrownBy(() -> productService.changePrice(productId, expected))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품의 목록을 조회할 수 있다.")
+    @Test
+    void findAll() {
+        productRepository.save(new Product("후라이드", 16_000L));
+        productRepository.save(new Product("양념치킨", 16_000L));
+        final List<Product> actual = productService.findAll();
+        assertThat(actual).hasSize(2);
+    }
+}
