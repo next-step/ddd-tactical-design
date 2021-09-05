988d7b8c26dc0acdc3a017d1eceb5fa6fff59d88
skid901 <tlrudejr1993@gmail.com>
2021-09-06 03:41:57 +0900

    feat(kitchenpos/menus): create menu entity

---
+++ src/main/java/kitchenpos/menus/tobe/domain/Menu.java
@@ -0,0 +1,88 @@
+package kitchenpos.menus.tobe.domain;
+
+import kitchenpos.common.tobe.DisplayedName;
+import kitchenpos.common.tobe.Price;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Menu {
+
+    private static final String PRICE_AMOUNT_EXCEPTION_MESSAGE = "가격은 금액보다 적거나 같아야 합니다";
+
+    private final UUID id;
+
+    private final DisplayedName name;
+
+    private Price price;
+
+    private final MenuProducts menuProducts;
+
+    private final UUID menuGroupId;
+
+    private boolean displayed;
+
+    public Menu(final UUID id,
+                final DisplayedName name,
+                final Price price,
+                final MenuProducts menuProducts,
+                final UUID menuGroupId,
+                final boolean displayed) {
+        if (displayed && isPriceGreaterThanAmount(price, menuProducts)) {
+            throw new IllegalArgumentException(PRICE_AMOUNT_EXCEPTION_MESSAGE);
+        }
+
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.menuProducts = menuProducts;
+        this.menuGroupId = menuGroupId;
+        this.displayed = displayed;
+    }
+
+    private boolean isPriceGreaterThanAmount(final Price price, final MenuProducts menuProducts) {
+        final BigDecimal menuPrice = price.value();
+        final BigDecimal amount = menuProducts.getAmount();
+        return menuPrice.compareTo(amount) > 0;
+    }
+
+    public void changePrice(final Price price) {
+        if (displayed && isPriceGreaterThanAmount(price, menuProducts)) {
+            throw new IllegalArgumentException(PRICE_AMOUNT_EXCEPTION_MESSAGE);
+        }
+
+        this.price = price;
+    }
+
+    public void display() {
+        if (isPriceGreaterThanAmount(price, menuProducts)) {
+            throw new IllegalStateException(PRICE_AMOUNT_EXCEPTION_MESSAGE);
+        }
+
+        displayed = true;
+    }
+
+    public void hide() {
+        displayed = false;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.value();
+    }
+
+    public BigDecimal getPrice() {
+        return price.value();
+    }
+
+    public boolean isDisplayed() {
+        return displayed;
+    }
+
+    public UUID getMenuGroupId() {
+        return menuGroupId;
+    }
+}
