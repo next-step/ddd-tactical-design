8ddfb7c33301181cc504c84e4a9a4904731df76a
skid901 <tlrudejr1993@gmail.com>
2021-09-06 00:30:25 +0900

    test(kitchenpos/menus): verify product entity

--- src/test/java/kitchenpos/menus/tobe/domain/Fixtures.java
+++ src/test/java/kitchenpos/menus/tobe/domain/Fixtures.java
@@ -1,7 +1,9 @@
 package kitchenpos.menus.tobe.domain;
 
 import kitchenpos.common.tobe.DisplayedName;
+import kitchenpos.common.tobe.Price;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class Fixtures {
@@ -16,5 +18,19 @@ public class Fixtures {
 
     public static MenuGroup MENU_GROUP_WITH_NAME(final String name) {
         return MENU_GROUP_WITH_ALL(UUID.randomUUID(), name);
+    }public static Product PRODUCT_WITH_ALL(final UUID id, final BigDecimal price) {
+        return new Product(id, new Price(price));
+    }
+
+    public static Product DEFAULT_PRODUCT() {
+        return PRODUCT_WITH_ALL(UUID.randomUUID(), BigDecimal.valueOf(16_000L));
+    }
+
+    public static Product PRODUCT_WITH_ID(final UUID id) {
+        return PRODUCT_WITH_ALL(id, BigDecimal.valueOf(16_000L));
+    }
+
+    public static Product PRODUCT_WITH_PRICE(final BigDecimal price) {
+        return PRODUCT_WITH_ALL(UUID.randomUUID(), price);
     }
 }
