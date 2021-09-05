2c9bab4cbdeb4aa15a0d42bf68fdb6cd3898032b
skid901 <tlrudejr1993@gmail.com>
2021-09-06 03:54:53 +0900

    test(kitchenpos/menus): verify validation logic in menu entity

--- src/test/java/kitchenpos/menus/tobe/domain/MenuProductTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuProductTest.java
@@ -1,6 +1,7 @@
 package kitchenpos.menus.tobe.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,8 +10,16 @@ import java.math.BigDecimal;
 import static kitchenpos.menus.tobe.domain.Fixtures.DEFAULT_PRODUCT;
 import static kitchenpos.menus.tobe.domain.Fixtures.MENU_PRODUCT_WITH_ALL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuProductTest {
+
+    @DisplayName("메뉴 상품에는 상품이 필수다.")
+    @Test
+    void MenuProduct() {
+        assertThatThrownBy(() -> MENU_PRODUCT_WITH_ALL(null, 0)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품은 필수입니다");
+    }
 
     @DisplayName("메뉴 상품은 금액을 반환한다.")
     @ValueSource(longs = {0, 1, 2})
