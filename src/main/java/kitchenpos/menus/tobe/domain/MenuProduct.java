f76b236268b78424c169c826422ad6a64c3ebfaf
skid901 <tlrudejr1993@gmail.com>
2021-09-06 03:54:17 +0900

    refactor(kitchenpos/menus): add validation logic in menu entity

--- src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
@@ -3,16 +3,24 @@ package kitchenpos.menus.tobe.domain;
 import kitchenpos.common.tobe.Quantity;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class MenuProduct {
 
     private final Product product;
 
-    private  final Quantity quantity;
+    private final Quantity quantity;
 
     public MenuProduct(final Product product, final Quantity quantity) {
+        validate(product);
         this.product = product;
         this.quantity = quantity;
+    }
+
+    private void validate(final Product product) {
+        if (Objects.isNull(product)) {
+            throw new IllegalArgumentException("상품은 필수입니다");
+        }
     }
 
     public BigDecimal getAmount() {
