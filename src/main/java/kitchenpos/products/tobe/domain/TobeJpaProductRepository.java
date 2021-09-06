ef714a9a04a340b3d1b4c1b8141d3806a69c22db
apdkeb <apdkeb@naver.com>
2021-09-06 18:12:19 +0900

    feat : TobeJpaProductRepository 생성
    2021. 09. 06.
    - 레거시를 수정하기 위한 TobeJpaProductRepository 생성
    
    Resolver : loop-study
    See Also : .

---
+++ src/main/java/kitchenpos/products/tobe/domain/TobeJpaProductRepository.java
@@ -0,0 +1,8 @@
+package kitchenpos.products.tobe.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TobeJpaProductRepository extends TobeProductRepository, JpaRepository<TobeProduct, UUID> {
+}
