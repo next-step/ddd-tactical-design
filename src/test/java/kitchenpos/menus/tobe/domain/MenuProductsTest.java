66a113b41d4a1d3fc5e00cb3f31505946d70181c
skid901 <tlrudejr1993@gmail.com>
2021-09-08 02:22:44 +0900

    test(kitchenpos/menus): verify validation logic in menu products first-class collection

--- src/test/java/kitchenpos/menus/tobe/domain/MenuProductsTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuProductsTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.menus.tobe.domain;
 
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +9,18 @@ import java.math.BigDecimal;
 import static kitchenpos.menus.tobe.domain.Fixtures.*;
 import static kitchenpos.menus.tobe.domain.Fixtures.MENU_PRODUCT_WITH_ALL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuProductsTest {
+
+    @DisplayName("메뉴 상품 목록은 1 개 이상의 메뉴 상품을 가져야 한다.")
+    @Test
+    void createMenuProducts() {
+        final ThrowableAssert.ThrowingCallable when = Fixtures::MENU_PRODUCTS_WITH_MENU_PRODUCT;
+
+        assertThatThrownBy(when).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1 개 이상의 메뉴 상품을 가져야 합니다");
+    }
 
     @DisplayName("메뉴 상품 목록은 금액을 반환한다.")
     @Test
