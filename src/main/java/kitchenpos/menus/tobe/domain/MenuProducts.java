38483fc8bedbec1e08112dbd833e65bb5f31c675
skid901 <tlrudejr1993@gmail.com>
2021-09-08 02:22:33 +0900

    refactor(kitchenpos/menus): add validation logic in menu products first-class collection

--- src/main/java/kitchenpos/menus/tobe/domain/MenuProducts.java
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProducts.java
@@ -8,7 +8,14 @@ public class MenuProducts {
     private final List<MenuProduct> menuProducts;
 
     public MenuProducts(final List<MenuProduct> menuProducts) {
+        validate(menuProducts);
         this.menuProducts = menuProducts;
+    }
+
+    private void validate(final List<MenuProduct> menuProducts) {
+        if (menuProducts.isEmpty()) {
+            throw new IllegalArgumentException("1 개 이상의 메뉴 상품을 가져야 합니다");
+        }
     }
 
     public BigDecimal getAmount() {
