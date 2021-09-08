392eaf03aa015e8727e5ffd68d7bad5f658895cd
skid901 <tlrudejr1993@gmail.com>
2021-09-08 22:38:47 +0900

    refactor(kitchenpos/menus): remake menu product entity to replace product entity

--- src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
@@ -1,30 +1,42 @@
 package kitchenpos.menus.tobe.domain;
 
+import kitchenpos.commons.tobe.domain.Price;
 import kitchenpos.commons.tobe.domain.Quantity;
 
 import java.math.BigDecimal;
 import java.util.Objects;
+import java.util.UUID;
 
 public class MenuProduct {
 
-    private final Product product;
+    private final UUID menuId;
+
+    private final UUID productId;
+
+    private final Price price;
 
     private final Quantity quantity;
 
-    public MenuProduct(final Product product, final Quantity quantity) {
-        validate(product);
-        this.product = product;
+    public MenuProduct(final UUID menuId, final UUID productId, final Price price, final Quantity quantity) {
+        validate(menuId, productId);
+
+        this.menuId = menuId;
+        this.productId = productId;
+        this.price = price;
         this.quantity = quantity;
     }
 
-    private void validate(final Product product) {
-        if (Objects.isNull(product)) {
-            throw new IllegalArgumentException("상품은 필수입니다");
+    private void validate(final UUID menuId, final UUID productId) {
+        if (Objects.isNull(menuId)) {
+            throw new IllegalArgumentException("메뉴 식별자는 필수입니다");
+        }
+        if (Objects.isNull(productId)) {
+            throw new IllegalArgumentException("상품 식별자는 필수입니다");
         }
     }
 
     public BigDecimal getAmount() {
-        final BigDecimal price = product.getPrice();
+        final BigDecimal price = this.price.value();
         final BigDecimal quantity = BigDecimal.valueOf(this.quantity.value());
         return price.multiply(quantity);
     }
