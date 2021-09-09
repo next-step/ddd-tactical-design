1fdf0933e229c03816eda2269500414394c2ad73
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-09-09 19:35:37 +0900

    refactor: 상품 가격 Value Object 분리

---
+++ src/main/java/kitchenpos/products/tobe/domain/Price.java
@@ -0,0 +1,52 @@
+package kitchenpos.products.tobe.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Price {
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal value;
+
+    protected Price() {
+    }
+
+    Price(BigDecimal value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final BigDecimal value) {
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException("상품의 가격이 올바르지 않으면 등록할 수 없습니다.");
+        }
+
+        if (value.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("상품의 가격은 0원 이상이어야 합니다.");
+        }
+    }
+
+    public BigDecimal getValue() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Price price = (Price) o;
+        return Objects.equals(value, price.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
