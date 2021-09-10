e3915307ba197cb65cb5c0c4def7543744b17298
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-09-10 09:10:11 +0900

    test: Product Fixture 추가

--- src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
+++ src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
@@ -5,25 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+import kitchenpos.ToBeFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ProductTest {
 
-    private static final Profanities profanities = new FakeProfanities();
-
     @DisplayName("상품을 생성할 수 있다.")
     @Test
     void 생성() {
         assertDoesNotThrow(
-            () -> createProduct("치킨", 16_000L)
+            () -> ToBeFixtures.product("치킨", 16_000L)
         );
     }
 
     @DisplayName("상품의 가격을 변경할 수 있다.")
     @Test
     void 가격변경() {
-        final Product product = createProduct("치킨", 16_000L);
+        final Product product = ToBeFixtures.product();
         final Price newPrice = new Price(BigDecimal.valueOf(20_000L));
 
         product.changePrice(newPrice);
@@ -36,21 +35,9 @@ class ProductTest {
     void 동등성() {
         final UUID id = UUID.randomUUID();
 
-        final Product product1 = createProduct(id, "치킨", 16_000L);
-        final Product product2 = createProduct(id, "치킨", 16_000L);
+        final Product product1 = ToBeFixtures.product(id, "치킨", 16_000L);
+        final Product product2 = ToBeFixtures.product(id, "치킨", 16_000L);
 
         assertThat(product1).isEqualTo(product2);
-    }
-
-    private Product createProduct(UUID id, final String displayedName, final Long price) {
-        return new Product(
-            id,
-            new DisplayedName(displayedName, profanities),
-            new Price(BigDecimal.valueOf(price))
-        );
-    }
-
-    private Product createProduct(final String displayedName, final Long price) {
-        return createProduct(UUID.randomUUID(), displayedName, price);
     }
 }
