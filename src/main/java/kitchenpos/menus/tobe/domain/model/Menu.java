588912c4f017415e681884090b98be185ac66e1b
jonny-cho <andante2183@gmail.com>
2021-09-12 03:00:18 +0900

    refactor(step2): Menu 추가

---
+++ src/main/java/kitchenpos/menus/tobe/domain/model/Menu.java
@@ -0,0 +1,56 @@
+package kitchenpos.menus.tobe.domain.model;
+
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import kitchenpos.common.tobe.domain.DisplayedName;
+import kitchenpos.common.tobe.domain.Price;
+
+@Table(name = "menu")
+@Entity
+public class Menu {
+
+    @Column(name = "id", columnDefinition = "varbinary(16)")
+    @Id
+    private UUID id;
+
+    @Embedded
+    @Column(name = "name", nullable = false)
+    private DisplayedName name;
+
+    @Embedded
+    @Column(name = "price", nullable = false)
+    private Price price;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(
+        name = "menu_group_id",
+        columnDefinition = "varbinary(16)",
+        foreignKey = @ForeignKey(name = "fk_menu_to_menu_group")
+    )
+    private MenuGroup menuGroup;
+
+    @Column(name = "displayed", nullable = false)
+    private boolean displayed;
+
+    @Embedded
+    private MenuProducts menuProducts;
+
+    protected Menu() {
+    }
+
+    public Menu(final DisplayedName name, final Price price, final MenuGroup menuGroup, final boolean displayed, final MenuProducts menuProducts) {
+        this.name = name;
+        this.price = price;
+        this.menuGroup = menuGroup;
+        this.displayed = displayed;
+        this.menuProducts = menuProducts;
+    }
+
+}
