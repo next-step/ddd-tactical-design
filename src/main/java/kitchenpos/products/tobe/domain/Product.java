5f6bb2e5cd8fe7a416c1b1e25668f30dbf582d80
JaeHan-Kim <hgal8877@gmail.com>
2021-09-12 22:21:06 +0900

    refactor: 상품 도메인 모델 수정

--- src/main/java/kitchenpos/products/tobe/domain/Product.java
+++ src/main/java/kitchenpos/products/tobe/domain/Product.java
@@ -2,6 +2,7 @@ package kitchenpos.products.tobe.domain;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 @Table(name = "product")
@@ -19,30 +20,18 @@ public class Product {
 
     protected Product() {}
 
-    private Product(ProductPrice price) {
+    public Product(final ProductPrice price) {
         this.price = price;
     }
 
-    private Product(UUID id, ProductName name, ProductPrice price) {
+    public Product(final UUID id, final ProductName name, final ProductPrice price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
 
-    public Product(final BigDecimal price) {
-        this(new ProductPrice(price));
-    }
-
-    public Product(final UUID id, final String name, final BigDecimal price) {
-        this(id, new ProductName(name), new ProductPrice(price));
-    }
-
-    public Product(final String name, final long price) {
-        this(UUID.randomUUID(), new ProductName(name), new ProductPrice(BigDecimal.valueOf(price)));
-    }
-
-    public Product(final String name, final BigDecimal price) {
-        this(UUID.randomUUID(), new ProductName(name), new ProductPrice(price));
+    public Product(final ProductName name, final ProductPrice price) {
+        this(UUID.randomUUID(), name, price);
     }
 
     public UUID getId() {
@@ -57,7 +46,20 @@ public class Product {
         return price.getPrice();
     }
 
-    public void changePrice(BigDecimal price) {
-        this.price = new ProductPrice(price);
+    public void changePrice(final ProductPrice price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/kitchenpos/products/tobe/domain/ProductName.java
+++ src/main/java/kitchenpos/products/tobe/domain/ProductName.java
@@ -1,21 +1,40 @@
 package kitchenpos.products.tobe.domain;
 
+import kitchenpos.products.infra.PurgomalumClient;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class ProductName {
 
     @Column(name = "name", nullable = false)
-    private String name;
+    private String value;
 
     protected ProductName() {}
 
-    public ProductName(String name) {
-        this.name = name;
+    public ProductName(final String value, final PurgomalumClient profanities) {
+        if (Objects.isNull(value) || profanities.containsProfanity(value)) {
+            throw new IllegalArgumentException("이름에는 욕설이 포함될 수 없습니다.");
+        }
+        this.value = value;
     }
 
     public String getName() {
-        return name;
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductName that = (ProductName) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
