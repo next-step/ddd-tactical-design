af434a52de94d2b88b79a54aafa08e153e29d840
skid901 <tlrudejr1993@gmail.com>
2021-09-14 00:40:58 +0900

    test(kitchenpos/menus): verify constructor of menu product entity

--- src/test/java/kitchenpos/menus/tobe/domain/model/MenuProductTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/model/MenuProductTest.java
@@ -1,15 +1,34 @@
 package kitchenpos.menus.tobe.domain.model;
 
+import kitchenpos.commons.tobe.domain.model.Price;
+import kitchenpos.commons.tobe.domain.model.Quantity;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import static kitchenpos.menus.tobe.domain.fixture.MenuProductFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class MenuProductTest {
+
+    @DisplayName("메뉴 상품을 생성한다.")
+    @Test
+    void createMenuProduct() {
+        final Price price = new Price(BigDecimal.valueOf(16_000L));
+        final Quantity quantity = new Quantity(1L);
+
+        final MenuProduct menuProduct = new MenuProduct(UUID.randomUUID(),UUID.randomUUID(), price, quantity);
+
+        assertAll(
+                () -> assertThat(menuProduct.getId()).isNotNull(),
+                () -> assertThat(menuProduct.getProductId()).isNotNull()
+        );
+    }
 
     @DisplayName("메뉴 상품은 금액을 반환한다.")
     @ValueSource(longs = {0, 1, 2})
