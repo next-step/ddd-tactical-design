21f439b49b56171310cc506055d393d6991b11f6
skid901 <tlrudejr1993@gmail.com>
2021-09-14 01:15:53 +0900

    test(kitchenpos/menus): verify constructor of menu entity

--- src/test/java/kitchenpos/menus/tobe/domain/model/MenuTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/model/MenuTest.java
@@ -1,14 +1,53 @@
 package kitchenpos.menus.tobe.domain.model;
 
+import kitchenpos.commons.tobe.domain.model.DisplayedName;
+import kitchenpos.commons.tobe.domain.model.Price;
+import kitchenpos.commons.tobe.domain.model.Quantity;
+import kitchenpos.commons.tobe.domain.service.Validator;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.UUID;
+
 import static kitchenpos.menus.tobe.domain.fixture.MenuFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class MenuTest {
+
+    @DisplayName("메뉴를 생성한다.")
+    @Test
+    void createMenu() {
+        final DisplayedName displayedName = new DisplayedName("후라이드치킨");
+        final Price price = new Price(BigDecimal.valueOf(16_000L));
+        final MenuProduct menuProduct = new MenuProduct(UUID.randomUUID(), UUID.randomUUID(), price, new Quantity(1L));
+        final MenuProducts menuProducts = new MenuProducts(Collections.singletonList(menuProduct));
+        final Validator<Menu> validator = menu -> {
+        };
+
+        final Menu menu = new Menu(
+                UUID.randomUUID(),
+                displayedName,
+                price,
+                menuProducts,
+                UUID.randomUUID(),
+                true,
+                validator
+        );
+
+        assertAll(
+                () -> assertThat(menu.getId()).isNotNull(),
+                () -> assertThat(menu.getName()).isEqualTo(displayedName.value()),
+                () -> assertThat(menu.getPrice()).isEqualTo(price.value()),
+                () -> assertThat(menu.getProductIds()).containsSequence(menuProduct.getProductId()),
+                () -> assertThat(menu.getMenuGroupId()).isNotNull(),
+                () -> assertThat(menu.isDisplayed()).isTrue()
+        );
+    }
 
     @DisplayName("가격이 메뉴 상품들의 총 금액 보다 큰지 비교한다.")
     @Test
