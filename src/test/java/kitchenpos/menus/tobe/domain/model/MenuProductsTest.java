80c00ddc83794c7c3cec97547d8a417e204bf00d
skid901 <tlrudejr1993@gmail.com>
2021-09-14 00:48:48 +0900

    test(kitchenpos/menus): verify constructor of menu products first-class collection

--- src/test/java/kitchenpos/menus/tobe/domain/model/MenuProductsTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/model/MenuProductsTest.java
@@ -1,15 +1,31 @@
 package kitchenpos.menus.tobe.domain.model;
 
+import kitchenpos.commons.tobe.domain.model.Price;
+import kitchenpos.commons.tobe.domain.model.Quantity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.*;
 
 import static kitchenpos.menus.tobe.domain.fixture.MenuProductFixture.MENU_PRODUCT_WITH_PRICE_AND_QUANTITY;
 import static kitchenpos.menus.tobe.domain.fixture.MenuProductsFixture.MENU_PRODUCTS_WITH_MENU_PRODUCT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MenuProductsTest {
+
+    @DisplayName("메뉴 상품 목록을 생성한다.")
+    @Test
+    void createMenuProducts() {
+        final Price price = new Price(BigDecimal.valueOf(16_000L));
+        final Quantity quantity = new Quantity(1L);
+        final MenuProduct menuProduct1 = new MenuProduct(UUID.randomUUID(), UUID.randomUUID(), price, quantity);
+        final MenuProduct menuProduct2 = new MenuProduct(UUID.randomUUID(), UUID.randomUUID(), price, quantity);
+
+        final MenuProducts menuProducts = new MenuProducts(Arrays.asList(menuProduct1, menuProduct2));
+
+        assertThat(menuProducts.getProductIds()).containsSequence(menuProduct1.getProductId(), menuProduct2.getProductId());
+    }
 
     @DisplayName("메뉴 상품 목록은 메뉴 상품들의 총 금액을 반환한다.")
     @Test
