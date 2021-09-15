e94379cf921a54375f00d6676ee3bc547c13ce1d
apdkeb <apdkeb@naver.com>
2021-09-15 19:47:03 +0900

    Test : MenuGroupNameTest 생성
    2021. 09. 15.
    - 메뉴그룹명 테스트 생성
    
    Resolver : loop-study
    See Also : sihoony

---
+++ src/test/java/kitchenpos/menus/application/tobe/MenuGroupNameTest.java
@@ -0,0 +1,22 @@
+package kitchenpos.menus.application.tobe;
+
+import kitchenpos.menus.tobe.domain.TobeMenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MenuGroupNameTest {
+
+    @DisplayName("메뉴 그룹명 생성")
+    @Test
+    void create() {
+        MenuGroupName menuGroupName = new MenuGroupName("메뉴그룹");
+
+        assertThat(menuGroupName).isNotNull();
+    }
+}
+
+
+
+
