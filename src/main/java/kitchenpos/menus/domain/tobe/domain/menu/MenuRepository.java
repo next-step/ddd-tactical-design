799b5295c0ea1c021e1f6dc153197fa9ada2e73a
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-09-17 21:04:24 +0900

    refactor: (tobe) 메뉴 리포지토리 리팩터링

---
+++ src/main/java/kitchenpos/menus/domain/tobe/domain/menu/MenuRepository.java
@@ -0,0 +1,18 @@
+package kitchenpos.menus.domain.tobe.domain.menu;
+
+import java.util.List;
+import java.util.Optional;
+import kitchenpos.common.domain.MenuId;
+
+public interface MenuRepository {
+
+    Menu save(Menu menu);
+
+    Optional<Menu> findById(MenuId id);
+
+    List<Menu> findAll();
+
+    List<Menu> findAllByIdIn(List<MenuId> ids);
+
+    // List<Menu> findAllByProductId(ProductId productId);
+}
