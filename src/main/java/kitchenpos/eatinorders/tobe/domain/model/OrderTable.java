fdba0c1df913c42a6634f323cf91de33d5031839
skid901 <tlrudejr1993@gmail.com>
2021-09-20 02:21:55 +0900

    refactor(kitchenpos/eatinorders): implement entity, order table

--- src/main/java/kitchenpos/eatinorders/tobe/domain/model/OrderTable.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/model/OrderTable.java
@@ -1,36 +1,58 @@
 package kitchenpos.eatinorders.tobe.domain.model;
 
 import kitchenpos.commons.tobe.domain.model.DisplayedName;
+import kitchenpos.commons.tobe.domain.service.Validator;
 
 import java.util.UUID;
 
 public class OrderTable {
 
+    private final UUID id;
+
+    private final DisplayedName displayedName;
+
+    private NumberOfGuests numberOfGuests;
+
+    private boolean isEmpty;
+
     public OrderTable(final UUID id, final DisplayedName displayedName, final NumberOfGuests numberOfGuests) {
+        this.id = id;
+        this.displayedName = displayedName;
+        this.numberOfGuests = numberOfGuests;
+        this.isEmpty = true;
     }
 
     public void sit() {
+        isEmpty = false;
     }
 
-    public void clear() {
+    public void clear(final Validator<OrderTable> validator) {
+        validator.validate(this);
+
+        isEmpty = true;
     }
 
     public void changeNumberOfGuests(final NumberOfGuests numberOfGuests) {
+        if (isEmpty) {
+            throw new IllegalStateException("빈 테이블의 방문한 손님 수는 0에서 변경할 수 없습니다.");
+        }
+
+        this.numberOfGuests = numberOfGuests;
     }
 
     public UUID getId() {
-        return null;
+        return id;
     }
 
     public String getName() {
-        return null;
+        return displayedName.value();
     }
 
     public long getNumberOfGuests() {
-        return -1L;
+        return numberOfGuests.value();
     }
 
     public boolean isEmpty() {
-        return false;
+        return isEmpty;
     }
 }
--- src/test/java/kitchenpos/eatinorders/tobe/domain/model/OrderTableTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/model/OrderTableTest.java
@@ -1,15 +1,12 @@
 package kitchenpos.eatinorders.tobe.domain.model;
 
 import kitchenpos.commons.tobe.domain.model.DisplayedName;
-import kitchenpos.commons.tobe.domain.model.Price;
-import kitchenpos.eatinorders.tobe.domain.model.orderstatus.Waiting;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import javax.validation.constraints.PositiveOrZero;
-import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,7 +33,7 @@ class OrderTableTest {
 
     @DisplayName("주문 테이블은 빈 테이블이 해제된다.")
     @Test
-    void 빈_테이블_해제() {
+    void 빈_테이블_해제_성공() {
         final OrderTable orderTable = DEFAULT_ORDER_TABLE();
 
         orderTable.sit();
@@ -49,29 +46,18 @@ class OrderTableTest {
     void 빈_테이블_설정_성공() {
         final OrderTable orderTable = DEFAULT_ORDER_TABLE();
 
-        orderTable.clear();
+        orderTable.clear(dummy -> {});
 
         assertThat(orderTable.isEmpty()).isTrue();
     }
 
-    @DisplayName("주문 테이블의 주문 식별자 목록에 계산 완료되지 않은 주문이 있으면 IllegalStateException을 던진다.")
-    @Test
-    void 빈_테이블_설정_실패() {
-        final OrderTable orderTable = DEFAULT_ORDER_TABLE();
-        ORDER_WITH_ORDER_TABLE(orderTable);
-
-        final ThrowableAssert.ThrowingCallable when = orderTable::clear;
-
-        assertThatThrownBy(when).isInstanceOf(IllegalStateException.class)
-                .hasMessage("계산 완료되지 않은 주문이 있습니다.");
-    }
-
     @DisplayName("주문 테이블은 방문한 손님 수가 변경된다.")
-    @Test
-    @PositiveOrZero
+    @ParameterizedTest
+    @ValueSource(longs = {0L, 1L, 2L})
     void 방문한_손님_수_변경_성공(final long expected) {
         final OrderTable orderTable = DEFAULT_ORDER_TABLE();
         final NumberOfGuests numberOfGuests = new NumberOfGuests(expected);
+        orderTable.sit();
 
         orderTable.changeNumberOfGuests(numberOfGuests);
 
@@ -79,8 +65,8 @@ class OrderTableTest {
     }
 
     @DisplayName("주문 테이블이 빈 테이블일 경우, 방문한 손님 수를 변경하면 IllegalStateException을 던진다.")
-    @Test
-    @PositiveOrZero
+    @ParameterizedTest
+    @ValueSource(longs = {0L, 1L, 2L})
     void 방문한_손님_수_변경_실패(final long expected) {
         final OrderTable orderTable = DEFAULT_ORDER_TABLE();
         final NumberOfGuests numberOfGuests = new NumberOfGuests(expected);
@@ -93,25 +79,5 @@ class OrderTableTest {
 
     private OrderTable DEFAULT_ORDER_TABLE() {
         return new OrderTable(UUID.randomUUID(), new DisplayedName("1번 테이블"), new NumberOfGuests(0L));
-    }
-
-    private Order ORDER_WITH_ORDER_TABLE(final OrderTable orderTable) {
-        return new Order(
-                UUID.randomUUID(),
-                orderTable.getId(),
-                new Waiting(),
-                new OrderLineItems(
-                        Collections.singletonList(
-                                new OrderLineItem(
-                                        UUID.randomUUID(),
-                                        UUID.randomUUID(),
-                                        new Price(BigDecimal.valueOf(16_000L)),
-                                        1L
-                                )
-                        )
-                ),
-                order -> {
-                }
-        );
     }
 }
