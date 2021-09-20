761e149fdb016f6f8b081b33376dbdca5127f7b0
skid901 <tlrudejr1993@gmail.com>
2021-09-20 14:25:02 +0900

    refactor(kitchenpos/eatinorders): implement domain service, order table clear validator

--- src/main/java/kitchenpos/eatinorders/tobe/domain/service/OrderTableClearValidator.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/service/OrderTableClearValidator.java
@@ -2,14 +2,21 @@ package kitchenpos.eatinorders.tobe.domain.service;
 
 import kitchenpos.commons.tobe.domain.service.Validator;
 import kitchenpos.eatinorders.tobe.domain.model.OrderTable;
+import kitchenpos.eatinorders.tobe.domain.model.orderstatus.Completed;
 import kitchenpos.eatinorders.tobe.domain.repository.OrderRepository;
 
 public class OrderTableClearValidator implements Validator<OrderTable> {
 
+    private final OrderRepository orderRepository;
+
     public OrderTableClearValidator(final OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public void validate(final OrderTable orderTable) {
+        if (orderRepository.existsByOrderTableAndStatusNot(orderTable, new Completed())) {
+            throw new IllegalStateException("계산 완료되지 않은 주문이 있습니다.");
+        }
     }
 }
