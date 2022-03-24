5101205bb56090402e6cce7db9323d9ead8736fb
jun731 <96776513+jun731@users.noreply.github.com>
2022-03-24 19:46:36 +0900

    feat: 빅속어가 포함될 수 없는 상품이름 구현

---
+++ src/main/java/kitchenpos/products/tobe/domain/Name.java
@@ -0,0 +1,43 @@
+package kitchenpos.products.tobe.domain;
+
+import org.springframework.util.StringUtils;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Name {
+    private final String name;
+
+    protected Name() {
+        this("", text -> false);
+    }
+
+    public Name(final String name, Profanities profanities) {
+        verify(name, profanities);
+        this.name = name;
+    }
+
+    private void verify(final String name, Profanities profanities) {
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
+        }
+        if (profanities.contains(name)) {
+            throw new IllegalArgumentException("이름에 비속어는 포함될 수 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
---
+++ src/test/java/kitchenpos/products/tobe/infra/FakeProfanities.java
@@ -0,0 +1,20 @@
+package kitchenpos.products.tobe.infra;
+
+import kitchenpos.products.tobe.domain.Profanities;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FakeProfanities implements Profanities {
+
+    private static final List<String> profanities;
+
+    static {
+        profanities = Arrays.asList("욕설", "비속어");
+    }
+
+    @Override
+    public boolean contains(final String text) {
+        return profanities.stream().anyMatch(text::contains);
+    }
+}
