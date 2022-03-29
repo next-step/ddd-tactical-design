075c06fc27185a12ff28c464165381e43fc06b13
mynameisdaun <daun9870.jung@gmail.com>
2022-03-30 02:07:39 +0900

    <refactor>(TobeProduct) 빌더패턴 작성

--- src/main/java/kitchenpos/products/domain/tobe/domain/TobeProduct.java
+++ src/main/java/kitchenpos/products/domain/tobe/domain/TobeProduct.java
@@ -1,11 +1,15 @@
 package kitchenpos.products.domain.tobe.domain;
 
+import kitchenpos.products.domain.Product;
+import kitchenpos.products.domain.tobe.domain.policy.ProductNamingRule;
 import kitchenpos.products.domain.tobe.domain.policy.ProductPricingRule;
 import kitchenpos.products.domain.tobe.domain.vo.ProductId;
 import kitchenpos.products.domain.tobe.domain.vo.ProductName;
 import kitchenpos.products.domain.tobe.domain.vo.ProductPrice;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
 
 @Table(name = "product")
 @Entity
@@ -22,7 +26,43 @@ public class TobeProduct {
     @Embedded
     private ProductPrice price;
 
-    public TobeProduct() {
+    protected TobeProduct() {
+    }
+
+    private TobeProduct(ProductName name, ProductPrice price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    private TobeProduct(ProductId id, ProductName name, ProductPrice price) {
+        this(name, price);
+        this.id = id;
+    }
+
+    public static class ProductBuilder{
+        private ProductName name;
+        private ProductPrice price;
+
+        public ProductBuilder() {
+
+        }
+
+        public ProductBuilder name(String name, ProductNamingRule namingRule) {
+            this.name = new ProductName(name, namingRule);
+            return this;
+        }
+
+        public ProductBuilder price(BigDecimal price, ProductPricingRule pricingRule) {
+            this.price = new ProductPrice(price, pricingRule);
+            return this;
+        }
+
+        public TobeProduct build() {
+            if(Objects.isNull(name)||Objects.isNull(price)) {
+                throw new IllegalArgumentException();
+            }
+            return new TobeProduct(name, price);
+        }
     }
 
     public ProductPrice changePrice(ProductPrice productPrice, ProductPricingRule rule) {
