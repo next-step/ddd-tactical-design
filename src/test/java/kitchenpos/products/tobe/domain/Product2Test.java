47b160d4c51db69f4ade700dddfaeb13fbc33662
seul <eeseul.p@gmail.com>
2022-03-31 02:11:37 +0900

    test: product 테스트 코드 추가

---
+++ src/test/java/kitchenpos/products/tobe/domain/Product2Test.java
@@ -0,0 +1,72 @@
+package kitchenpos.products.tobe.domain;
+
+import kitchenpos.products.application.FakePurgomalumClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class Product2Test {
+
+    @DisplayName("상품을 생성할 수 있다.")
+    @Test
+    void constructor() {
+        assertDoesNotThrow(() -> product2(14000));
+    }
+
+    @DisplayName("상품의 가격은 0 보다 작을 수 없다.")
+    @Test
+    void negativePrice() {
+        assertThatThrownBy(() -> product2(-100))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품의 가격은 0원 이상이어야 합니다.");
+    }
+
+    @DisplayName("상품의 가격은 입력하지 않을 수 없다.")
+    @ParameterizedTest
+    @NullSource
+    void nullPrice(BigDecimal price) {
+        assertThatThrownBy(() -> new Product2("엽기떡볶이", price, new FakePurgomalumClient()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품의 가격은 0원 이상이어야 합니다.");
+    }
+
+    @DisplayName("상품의 이름에 금지어가 포함될 수 없다.")
+    @Test
+    void forbiddenName() {
+        assertThatThrownBy(() -> new Product2("비속어", BigDecimal.valueOf(0), new FakePurgomalumClient()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품의 이름으로 사용할 수 없습니다.");
+    }
+
+    @DisplayName("상품의 가격을 변경할 수 있다.")
+    @Test
+    void changePrice() {
+        BigDecimal newPrice = BigDecimal.valueOf(14000);
+        Product2 product2 = product2(10000);
+
+        product2.changePrice(newPrice);
+
+        assertThat(product2.getPrice()).isEqualTo(new ProductPrice(newPrice));
+    }
+
+    @DisplayName("상품의 가격은 0원 이하로 변경할 수 없다.")
+    @Test
+    void changeNegativePrice() {
+        Product2 product2 = product2(10000);
+
+        assertThatThrownBy(() -> product2.changePrice(BigDecimal.valueOf(-100)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품의 가격은 0원 이상이어야 합니다.");
+    }
+
+    private Product2 product2(Integer price) {
+        return new Product2("엽기떡볶이", BigDecimal.valueOf(price), new FakePurgomalumClient());
+    }
+}
