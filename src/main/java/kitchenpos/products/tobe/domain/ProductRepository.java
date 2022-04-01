08eb82fdfeabbc2790d9890e6bbf94fb1ebd6f55
hyunssooo <jhs695@gmail.com>
2022-04-01 23:36:04 +0900

    fix: ProductRepository 메서드 파라미터 ProductId로 변경

--- src/main/java/kitchenpos/products/tobe/domain/ProductRepository.java
+++ src/main/java/kitchenpos/products/tobe/domain/ProductRepository.java
@@ -1,15 +1,14 @@
 package kitchenpos.products.tobe.domain;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface ProductRepository {
 
     Product save(Product product);
 
-    Product findById(UUID id);
+    Product findById(ProductId id);
 
     List<Product> findAll();
 
-    List<Product> findAllByIdIn(List<UUID> ids);
+    List<Product> findAllByIdIn(List<ProductId> ids);
 }
