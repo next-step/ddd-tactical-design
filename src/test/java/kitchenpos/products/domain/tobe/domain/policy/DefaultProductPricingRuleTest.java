a95ba2f646875b74e57e7606585094acb492845a
mynameisdaun <daun9870.jung@gmail.com>
2022-04-03 16:57:38 +0900

    <test>(DefaultProductPricingRuleTest) 기본 상품가격 정책 테스트

---
+++ src/test/java/kitchenpos/products/domain/tobe/domain/policy/DefaultProductPricingRuleTest.java
@@ -0,0 +1,44 @@
+package kitchenpos.products.domain.tobe.domain.policy;
+
+import kitchenpos.products.exception.ProductPricingRuleViolationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DefaultProductPricingRuleTest {
+
+    private ProductPricingRule productPricingRule;
+
+    @BeforeEach
+    void setUp() {
+        productPricingRule = new DefaultProductPricingRule();
+    }
+
+    @DisplayName("상품가격은 0원 이상이어야 한다")
+    @ValueSource(strings = {"16000","0"})
+    @ParameterizedTest
+    void checkRule_success(final BigDecimal price) {
+        //given, when
+        boolean result = productPricingRule.checkRule(price);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("상품가격이 음수이거나, 빈 값일 경우 상품가격 정책 위반이다")
+    @ValueSource(strings = "-1000")
+    @NullSource
+    @ParameterizedTest
+    void checkRule_fail(final BigDecimal price) {
+        //given, when,then
+        assertThatThrownBy(() -> productPricingRule.checkRule(price))
+                .isInstanceOf(ProductPricingRuleViolationException.class);
+    }
+}
