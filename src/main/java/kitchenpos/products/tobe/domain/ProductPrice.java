313742b088be332d30d444a9b355d13ad3a6227b
dpudpu <dpudpu11@gmail.com>
2022-04-03 11:28:05 +0900

    refactor: 상품 도메인 리팩토링 (#74)

---
+++ src/main/java/kitchenpos/products/tobe/domain/ProductPrice.java
@@ -0,0 +1,37 @@
+package kitchenpos.products.tobe.domain;
+
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+public class ProductPrice {
+    private BigDecimal value;
+
+    protected ProductPrice() {
+    }
+
+    public ProductPrice(BigDecimal value) {
+        if (Objects.isNull(value) || value.intValue() < 0) {
+            throw new IllegalArgumentException("상품의 가격은 0원 이상이어야 합니다.");
+        }
+        this.value = value;
+    }
+
+    public ProductPrice(int value) {
+        this(BigDecimal.valueOf(value));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductPrice that = (ProductPrice) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
@@ -0,0 +1,49 @@
+package kitchenpos.products.tobe.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+class ProductTest {
+
+    @Test
+    @DisplayName("상품의 가격이 0원 이상이면 등록할 수 있다")
+    void constructor01() {
+        assertThatCode(() ->
+                new Product(UUID.randomUUID(), "김치", 0)
+        ).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("상품의 가격이 0원 미만이면 등록할 수 없다")
+    void constructor02() {
+        assertThatThrownBy(() ->
+                new Product(UUID.randomUUID(), "김치", -1)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("상품의 가격이 0원 이상이면 변경할 수 있다")
+    void price01() {
+        Product product = new Product(UUID.randomUUID(), "김치", 0);
+
+        product.changePrice(10);
+
+        assertThat(product.getPrice()).isEqualTo(new ProductPrice(10));
+    }
+
+    @Test
+    @DisplayName("상품의 가격이 0원 미만일 때는 변경할 수 없다")
+    void price02() {
+        Product product = new Product(UUID.randomUUID(), "김치", 0);
+
+        assertThatThrownBy(() ->
+                product.changePrice(-1)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
