4c27f0e874daf7b45bfca3fe05b6dfd5d3e3951a
mynameisdaun <daun9870.jung@gmail.com>
2022-04-03 12:28:17 +0900

    <test>(FakeProductNamingRule) 테스트용 상품이름 정책 생성

---
+++ src/test/java/kitchenpos/products/domain/tobe/policy/FakeProductNamingRule.java
@@ -0,0 +1,10 @@
+package kitchenpos.products.domain.tobe.policy;
+
+import kitchenpos.products.domain.tobe.domain.policy.ProductNamingRule;
+
+public class FakeProductNamingRule implements ProductNamingRule {
+    @Override
+    public void checkRule(String name) {
+
+    }
+}
