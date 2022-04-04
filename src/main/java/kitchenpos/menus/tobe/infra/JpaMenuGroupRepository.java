8105868e1f9cd720e9b9f03fb67834973b5850a1
강태엽 <ted.kang@woowahan.com>
2022-04-05 00:09:58 +0900

    refactor: apply code review -> move JpaMenuGroupRepository to infra package

--- src/main/java/kitchenpos/menus/tobe/infra/JpaMenuGroupRepository.java
+++ src/main/java/kitchenpos/menus/tobe/infra/JpaMenuGroupRepository.java
@@ -1,5 +1,7 @@
-package kitchenpos.menus.tobe.domain.menugroup;
+package kitchenpos.menus.tobe.infra;
 
+import kitchenpos.menus.tobe.domain.menugroup.MenuGroup;
+import kitchenpos.menus.tobe.domain.menugroup.MenuGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaMenuGroupRepository extends MenuGroupRepository, JpaRepository<MenuGroup, Long> {
