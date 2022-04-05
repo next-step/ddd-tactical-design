08a3d1f73b04cbc8b28803bd32a580e399dda844
mynameisdaun <daun9870.jung@gmail.com>
2022-04-06 02:08:26 +0900

    <refactor>(DefaultMenuPricingRule) 기본 메뉴 가격 정책 생성

---
+++ src/main/java/kitchenpos/menus/domain/tobe/domain/policy/DefaultMenuPricingRule.java
@@ -0,0 +1,34 @@
+package kitchenpos.menus.domain.tobe.domain.policy;
+
+import kitchenpos.common.exception.PricingRuleViolationException;
+import kitchenpos.common.policy.PricingRule;
+import kitchenpos.menus.domain.tobe.domain.TobeMenuProduct;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+public class DefaultMenuPricingRule implements PricingRule {
+    private final List<TobeMenuProduct> menuProducts;
+
+    public DefaultMenuPricingRule(List<TobeMenuProduct> menuProducts) {
+        if(Objects.isNull(menuProducts) || menuProducts.isEmpty()) {
+            throw new PricingRuleViolationException();
+        }
+        this.menuProducts = menuProducts;
+    }
+
+    @Override
+    public boolean checkRule(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0 || price.compareTo(getTotalAmount()) > 0) {
+            throw new PricingRuleViolationException();
+        }
+        return true;
+    }
+
+    private BigDecimal getTotalAmount() {
+        return menuProducts.stream()
+                .map(TobeMenuProduct::calculateAmount)
+                .reduce(BigDecimal.ZERO, (a,b) -> a.add(b));
+    }
+}
