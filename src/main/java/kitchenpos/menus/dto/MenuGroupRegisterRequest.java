d989169acae1c1847fb31ea23697d8dd9d350465
mynameisdaun <daun9870.jung@gmail.com>
2022-04-05 16:54:59 +0900

    <refactor>(MenuGroupRegisterRequest) 메뉴그룹 등록 요청 Dto 생성

---
+++ src/main/java/kitchenpos/menus/dto/MenuGroupRegisterRequest.java
@@ -0,0 +1,33 @@
+package kitchenpos.menus.dto;
+
+import kitchenpos.common.policy.NamingRule;
+
+public class MenuGroupRegisterRequest {
+    private String name;
+    private NamingRule namingRule;
+
+    public MenuGroupRegisterRequest(String name) {
+        this.name = name;
+    }
+
+    public MenuGroupRegisterRequest(String name, NamingRule namingRule) {
+        this.name = name;
+        this.namingRule = namingRule;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public NamingRule getNamingRule() {
+        return namingRule;
+    }
+
+    public void setNamingRule(NamingRule namingRule) {
+        this.namingRule = namingRule;
+    }
+}
