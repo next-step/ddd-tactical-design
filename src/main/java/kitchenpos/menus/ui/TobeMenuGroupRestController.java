398c1d9520b429f8d912895292a106a617b8dfce
mynameisdaun <daun9870.jung@gmail.com>
2022-04-05 16:57:00 +0900

    <refactor>(TobeMenuGroupRestController ) tobe메뉴그룹 controller 생성

---
+++ src/main/java/kitchenpos/menus/ui/TobeMenuGroupRestController.java
@@ -0,0 +1,43 @@
+package kitchenpos.menus.ui;
+
+import kitchenpos.menus.application.MenuGroupService;
+import kitchenpos.menus.application.TobeMenuGroupService;
+import kitchenpos.menus.domain.MenuGroup;
+import kitchenpos.menus.domain.tobe.domain.TobeMenuGroup;
+import kitchenpos.menus.dto.MenuGroupDto;
+import kitchenpos.menus.dto.MenuGroupRegisterRequest;
+import kitchenpos.menus.dto.MenuGroupRegisterResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequestMapping("/api/menu-groups/tobe")
+@RestController
+public class TobeMenuGroupRestController {
+    private final TobeMenuGroupService menuGroupService;
+
+    public TobeMenuGroupRestController(final TobeMenuGroupService menuGroupService) {
+        this.menuGroupService = menuGroupService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MenuGroupRegisterResponse> create(@RequestBody final MenuGroupRegisterRequest request) {
+        final TobeMenuGroup menuGroup = menuGroupService.create(request);
+        final MenuGroupRegisterResponse response = new MenuGroupRegisterResponse(menuGroup);
+        return ResponseEntity.created(URI.create("/api/menu-groups/" + response.getId()))
+            .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MenuGroupDto>> findAll() {
+        return ResponseEntity.ok(
+                menuGroupService.findAll()
+                        .stream()
+                        .map(MenuGroupDto::new)
+                        .collect(Collectors.toList())
+        );
+    }
+}
