aab33f3b579e604a95638da94ad7f6f34b30f6f2
mynameisdaun <daun9870.jung@gmail.com>
2022-04-05 16:54:22 +0900

    <refactor>(MenuGroupDto) 메뉴그룹 Dto 생성

---
+++ src/main/java/kitchenpos/menus/dto/MenuGroupDto.java
@@ -0,0 +1,38 @@
+package kitchenpos.menus.dto;
+
+import kitchenpos.menus.domain.tobe.domain.TobeMenuGroup;
+
+import java.util.UUID;
+
+public class MenuGroupDto {
+    private UUID id;
+    private String name;
+
+    public MenuGroupDto(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public MenuGroupDto(TobeMenuGroup menuGroup) {
+        this.id = menuGroup.getId().getValue();
+        this.name = menuGroup.getName().getValue();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
