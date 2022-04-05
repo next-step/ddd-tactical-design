1b13b4d4a715c1ab325c56be26b93abad5700d58
mynameisdaun <daun9870.jung@gmail.com>
2022-04-06 02:07:47 +0900

    <test>(DefaultMenuNamingRuleTest) 기본 메뉴명 정책 테스트 생성

---
+++ src/test/java/kitchenpos/menus/domain/tobe/domain/policy/DefaultMenuNamingRuleTest.java
@@ -0,0 +1,45 @@
+package kitchenpos.menus.domain.tobe.domain.policy;
+
+import kitchenpos.common.exception.NamingRuleViolationException;
+import kitchenpos.common.policy.NamingRule;
+import kitchenpos.products.application.FakePurgomalumClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DefaultMenuNamingRuleTest {
+
+    private NamingRule menuNamingRule;
+
+    @BeforeEach
+    void setUp() {
+        menuNamingRule = new DefaultMenuNamingRule(new FakePurgomalumClient());
+    }
+
+    @DisplayName("메뉴명은 빈 값이 아니고, 비속어가 포함되지 않아야 한다")
+    @Test
+    void checkRule_success() {
+        //given, when
+        boolean result = menuNamingRule.checkRule("후라이드치킨");
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("메뉴명이 빈 값이거나, 욕설 혹은 비속어가 포함될 경우 메뉴명 정책 위반이다")
+    @ValueSource(strings = {"욕설","비속어"})
+    @NullSource
+    @ParameterizedTest
+    void checkRule_fail(final String name) {
+        //given, when,then
+        assertThatThrownBy(() -> menuNamingRule.checkRule(name))
+                .isInstanceOf(NamingRuleViolationException.class);
+    }
+
+}
