414c3db71bc9984f2b849f9407859d32241a2102
mynameisdaun <daun9870.jung@gmail.com>
2022-04-05 10:05:56 +0900

    <refactor>(MenuProductSeq) 메뉴 구성상품 일련번호 VO 생성

---
+++ src/main/java/kitchenpos/menus/domain/tobe/domain/vo/MenuProductSeq.java
@@ -0,0 +1,38 @@
+package kitchenpos.menus.domain.tobe.domain.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+public class MenuProductSeq implements Serializable {
+    @Column(name = "seq")
+    private Long seq;
+
+    public MenuProductSeq(Long seq) {
+        this.seq = seq;
+    }
+
+    protected MenuProductSeq() {
+    }
+
+    public Long getValue() {
+        return seq;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MenuProductSeq productId = (MenuProductSeq) o;
+
+        return seq != null ? seq.equals(productId.seq) : productId.seq == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return seq != null ? seq.hashCode() : 0;
+    }
+}
