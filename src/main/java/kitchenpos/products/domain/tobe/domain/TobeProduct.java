39a9eaa3a6a32acee74abca3942da09c10f93c5a
mynameisdaun <daun9870.jung@gmail.com>
2022-04-05 09:52:08 +0900

    <refactor>(TobeProduct) java.lang.String -> String 불필요한 경로 삭제

--- src/main/java/kitchenpos/products/domain/tobe/domain/TobeProduct.java
+++ src/main/java/kitchenpos/products/domain/tobe/domain/TobeProduct.java
@@ -61,7 +61,7 @@ public class TobeProduct {
 
     public static class ProductBuilder {
         private ProductId productId;
-        private java.lang.String name;
+        private String name;
         private ProductNamingRule namingRule;
         private BigDecimal price;
         private ProductPricingRule pricingRule;
@@ -70,7 +70,7 @@ public class TobeProduct {
 
         }
 
-        public ProductBuilder name(java.lang.String name) {
+        public ProductBuilder name(String name) {
             this.name=name;
             return this;
         }
