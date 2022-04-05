9f422cfa3739807f19dfae9b287375f010a4969d
mynameisdaun <daun9870.jung@gmail.com>
2022-04-05 16:55:17 +0900

    <refactor>(MenuGroupRegisterResponse) 메뉴그룹 등록 응답 Dto 생성

---
+++ src/main/java/kitchenpos/menus/dto/MenuGroupRegisterResponse.java
@@ -0,0 +1,53 @@
+package kitchenpos.menus.dto;
+
+import kitchenpos.menus.domain.tobe.domain.TobeMenuGroup;
+
+import java.util.UUID;
+
+public class MenuGroupRegisterResponse {
+    private UUID id;
+    private String name;
+
+    public MenuGroupRegisterResponse(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public MenuGroupRegisterResponse(TobeMenuGroup menuGroup) {
+        this(menuGroup.getId().getValue(), menuGroup.getName().getValue());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MenuGroupRegisterResponse that = (MenuGroupRegisterResponse) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
