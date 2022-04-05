9aa80cdfdfce180242fd89ebcc4fd3abf59e7554
mynameisdaun <daun9870.jung@gmail.com>
2022-04-05 15:53:22 +0900

    <refactor>(TobeMenuGroup) Tobe메뉴그룹 생성

--- src/main/java/kitchenpos/menus/domain/tobe/domain/TobeMenuGroup.java
+++ src/main/java/kitchenpos/menus/domain/tobe/domain/TobeMenuGroup.java
@@ -1,12 +1,17 @@
 package kitchenpos.menus.domain.tobe.domain;
 
+import kitchenpos.common.policy.NamingRule;
 import kitchenpos.menus.domain.tobe.domain.vo.MenuGroupId;
 import kitchenpos.menus.domain.tobe.domain.vo.MenuGroupName;
+import kitchenpos.menus.exception.MenuGroupNamingRuleViolationException;
+import kitchenpos.products.exception.ProductNamingRuleViolationException;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Objects;
+import java.util.UUID;
 
 @Table(name = "menu_group")
 @Entity
@@ -20,6 +25,11 @@ public class TobeMenuGroup {
 
     protected TobeMenuGroup() { }
 
+    private TobeMenuGroup(MenuGroupId id, MenuGroupName name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public MenuGroupId getId() {
         return id;
     }
@@ -31,15 +41,28 @@ public class TobeMenuGroup {
     public static class MenuGroupBuilder {
         private MenuGroupId menuGroupId;
         private String name;
-
+        private NamingRule namingRule;
 
         public MenuGroupBuilder() {
+            this.menuGroupId = new MenuGroupId(UUID.randomUUID());
+        }
 
+        public MenuGroupBuilder name(String name) {
+            this.name=name;
+            return this;
         }
 
         public MenuGroupBuilder menuGroupName(String name) {
             this.name = name;
             return this;
+        }
+
+        public TobeMenuGroup build() {
+            if (Objects.isNull(name) || Objects.isNull(namingRule)) {
+                throw new MenuGroupNamingRuleViolationException();
+            }
+            namingRule.checkRule(name);
+            return new TobeMenuGroup(this.menuGroupId, new  MenuGroupName(name));
         }
     }
 
