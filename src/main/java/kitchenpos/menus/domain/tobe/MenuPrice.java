340012be7c547e2b99dbacb97c62aed016a806fe
etff <wish2rich@gmail.com>
2022-04-06 22:13:02 +0900

    feat: 메뉴 가격을 생성하라

---
+++ src/main/java/kitchenpos/menus/domain/tobe/MenuPrice.java
@@ -0,0 +1,67 @@
+package kitchenpos.menus.domain.tobe;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+public class MenuPrice {
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    protected MenuPrice() {
+    }
+
+    public MenuPrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.price = price;
+    }
+
+    public static MenuPrice valueOf(int value) {
+        return new MenuPrice(BigDecimal.valueOf(value));
+    }
+
+    public static MenuPrice valueOf(BigDecimal value) {
+        return new MenuPrice(value);
+    }
+
+    public MenuPrice add(MenuPrice value) {
+        return add(value);
+    }
+
+    public MenuPrice add(BigDecimal value) {
+        return new MenuPrice(price.add(value));
+    }
+
+    public MenuPrice multiply(MenuPrice value) {
+        return multiply(value.price);
+    }
+
+    public MenuPrice multiply(BigDecimal value) {
+        return new MenuPrice(price.multiply(value));
+    }
+
+    public boolean isBiggerPrice(BigDecimal value) {
+        return price.compareTo(value) > 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MenuPrice)) {
+            return false;
+        }
+        MenuPrice menuPrice = (MenuPrice) o;
+        return Objects.equals(price, menuPrice.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
+}
