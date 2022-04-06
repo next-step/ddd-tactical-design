3507a4304ff1ec69db05001848b0396604ae870c
chongmyung.jang <chongmyung.jang@linecorp.com>
2022-04-07 00:46:05 +0900

    chore(PurgomalumClient): Changes to the package name

--- src/main/java/kitchenpos/products/adapter/out/web/DefaultPurgomalumClient.java
+++ src/main/java/kitchenpos/products/adapter/out/web/DefaultPurgomalumClient.java
@@ -1,4 +1,4 @@
-package kitchenpos.products.adapter.out.client;
+package kitchenpos.products.adapter.out.web;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -14,15 +14,15 @@ public class DefaultPurgomalumClient implements PurgomalumClient {
     private final RestTemplate restTemplate;
 
     public DefaultPurgomalumClient(final RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+        restTemplate = restTemplateBuilder.build();
     }
 
     @Override
     public boolean containsProfanity(final String text) {
         final URI url = UriComponentsBuilder.fromUriString("https://www.purgomalum.com/service/containsprofanity")
-            .queryParam("text", text)
-            .build()
-            .toUri();
+                                            .queryParam("text", text)
+                                            .build()
+                                            .toUri();
         return Boolean.parseBoolean(restTemplate.getForObject(url, String.class));
     }
 }
