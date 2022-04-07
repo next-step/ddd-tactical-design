1225c4bcc1b17622194c206611c1b96b9a8062a1
etff <wish2rich@gmail.com>
2022-04-07 21:57:54 +0900

    refactor: 수량 객체를 생성하라

---
+++ src/main/java/kitchenpos/menus/domain/tobe/MenuQuantity.java
@@ -0,0 +1,37 @@
+package kitchenpos.menus.domain.tobe;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class MenuQuantity {
+    private static final String NEGATIVE_NUMBER_NOT_ALLOWED = "수량이 음수일 수 없습니다.";
+    private Long quantity;
+
+    protected MenuQuantity() {
+    }
+
+    public MenuQuantity(long quantity) {
+        validate(quantity);
+        this.quantity = quantity;
+    }
+
+    private void validate(Long quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_NOT_ALLOWED);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MenuQuantity)) return false;
+        MenuQuantity that = (MenuQuantity) o;
+        return Objects.equals(quantity, that.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(quantity);
+    }
+}
