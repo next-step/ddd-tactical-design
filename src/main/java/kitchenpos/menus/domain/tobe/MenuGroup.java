5f23bbf3c04550c38048f64cac7e69d3a10b8479
etff <wish2rich@gmail.com>
2022-04-07 21:22:04 +0900

    refactor: 메뉴 그룹을 새로 생성하라

---
+++ src/main/java/kitchenpos/menus/domain/tobe/MenuGroup.java
@@ -0,0 +1,48 @@
+package kitchenpos.menus.domain.tobe;
+
+import kitchenpos.products.domain.tobe.BanWordFilter;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Id;
+import java.util.Objects;
+import java.util.UUID;
+
+public class MenuGroup {
+
+    @Column(name = "id", columnDefinition = "varbinary(16)")
+    @Id
+    private UUID id;
+
+    @Embedded
+    private MenuName name;
+
+    protected MenuGroup() {
+    }
+
+    public MenuGroup(String name, BanWordFilter banWordFilter) {
+        this(UUID.randomUUID(), name, banWordFilter);
+    }
+
+    public MenuGroup(UUID id, String name, BanWordFilter banWordFilter) {
+        this.id = id;
+        this.name = new MenuName(name, banWordFilter);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MenuGroup)) {
+            return false;
+        }
+        MenuGroup menuGroup = (MenuGroup) o;
+        return Objects.equals(id, menuGroup.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
