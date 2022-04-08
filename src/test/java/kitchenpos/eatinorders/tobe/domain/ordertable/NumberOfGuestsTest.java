1b44ecdebf9fbad84d432970181043ce1371a0fd
강태엽 <ted.kang@woowahan.com>
2022-04-09 02:26:40 +0900

    feat: add NumberOfGuests

---
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/ordertable/NumberOfGuestsTest.java
@@ -0,0 +1,34 @@
+package kitchenpos.eatinorders.tobe.domain.ordertable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NumberOfGuestsTest {
+
+  @DisplayName("방문한 손님 수는 0명 이상이어야 한다.")
+  @Test
+  void emptyNameTest() {
+    //given
+    int given = -1;
+
+    //when & then
+    assertThatThrownBy(() -> new NumberOfGuests(given))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("방문한 손님 수를 비교할 수 있다.")
+  @Test
+  void equalsTest() {
+    //given
+    int given = 4;
+    NumberOfGuests name = new NumberOfGuests(given);
+    NumberOfGuests theOther = new NumberOfGuests(given);
+
+    //when & then
+    assertThat(name.equals(theOther)).isTrue();
+  }
+
+}
