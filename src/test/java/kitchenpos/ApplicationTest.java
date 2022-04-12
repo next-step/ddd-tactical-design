941344936c37eb8a5924d817dfc95a33fedc6335
mynameisdaun <daun9870.jung@gmail.com>
2022-04-13 00:38:10 +0900

    <test>(ApplicationTest) SpringBootTest 비활성화

--- src/test/java/kitchenpos/ApplicationTest.java
+++ src/test/java/kitchenpos/ApplicationTest.java
@@ -3,7 +3,7 @@ package kitchenpos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ApplicationTest {
     @Test
     void contextLoads() {
