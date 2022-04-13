a5350834850ee582f8d9493c36bdf1d3ca3b3391
Imaspear <rjsckdd12@gmail.com>
2022-04-13 15:30:26 +0900

    refact : update tab size
    2 spaces -> 4 spaces

--- src/main/java/kitchenpos/support/domain/Value.java
+++ src/main/java/kitchenpos/support/domain/Value.java
@@ -7,18 +7,18 @@ public abstract class Value {
     @Override
     public int hashCode() {
         return Objects.hash(
-                Arrays.stream(getClass().getDeclaredFields())
-                        .map(
-                                field -> {
-                                    field.setAccessible(true);
-                                    try {
-                                        return field.get(this);
-                                    } catch (IllegalAccessException e) {
-                                        e.printStackTrace();
-                                        return null;
-                                    }
-                                }
-                        ).toArray()
+            Arrays.stream(getClass().getDeclaredFields())
+                .map(
+                    field -> {
+                        field.setAccessible(true);
+                        try {
+                            return field.get(this);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                            return null;
+                        }
+                    }
+                ).toArray()
         );
     }
 
@@ -26,14 +26,14 @@ public abstract class Value {
     public boolean equals(Object obj) {
         if (obj == null || getClass() != obj.getClass()) return false;
         return Arrays.stream(getClass().getDeclaredFields())
-                .allMatch(field -> {
-                    field.setAccessible(true);
-                    try {
-                        return Objects.equals(field.get(this), field.get(obj));
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                        return false;
-                    }
-                });
+            .allMatch(field -> {
+                field.setAccessible(true);
+                try {
+                    return Objects.equals(field.get(this), field.get(obj));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            });
     }
 }
