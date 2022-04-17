19533cedf6ef81140ebe4e993019935007068124
hiro <kiryuinh32@gmail.com>
2022-04-17 15:42:03 +0900

    JPAProductRepository expands ProductRepository

--- src/main/java/kitchenpos/products/infrastructure/JPAProductRepository.java
+++ src/main/java/kitchenpos/products/infrastructure/JPAProductRepository.java
@@ -2,10 +2,11 @@ package kitchenpos.products.infrastructure;
 
 import java.util.UUID;
 import kitchenpos.products.domain.Product;
+import kitchenpos.products.domain.ProductRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JPAProductRepository extends JpaRepository<Product, UUID> {
+public interface JPAProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
 
 }
