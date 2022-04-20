8a672e0b0ed01b412e1c161aca1e565b9a6cba1e
hiro <kiryuinh32@gmail.com>
2022-04-20 17:29:48 +0900

    feat: create MenuProduct Object

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
@@ -0,0 +1,32 @@
+package kitchenpos.menus.tobe.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import kitchenpos.products.domain.Product;
+
+public class MenuProduct {
+    private Product product;
+    private BigDecimal price;
+    private long quantity;
+
+    public MenuProduct(Product product, long quantity) {
+        validate(product, quantity);
+        this.product = product;
+        this.price = calcPrice(product, quantity);
+        this.quantity = quantity;
+    }
+
+    private BigDecimal calcPrice(Product product, long quantity) {
+        BigDecimal price = product.getPrice().getPrice();
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    private void validate(Product product, long quantity) {
+        if (Objects.isNull(product)) {
+            throw new IllegalArgumentException();
+        }
+        if (quantity < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/kitchenpos/menus/application/tobe/MenuProductTest.java
@@ -0,0 +1,21 @@
+package kitchenpos.menus.application.tobe;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import kitchenpos.Fixtures;
+import kitchenpos.menus.tobe.domain.MenuProduct;
+import kitchenpos.products.domain.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MenuProductTest {
+
+    @DisplayName("메뉴 상품의 수량이 0보다 작다면 생성하지 못한다.")
+    @ParameterizedTest
+    @ValueSource(longs = {-10, -1})
+    void menu_product_without_product(long quantity) {
+        Product product = Fixtures.product();
+        assertThatThrownBy(() -> new MenuProduct(product, quantity)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
