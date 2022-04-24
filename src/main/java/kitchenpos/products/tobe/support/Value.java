80e18e5a37cdeb40cba3001b2c584a85a4ec7429
백승열 <beck33333@naver.com>
2022-04-25 00:10:53 +0900

    feat(Value) : Value 클래스 추가

---
+++ src/main/java/kitchenpos/products/tobe/support/Value.java
@@ -0,0 +1,39 @@
+package kitchenpos.products.tobe.support;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public abstract class Value<T extends Value<T>> {
+
+    public boolean equals(final Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+
+        return Arrays.stream(getClass().getDeclaredFields())
+                .allMatch(field -> {
+                    field.setAccessible(true);
+                    try {
+                        return Objects.equals(field.get(this),field.get(o));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                });
+    }
+
+    public int hashCode() {
+        return Objects.hash(
+                Arrays.stream(getClass().getDeclaredFields())
+                .map(field -> {
+                    field.setAccessible(true);
+                    try {
+                        return field.get(this);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }).toArray()
+        );
+
+    }
+}
