8c499feb49f3f4397e98a2fb4f705318307671e8
mandykr <hongseokii92@gmail.com>
2022-04-28 12:14:17 +0900

    feat(eatinorders): OrderTable 추가

---
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
@@ -0,0 +1,64 @@
+package kitchenpos.eatinorders.tobe.domain;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Table(name = "order_table")
+@Entity
+public class OrderTable {
+    private static final String EMPTY_TABLE_CANNOT_CHANGE_NUMBER_OF_GUESTS = "빈 테이블은 방문한 손님 수를 변경할 수 없습니다.";
+    @Column(name = "id", columnDefinition = "varbinary(16)")
+    @Id
+    private UUID id;
+
+    @Embedded
+    private OrderTableName name;
+
+    @Embedded
+    private NumberOfGuests numberOfGuests;
+
+    @Column(name = "empty", nullable = false)
+    private boolean empty;
+
+    protected OrderTable() {
+    }
+
+    public OrderTable(String name) {
+        this.id = UUID.randomUUID();
+        this.name = new OrderTableName(name);
+        this.numberOfGuests = new NumberOfGuests(0);
+        this.empty = true;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public OrderTableName getName() {
+        return name;
+    }
+
+    public NumberOfGuests getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public void sit() {
+        this.empty = false;
+    }
+
+    public void changeNumberOfGuests(int numberOfGuests) {
+        if (isEmpty()) {
+            throw new IllegalStateException(EMPTY_TABLE_CANNOT_CHANGE_NUMBER_OF_GUESTS);
+        }
+        this.numberOfGuests = new NumberOfGuests(numberOfGuests);
+    }
+
+    public void clear() {
+        this.empty = true;
+        this.numberOfGuests = new NumberOfGuests(0);
+    }
+}
---
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTableTest.java
@@ -0,0 +1,82 @@
+package kitchenpos.eatinorders.tobe.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTableTest {
+
+    private OrderTable orderTable;
+
+    @BeforeEach
+    void setUp() {
+        orderTable = new OrderTable("1번");
+    }
+
+    @DisplayName("주문 테이블 이름으로 주문 테이블을 생성한다")
+    @Test
+    void create() {
+        assertThatCode(() -> new OrderTable("99번"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("빈 테이블을 해지한다")
+    @Test
+    void sit() {
+        // when
+        orderTable.sit();
+
+        // then
+        assertThat(orderTable.isEmpty()).isFalse();
+    }
+
+    @DisplayName("방문한 손님 수를 변경한다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void changeNumberOfGuests(int numberOfGuests) {
+        // given
+        orderTable.sit();
+
+        // when
+        orderTable.changeNumberOfGuests(numberOfGuests);
+
+        // then
+        assertThat(orderTable.getNumberOfGuests()).isEqualTo(new NumberOfGuests(numberOfGuests));
+    }
+
+    @DisplayName("빈 테이블은 방문한 손님 수를 변경할 수 없다")
+    @Test
+    void changeNumberOfGuestsEmptyTable() {
+        assertThatThrownBy(() -> orderTable.changeNumberOfGuests(1))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("빈 테이블은 방문한 손님 수를 변경할 수 없습니다.");
+    }
+
+    @DisplayName("방문한 손님 수는 0 이상이어야 한다")
+    @Test
+    void changeInvalidNumberOfGuests() {
+        // given
+        orderTable.sit();
+
+        // when
+        assertThatThrownBy(() -> orderTable.changeNumberOfGuests(-1))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("방문한 손님 수는 0 이상이어야 합니다. 입력값 : " + -1);
+    }
+
+    @DisplayName("빈 테이블을 설정한다")
+    @Test
+    void clear() {
+        // when
+        orderTable.clear();
+
+        // then
+        assertThat(orderTable.isEmpty()).isTrue();
+        assertThat(orderTable.getNumberOfGuests()).isEqualTo(new NumberOfGuests(0));
+    }
+}
