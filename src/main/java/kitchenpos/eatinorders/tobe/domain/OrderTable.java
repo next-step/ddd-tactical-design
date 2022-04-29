94109d425af836d76f7bd9feb90c897182709f1d
mandykr <hongseokii92@gmail.com>
2022-04-29 11:59:33 +0900

    feat(eatinorders): OrderTable 기능 추가 - 주문 테이블에 모든 주문이 완료되었는지 확인하는 기능 추가

--- src/main/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
@@ -1,12 +1,14 @@
 package kitchenpos.eatinorders.tobe.domain;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.UUID;
 
 @Table(name = "order_table")
 @Entity
 public class OrderTable {
     private static final String EMPTY_TABLE_CANNOT_CHANGE_NUMBER_OF_GUESTS = "빈 테이블은 방문한 손님 수를 변경할 수 없습니다.";
+
     @Column(name = "id", columnDefinition = "varbinary(16)")
     @Id
     private UUID id;
@@ -20,6 +22,9 @@ public class OrderTable {
     @Column(name = "empty", nullable = false)
     private boolean empty;
 
+    @Embedded
+    private EatInOrders eatInOrders;
+
     protected OrderTable() {
     }
 
@@ -28,6 +33,7 @@ public class OrderTable {
         this.name = new OrderTableName(name);
         this.numberOfGuests = new NumberOfGuests(0);
         this.empty = true;
+        this.eatInOrders = new EatInOrders();
     }
 
     public UUID getId() {
@@ -46,6 +52,10 @@ public class OrderTable {
         return empty;
     }
 
+    public List<EatInOrder> getEatInOrders() {
+        return eatInOrders.getEatInOrders();
+    }
+
     public void sit() {
         this.empty = false;
     }
@@ -55,6 +65,14 @@ public class OrderTable {
             throw new IllegalStateException(EMPTY_TABLE_CANNOT_CHANGE_NUMBER_OF_GUESTS);
         }
         this.numberOfGuests = new NumberOfGuests(numberOfGuests);
+    }
+
+    public void addEatInOrder(EatInOrder eatInOrder) {
+        eatInOrders.add(eatInOrder);
+    }
+
+    public boolean checkAllOrdersCompleted() {
+        return eatInOrders.checkAllCompleted();
     }
 
     public void clear() {
--- src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTableTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTableTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTableTest {
 
@@ -67,6 +68,18 @@ class OrderTableTest {
         assertThatThrownBy(() -> orderTable.changeNumberOfGuests(-1))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("방문한 손님 수는 0 이상이어야 합니다. 입력값 : " + -1);
+    }
+
+    @DisplayName("주문 테이블에 매장 주문을 추가한다")
+    void addEatInOrder() {
+        // given
+        EatInOrder eatInOrder = new EatInOrder(Collections.EMPTY_LIST, orderTable);
+
+        // when
+        orderTable.addEatInOrder(eatInOrder);
+
+        // then
+        assertThat(orderTable.getEatInOrders()).containsExactly(eatInOrder);
     }
 
     @DisplayName("빈 테이블을 설정한다")
