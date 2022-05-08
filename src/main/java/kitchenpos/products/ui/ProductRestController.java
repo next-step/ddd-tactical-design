33e7eb9657008d3f791f5879259be8ff89366ad9
younghoon <bblnotbb@yahho.com>
2022-05-08 11:26:00 +0900

    refactor : refactor controller layer

--- src/main/java/kitchenpos/products/ui/ProductRestController.java
+++ src/main/java/kitchenpos/products/ui/ProductRestController.java
@@ -1,13 +1,21 @@
 package kitchenpos.products.ui;
 
-import kitchenpos.products.application.ProductService;
-import kitchenpos.products.domain.Product;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kitchenpos.products.application.ProductService;
+import kitchenpos.products.dto.ProductRequest;
+import kitchenpos.products.dto.ProductResponse;
 
 @RequestMapping("/api/products")
 @RestController
@@ -19,19 +27,19 @@ public class ProductRestController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@RequestBody final Product request) {
-        final Product response = productService.create(request);
+    public ResponseEntity<ProductResponse> create(@RequestBody final ProductRequest request) {
+        final ProductResponse response = productService.create(request);
         return ResponseEntity.created(URI.create("/api/products/" + response.getId()))
             .body(response);
     }
 
     @PutMapping("/{productId}/price")
-    public ResponseEntity<Product> changePrice(@PathVariable final UUID productId, @RequestBody final Product request) {
+    public ResponseEntity<ProductResponse> changePrice(@PathVariable final UUID productId, @RequestBody final ProductRequest request) {
         return ResponseEntity.ok(productService.changePrice(productId, request));
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> findAll() {
+    public ResponseEntity<List<ProductResponse>> findAll() {
         return ResponseEntity.ok(productService.findAll());
     }
 }
