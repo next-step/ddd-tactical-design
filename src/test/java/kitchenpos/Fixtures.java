c4018ec5fee2d5d69820045ef648ca6945ca852c
younghoon <bblnotbb@yahho.com>
2022-05-08 11:29:10 +0900

    refactor : separate tobe product and asis product

--- src/test/java/kitchenpos/Fixtures.java
+++ src/test/java/kitchenpos/Fixtures.java
@@ -1,18 +1,27 @@
 package kitchenpos;
 
-import kitchenpos.eatinorders.domain.*;
-import kitchenpos.menus.domain.Menu;
-import kitchenpos.menus.domain.MenuGroup;
-import kitchenpos.menus.domain.MenuProduct;
-import kitchenpos.products.domain.Product;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.UUID;
 
+import kitchenpos.eatinorders.domain.Order;
+import kitchenpos.eatinorders.domain.OrderLineItem;
+import kitchenpos.eatinorders.domain.OrderStatus;
+import kitchenpos.eatinorders.domain.OrderTable;
+import kitchenpos.eatinorders.domain.OrderType;
+import kitchenpos.menus.domain.Menu;
+import kitchenpos.menus.domain.MenuGroup;
+import kitchenpos.menus.domain.MenuProduct;
+import kitchenpos.products.infra.PurgomalumClient;
+import kitchenpos.products.tobe.domain.Name;
+import kitchenpos.products.tobe.domain.Price;
+import kitchenpos.products.tobe.domain.Product;
+
 public class Fixtures {
+	private static PurgomalumClient purgomalumClient;
+
     public static final UUID INVALID_ID = new UUID(0L, 0L);
 
     public static Menu menu() {
@@ -53,7 +62,7 @@ public class Fixtures {
         return menuProduct;
     }
 
-    public static MenuProduct menuProduct(final Product product, final long quantity) {
+    public static MenuProduct menuProduct(final kitchenpos.products.domain.Product product, final long quantity) {
         final MenuProduct menuProduct = new MenuProduct();
         menuProduct.setSeq(new Random().nextLong());
         menuProduct.setProduct(product);
@@ -113,15 +122,23 @@ public class Fixtures {
         return orderTable;
     }
 
-    public static Product product() {
-        return product("후라이드", 16_000L);
+	public static kitchenpos.products.domain.Product product() {
+		return product("후라이드", 16_000L);
+	}
+
+	public static kitchenpos.products.domain.Product product(final String name, final long price) {
+		final kitchenpos.products.domain.Product product = new kitchenpos.products.domain.Product();
+		Product.create(new Name(name, purgomalumClient), new Price(BigDecimal.valueOf(price)));
+		return product;
+	}
+
+    public static Product tobeProduct() {
+        return tobeProduct("후라이드", 16_000L);
     }
 
-    public static Product product(final String name, final long price) {
+    public static Product tobeProduct(final String name, final long price) {
         final Product product = new Product();
-        product.setId(UUID.randomUUID());
-        product.setName(name);
-        product.setPrice(BigDecimal.valueOf(price));
+        Product.create(new Name(name, purgomalumClient), new Price(BigDecimal.valueOf(price)));
         return product;
     }
 }
