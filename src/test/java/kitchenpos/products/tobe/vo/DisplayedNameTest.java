ea51d9916496bd1b8c3f4aaad85f440c7abb15f0
chkim <cndghks0213@gmail.com>
2022-09-24 22:32:51 +0900

    refactor: 불필요한 import문 제거

--- src/test/java/kitchenpos/products/tobe/vo/DisplayedNameTest.java
+++ src/test/java/kitchenpos/products/tobe/vo/DisplayedNameTest.java
@@ -1,6 +1,5 @@
 package kitchenpos.products.tobe.vo;
 
-import kitchenpos.products.tobe.vo.DisplayedName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
--- src/test/java/kitchenpos/products/tobe/vo/PriceTest.java
+++ src/test/java/kitchenpos/products/tobe/vo/PriceTest.java
@@ -1,6 +1,5 @@
 package kitchenpos.products.tobe.vo;
 
-import kitchenpos.products.tobe.vo.Price;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
