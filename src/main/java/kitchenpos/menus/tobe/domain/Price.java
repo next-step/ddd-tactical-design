2a674e396907e08c1aeebe35914747015b4a92f2
csh0034 <csh0034@gmail.com>
2022-09-25 20:37:20 +0900

    refactor: Price compareTo 추가

--- src/main/java/kitchenpos/menus/tobe/domain/Price.java
+++ src/main/java/kitchenpos/menus/tobe/domain/Price.java
@@ -31,6 +31,10 @@ public class Price {
         return value;
     }
 
+    public int compareTo(BigDecimal other) {
+        return value.compareTo(other);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
