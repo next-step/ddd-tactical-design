a0472f9f7a7bce4d23c1c8ca8d72fa82f54f61a6
chkim <cndghks0213@gmail.com>
2022-09-27 00:46:41 +0900

    feat: DisplayedName 구현

---
+++ src/main/java/kitchenpos/menus/tobe/vo/DisplayedName.java
@@ -0,0 +1,46 @@
+package kitchenpos.menus.tobe.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class DisplayedName {
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    protected DisplayedName() {
+    }
+
+    public DisplayedName(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름이 null이거나 빈칸이어서는 안됩니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DisplayedName that = (DisplayedName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
