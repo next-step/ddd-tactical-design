22aa01f62a047732b90a84041caa97534694b683
chkim <cndghks0213@gmail.com>
2022-09-27 00:49:04 +0900

    feat: MenuGroup 구현

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuGroupRepository.java
@@ -0,0 +1,6 @@
+package kitchenpos.menus.tobe.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuGroupRepository extends JpaRepository<MenuGroup, Long> {
+}
