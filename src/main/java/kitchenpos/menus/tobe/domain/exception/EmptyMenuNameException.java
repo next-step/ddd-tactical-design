41dea7ef4bd3cf9b8ab0038a72029e5571e7c576
gmoon92 <gmoon0929@gmail.com>
2022-09-27 23:55:06 +0900

    refactor: 불필요한 밸류 클래스 제거

--- src/main/java/kitchenpos/menus/tobe/domain/exception/EmptyMenuNameException.java
+++
@@ -1,8 +0,0 @@
-package kitchenpos.menus.tobe.domain.exception;
-
-public class EmptyMenuNameException extends IllegalArgumentException {
-
-    public EmptyMenuNameException() {
-        super("메뉴 이름은 공백을 가질 수 없다.");
-    }
-}
--- src/main/java/kitchenpos/menus/tobe/domain/exception/MinimumMenuPriceException.java
+++
@@ -1,8 +0,0 @@
-package kitchenpos.menus.tobe.domain.exception;
-
-public class MinimumMenuPriceException extends IllegalArgumentException {
-
-    public MinimumMenuPriceException() {
-        super("메뉴 가격은 0원 이상이어야 한다.");
-    }
-}
