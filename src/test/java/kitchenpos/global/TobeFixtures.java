997dbf6d3ed8baf4deb597a5eac99f1e848448ca
gmoon92 <gmoon0929@gmail.com>
2022-09-27 23:54:20 +0900

    refactor(TobeFixtures): tobe 전용 픽처 클래스 정리

--- src/test/java/kitchenpos/global/TobeFixtures.java
+++ src/test/java/kitchenpos/global/TobeFixtures.java
@@ -1,9 +1,15 @@
 package kitchenpos.global;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 import kitchenpos.global.vo.Name;
 import kitchenpos.global.vo.Price;
 import kitchenpos.global.vo.Quantity;
+import kitchenpos.menus.tobe.domain.Menu;
+import kitchenpos.menus.tobe.domain.MenuGroup;
+import kitchenpos.menus.tobe.domain.MenuProduct;
+import kitchenpos.menus.tobe.domain.MenuProducts;
 import kitchenpos.products.application.FakePurgomalumClient;
 import kitchenpos.products.tobe.domain.Product;
 
@@ -30,5 +36,60 @@ public final class TobeFixtures {
 
     public static Product product(String name, BigDecimal price) {
         return new Product(name(name), price(price));
+    }
+
+    public static Menu menu(BigDecimal price, MenuProduct... menuProduct) {
+        return menu(
+                "후라이드+후라이드",
+                "두마리 치킨",
+                price,
+                false,
+                menuProduct
+        );
+    }
+
+    public static Menu menu(BigDecimal menuPrice, BigDecimal menuProductAmount) {
+        MenuProduct menuProduct = menuProduct(1, menuProductAmount);
+        return new Menu(
+                UUID.randomUUID(),
+                name(" 후라이드+후라이드"),
+                price(menuPrice),
+                menuGroup("두마리 치킨"),
+                false,
+                menuProducts(menuProduct)
+        );
+    }
+
+    public static Menu menu(
+            String name,
+            String groupName,
+            BigDecimal price,
+            boolean displayed,
+            MenuProduct... menuProduct
+    ) {
+        MenuProducts menuProducts = menuProducts(menuProduct);
+        return new Menu(
+                name(name),
+                menuGroup(groupName),
+                menuProducts,
+                price(price),
+                displayed
+        );
+    }
+
+    public static MenuGroup menuGroup(String name) {
+        return new MenuGroup(name(name));
+    }
+
+    public static MenuProduct menuProduct(long quantity, BigDecimal productPrice) {
+        return new MenuProduct(
+                UUID.randomUUID(),
+                quantity(quantity),
+                price(productPrice)
+        );
+    }
+
+    public static MenuProducts menuProducts(MenuProduct... menuProducts) {
+        return new MenuProducts(List.of(menuProducts));
     }
 }
