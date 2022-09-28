5f8d4bd475271a2a735d83a981527e97cfed064d
wlroh <wlroh.paul@gmail.com>
2022-09-29 00:46:20 +0900

    feat: 메뉴그룹 생성 기능을 구현하라

---
+++ src/test/java/kitchenpos/menus/menugroup/tobe/domain/MenuGroupTest.java
@@ -0,0 +1,36 @@
+package kitchenpos.menus.menugroup.tobe.domain;
+
+import kitchenpos.menus.menugroup.tobe.domain.exception.InvalidMenuGroupNameException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuGroupTest {
+
+    @DisplayName("메뉴그룹을 생성한다.")
+    @Nested
+    class CreateTest {
+
+        @DisplayName("성공")
+        @Test
+        void success() {
+            final MenuGroup menuGroup = MenuGroup.create("인기메뉴");
+
+            assertAll(
+                    () -> assertThat(menuGroup.id()).isNotNull(),
+                    () -> assertThat(menuGroup.menuGroupName().value()).isEqualTo("인기메뉴")
+            );
+        }
+
+        @DisplayName("메뉴그룹명이 있어야 한다.")
+        @Test
+        void error() {
+            assertThatThrownBy(() -> MenuGroup.create(null))
+                    .isInstanceOf(InvalidMenuGroupNameException.class);
+        }
+    }
+}
