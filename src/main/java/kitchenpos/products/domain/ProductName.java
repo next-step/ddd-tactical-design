8b38fef6ee6c5be5898f05905afb0c5c9ded282b
hyeon9mak <jinha3507@gmail.com>
2022-09-28 20:55:52 +0900

    feat: 상품 이름 동등성 부여

--- src/main/java/kitchenpos/products/domain/ProductName.java
+++ src/main/java/kitchenpos/products/domain/ProductName.java
@@ -35,4 +35,21 @@ public class ProductName {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ProductName that = (ProductName) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
