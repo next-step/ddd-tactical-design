ec0886bdd32f1c4db63f2f3b24e1a565c1f48f6d
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-29 17:55:55 +0900

    feat($compile): 상품 애그리거트 Repository 구현

---
+++ src/main/java/kitchenpos/products/tobe/domain/JpaProductRepository.java
@@ -0,0 +1,8 @@
+package kitchenpos.products.tobe.domain;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
+
+}
