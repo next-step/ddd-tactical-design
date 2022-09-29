7577c9667ca4907a4b15755b865ce5b1732a3716
csh0034 <csh0034@gmail.com>
2022-09-29 19:25:15 +0900

    feat: OrderLineItem, Price 추가

--- src/test/java/kitchenpos/eatinorders/TobeFixtures.java
+++ src/test/java/kitchenpos/eatinorders/TobeFixtures.java
@@ -1,9 +1,24 @@
 package kitchenpos.eatinorders;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+import kitchenpos.eatinorders.tobe.domain.OrderLineItem;
 import kitchenpos.eatinorders.tobe.domain.OrderTable;
+import kitchenpos.eatinorders.tobe.domain.Price;
 
 public final class TobeFixtures {
+    public static final long ORDER_LINE_ITEM_PRICE = 10_000;
+    public static final long ORDER_LINE_ITEM_QUANTITY = 2;
+
     public static OrderTable orderTable() {
         return new OrderTable("테이블");
+    }
+
+    public static OrderLineItem orderLineItem() {
+        return new OrderLineItem(
+            UUID.randomUUID().toString(),
+            new Price(new BigDecimal(ORDER_LINE_ITEM_PRICE)),
+            ORDER_LINE_ITEM_QUANTITY,
+            true);
     }
 }
