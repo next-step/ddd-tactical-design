7183cd68010c7d78f62a486fbce4a35d57c85b62
ryumodrn <rjm9303@gmail.com>
2022-10-01 01:34:15 +0900

    feature(ExceptionMessages): 조회 실패 메세지 템플릿 추가

--- src/main/java/kitchenpos/core/constant/ExceptionMessages.java
+++ src/main/java/kitchenpos/core/constant/ExceptionMessages.java
@@ -2,9 +2,7 @@ package kitchenpos.core.constant;
 
 public final class ExceptionMessages {
 
-	private ExceptionMessages() {
-	}
-
+	public static final String NOT_FOUND_TEMPLATE = "%s을(를) 찾을 수 없습니다.";
 	public static final String EMPTY_PRICE_TEMPLATE = "%s 가격은 빈 값이 될 수 없습니다.";
 	public static final String NEGATIVE_PRICE_TEMPLATE = "%s 가격은 0보다 작을 수 없습니다.";
 	public static final String EMPTY_QUANTITY_TEMPLATE = "%s 수량은 빈 값이 될 수 없습니다.";
@@ -13,11 +11,15 @@ public final class ExceptionMessages {
 	public static final String PROFANITY_NAME_TEMPLATE = "%s 이름에 비속어가 포함되어 있습니다.";
 	public static final String EMPTY_INVENTORY_TEMPLATE = "%s 목록은 빈 값이 될 수 없습니다.";
 
+	private ExceptionMessages() {
+	}
+
 	public static final class Menu {
+
+		public static final String INVALID_PRICE = "메뉴의 가격이 메뉴 상품의 금액 총합보다 높을 수 없습니다.";
 
 		private Menu() {
 		}
 
-		public static final String INVALID_PRICE = "메뉴의 가격이 메뉴 상품의 금액 총합보다 높을 수 없습니다.";
 	}
 }
