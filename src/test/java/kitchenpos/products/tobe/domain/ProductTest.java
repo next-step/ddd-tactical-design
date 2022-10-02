c868827a9ce17f2b742c19c7a40b4a4f272da7a2
imcool2551 <imcool2551@gmail.com>
2022-10-03 00:13:54 +0900

    test(product): 상품 가격 변경 테스트

--- src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
+++ src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
@@ -5,9 +5,34 @@ import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ProductTest {
+
+    @DisplayName("가격은 0원 이상으로만 변경할 수 있다.")
+    @Test
+    void changePrice_Exception() {
+        Product product = new Product(UUID.randomUUID(), "후라이드 치킨", 9_000L);
+
+        assertThatThrownBy(() -> product.changePrice(-1000L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("가격은 0원 이상으로 변경하면 ProductPriceChangedEvent가 발행된다.")
+    @Test
+    void changePrice() {
+        UUID productId = UUID.randomUUID();
+        Product product = new Product(productId, "후라이드 치킨", 9_000L);
+
+        ProductPriceChangedEvent event = product.changePrice(10_000L);
+
+        assertAll(
+                () -> assertThat(event.getProductId()).isEqualTo(productId),
+                () -> assertThat(event.getChangedPrice()).isEqualTo(10_000L)
+        );
+    }
 
     @DisplayName("식별자가 같으면 동등한 상품이다.")
     @Test
