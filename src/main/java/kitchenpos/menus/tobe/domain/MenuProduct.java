eb4d398417580f02d5dfeb57e0880006120afed2
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-03 22:22:15 +0900

    refactoring($compile): MenuProduct JPA 엔티티 적용

--- src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
@@ -1,14 +1,35 @@
 package kitchenpos.menus.tobe.domain;
 
 import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "menu_product")
 public class MenuProduct {
 
+    @Column(name = "seq")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long seq;
+
+    @Column(name = "product_id", nullable = false)
     private UUID productId;
 
+    @Embedded
     private Price price;
 
+    @Embedded
     private Quantity quantity;
+
+    protected MenuProduct() {
+
+    }
 
     public MenuProduct(UUID productId, Price price, Quantity quantity) {
         this.productId = productId;
--- src/main/java/kitchenpos/menus/tobe/domain/MenuProducts.java
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProducts.java
@@ -1,11 +1,29 @@
 package kitchenpos.menus.tobe.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
+@Embeddable
 public class MenuProducts {
 
+  @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @JoinColumn(
+      name = "menu_id",
+      nullable = false,
+      columnDefinition = "binary(16)",
+      foreignKey = @ForeignKey(name = "fk_menu_product_to_menu")
+  )
   private final List<MenuProduct> menuProducts;
+
+  public MenuProducts() {
+    this(new ArrayList<>());
+  }
 
   public MenuProducts(List<MenuProduct> menuProducts) {
     this.menuProducts = menuProducts;
