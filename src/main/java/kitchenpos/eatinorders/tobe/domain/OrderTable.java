71adaa80198a545b5b88f2fc1cfd3cdcb84627e0
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-05 12:19:37 +0900

    test($compile): 주문 테이블의 이름이 올바르지 않으면 등록할 수 없다.

--- src/main/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
@@ -5,10 +5,16 @@ import java.util.UUID;
 public class OrderTable {
 
   private final UUID id;
-  private final String name;
+  private final OrderTableName name;
   private final int numberOfGuests;
 
   public OrderTable(UUID id, String name, int numberOfGuests) {
+    this.id = id;
+    this.name = OrderTableName.from(name);
+    this.numberOfGuests = numberOfGuests;
+  }
+
+  public OrderTable(UUID id, OrderTableName name, int numberOfGuests) {
     this.id = id;
     this.name = name;
     this.numberOfGuests = numberOfGuests;
--- src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTableTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTableTest.java
@@ -1,10 +1,13 @@
 package kitchenpos.eatinorders.tobe.domain;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class OrderTableTest {
 
@@ -14,4 +17,14 @@ class OrderTableTest {
     assertThatNoException()
         .isThrownBy(() -> new OrderTable(UUID.randomUUID(), "9번", 5));
   }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  @DisplayName("주문 테이블의 이름이 올바르지 않으면 등록할 수 없다.")
+  void create_NotValidName(final String name) {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> new OrderTable(UUID.randomUUID(), name, 5));
+  }
+
+
 }
