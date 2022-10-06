0b82b194aa76bdeeaddfb8eb27a49a179cd30c47
flamme <flamme1004@gmail.com>
2022-10-06 22:07:12 +0900

    refactor: ProductName Validate 로직을 세분화하라

--- src/main/java/kitchenpos/products/tobe/domain/ProductName.java
+++ src/main/java/kitchenpos/products/tobe/domain/ProductName.java
@@ -12,8 +12,12 @@ public class ProductName {
     private String name;
 
     public ProductName(final String name, final PurgomalumClient purgomalumClient) {
-        if (Objects.isNull(name) || purgomalumClient.containsProfanity(name)) {
-            throw new IllegalArgumentException();
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("이름이 존재하지 않습니다.");
+        }
+
+        if (purgomalumClient.containsProfanity(name)) {
+            throw new IllegalArgumentException("이름에 비속어가 들어가 있습니다.");
         }
 
         this.name = name;
@@ -26,7 +30,7 @@ public class ProductName {
     public ProductName(String name) {
         this.name = name;
     }
-    
+
     public String getName() {
         return name;
     }
