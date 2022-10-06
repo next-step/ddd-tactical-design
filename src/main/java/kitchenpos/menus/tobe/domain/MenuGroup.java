4446fc07393ab6aee2616ed47ac3740c1413d6a2
imcool2551 <imcool2551@gmail.com>
2022-10-06 19:47:40 +0900

    feat(menus): MenuGroup, MenuGroupRepository

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuGroup.java
@@ -0,0 +1,39 @@
+package kitchenpos.menus.tobe.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity(name = "Tobe_MenuGroup")
+public class MenuGroup {
+
+    @Id
+    @Column(name = "id", columnDefinition = "binary(16)")
+    private UUID id;
+
+    @Embedded
+    private MenuGroupName name;
+
+    protected MenuGroup() {
+    }
+
+    public MenuGroup(Profanity profanity, String name) {
+        this.name = new MenuGroupName(profanity, name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuGroup menuGroup = (MenuGroup) o;
+        return id.equals(menuGroup.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuGroupRepository.java
@@ -0,0 +1,15 @@
+package kitchenpos.menus.tobe.domain;
+
+import kitchenpos.menus.domain.MenuGroup;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MenuGroupRepository {
+    MenuGroup save(MenuGroup menuGroup);
+
+    Optional<MenuGroup> findById(UUID id);
+
+    List<MenuGroup> findAll();
+}
