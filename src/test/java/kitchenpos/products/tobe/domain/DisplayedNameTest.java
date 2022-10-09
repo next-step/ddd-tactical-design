7df9f4dbe41ac53193e1af829f44cc0ca24fbe84
mirrors89 <lee01494@gmail.com>
2022-10-09 10:14:49 +0900

    test: Product, DisplayedName - 비속어 테스트 코드 추가

--- src/test/java/kitchenpos/products/tobe/domain/DisplayedNameTest.java
+++ src/test/java/kitchenpos/products/tobe/domain/DisplayedNameTest.java
@@ -2,6 +2,8 @@ package kitchenpos.products.tobe.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -10,7 +12,7 @@ class DisplayedNameTest {
     private final Profanity profanity = new FakePurgomalumClient();
 
     @Test
-    @DisplayName("상점에 노출할 이름을 지을수 있다.")
+    @DisplayName("이름을 지을수 있다.")
     void name() {
         DisplayedName displayedName1 = new DisplayedName("상점 제목", profanity);
         DisplayedName displayedName2 = new DisplayedName("상점 제목", profanity);
@@ -19,9 +21,18 @@ class DisplayedNameTest {
     }
 
     @Test
-    @DisplayName("상점에 노출할 이름은 비어있을 수 없다.")
+    @DisplayName("이름은 비어있을 수 없다.")
     void name_empty() {
         assertThatThrownBy(() -> new DisplayedName("", profanity))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"비속어", "욕설이 포함된 이름"})
+    @DisplayName("이름이 비속어가 포함될 수 없다.")
+    void name_profanity(String name) {
+        assertThatThrownBy(() -> new DisplayedName(name, profanity))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
--- src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
+++ src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
@@ -2,6 +2,8 @@ package kitchenpos.products.tobe.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,6 +28,15 @@ class ProductTest {
     @DisplayName("이름이 비어있으면 상품을 만들 수 없다.")
     void product_empty_name() {
         assertThatThrownBy(() -> new Product(new DisplayedName("", profanity), new Price(1000L)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"비속어", "욕설이 포함된 이름"})
+    @DisplayName("상품의 이름이 올바르지 않으면 상품을 만들 수 없다.")
+    void name_profanity(String name) {
+        assertThatThrownBy(() -> new Product(new DisplayedName(name, profanity), new Price(1000L)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
