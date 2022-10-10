8b15ff37f64341f027153be92b8757ae11bb406f
benggrae <kbh0581@gmail.com>
2022-10-10 22:55:53 +0900

    feat: 욕설 도메인 추가

---
+++ src/main/java/kitchenpos/products/domain/Profanity.java
@@ -0,0 +1,33 @@
+package kitchenpos.products.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.util.ObjectUtils;
+
+public class Profanity {
+    private final Set<String> profanitys;
+
+    public Profanity(Set<String> profanitys) {
+        if (ObjectUtils.isEmpty(profanitys)) {
+            throw new IllegalArgumentException("비어 있을 수 없습니다.");
+        }
+        this.profanitys = profanitys;
+    }
+
+    private void add(String word) {
+        if (ObjectUtils.isEmpty(word)) {
+            throw new IllegalArgumentException("추가할 단어는 비어 있을수 없습니다.");
+        }
+        profanitys.add(word);
+    }
+
+    public static Profanity from(String profanity) {
+        final Profanity profanitys = new Profanity(new HashSet<>());
+        profanitys.add(profanity);
+        return profanitys;
+    }
+
+    public boolean isHas(String word) {
+        return profanitys.contains(word);
+    }
+}
