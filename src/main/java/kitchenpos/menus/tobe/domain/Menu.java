fce720abbc3a0b5e2c4eece3a94ce27c36254136
sonjuwon <logicalstark1105@gmail.com>
2022-10-10 17:24:23 +0900

    feat(Product): 메뉴 가격 변경

--- src/main/java/kitchenpos/menus/tobe/domain/Menu.java
+++ src/main/java/kitchenpos/menus/tobe/domain/Menu.java
@@ -1,4 +1,19 @@
 package kitchenpos.menus.tobe.domain;
 
 public class Menu {
+    private MenuPrice price;
+    private DisplayedName name;
+
+    public Menu(final MenuPrice price, final DisplayedName name) {
+        this.price = price;
+        this.name = name;
+    }
+
+    public MenuPrice getPrice() {
+        return price;
+    }
+
+    public void changePrice(final MenuPrice price) {
+        this.price = price;
+    }
 }
--- src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
@@ -3,9 +3,13 @@ package kitchenpos.menus.tobe.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /*
-- 메뉴의 가격을 변경할 수 있다.
-- 메뉴의 가격이 올바르지 않으면 변경할 수 없다.
+- [x] 메뉴의 가격을 변경할 수 있다.
+- [x] 메뉴의 가격이 올바르지 않으면 변경할 수 없다.
 - 메뉴에 속한 상품의 수량은 0 이상이어야 한다.
 - 메뉴에 속한 상품 금액의 합은 메뉴의 가격보다 크거나 같아야 한다.
 - 메뉴는 특정 메뉴 그룹에 속해야 한다.
@@ -18,9 +22,15 @@ import org.junit.jupiter.api.Test;
 - 메뉴의 목록을 조회할 수 있다.
  */
 class MenuTest {
-    @DisplayName("메뉴를 생성할 수 있다.")
+    @DisplayName("메뉴의 가격을 변경할 수 있다.")
     @Test
     void construct() {
-        Menu menu = new Menu();
+        final MenuPrice price = new MenuPrice(BigDecimal.TEN);
+        final DisplayedName name = new DisplayedName("치킨 세트", new FakeProfanity());
+        final Menu menu = new Menu(price, name);
+
+        menu.changePrice(new MenuPrice(BigDecimal.ONE));
+
+        assertThat(menu.getPrice()).isEqualTo(new MenuPrice(BigDecimal.ONE));
     }
 }
