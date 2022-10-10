b4a3a6eadf7b2f288869a75b0c231cf728dc8391
choiseon <com1368@naver.com>
2022-10-10 11:35:15 +0900

    feat : OrderTable 기능 코드 구현

---
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/ordertable/OrderTableRepository.java
@@ -0,0 +1,14 @@
+package kitchenpos.eatinorders.tobe.domain.ordertable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderTableRepository {
+
+    OrderTable save(OrderTable orderTable);
+
+    Optional<OrderTable> findById(UUID id);
+
+    List<OrderTable> findAll();
+}
