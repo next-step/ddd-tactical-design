e9eaa99da1df506473a3deb19f3ef7eb601d85fe
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-12 08:25:18 +0900

    feat(Menu.java) : 메뉴의 가격은 0원 이상이어야 한다.

--- src/main/java/kitchenpos/menus/tobe/domain/menu/Menu.java
+++ src/main/java/kitchenpos/menus/tobe/domain/menu/Menu.java
@@ -3,6 +3,7 @@ package kitchenpos.menus.tobe.domain.menu;
 import kitchenpos.products.tobe.domain.Price;
 
 import javax.persistence.Column;
+import java.math.BigDecimal;
 
 public class Menu {
 
@@ -15,18 +16,21 @@ public class Menu {
 
     public Menu(Price price, MenuProducts menuProducts) {
         this.price = price;
-        validate(menuProducts);
+        validate(price, menuProducts);
         this.menuProducts = menuProducts;
     }
 
-    private void validate(MenuProducts menuProducts) {
+    private void validate(Price price, MenuProducts menuProducts) {
         validateMenuProductsSize(menuProducts);
-        validatePrice(menuProducts);
+        validatePrice(price, menuProducts);
     }
 
-    private void validatePrice(MenuProducts menuProducts) {
+    private void validatePrice(Price price, MenuProducts menuProducts) {
         if (this.price.price().compareTo(menuProducts.sum()) > 0) {
             throw new IllegalArgumentException("메뉴에 속한 상품 금액의 합은 메뉴의 가격보다 작을 수 없습니다.");
+        }
+        if (price.price().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("메뉴의 가격은 0원 이상이어야 한다.");
         }
     }
 
