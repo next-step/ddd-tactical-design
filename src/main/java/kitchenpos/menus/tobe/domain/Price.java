d3142d2ca0e9eac6e8e2700c98c3318f5c1f15d7
sonjuwon <logicalstark1105@gmail.com>
2022-10-13 19:01:47 +0900

    refactor(Price): 메뉴와 상품에서 사용하도록 변경

--- src/main/java/kitchenpos/menus/tobe/domain/Price.java
+++ src/main/java/kitchenpos/menus/tobe/domain/Price.java
@@ -3,13 +3,13 @@ package kitchenpos.menus.tobe.domain;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public class MenuPrice {
-    private static final String EMPTY_NAME_MESSAGE = "메뉴 가격은 비어있을 수 없습니다.";
-    private static final String INVALID_PRICE_MESSAGE = "메뉴 가격은 0보다 크거다 같아야 합니다.";
+class Price implements Comparable<Price> {
+    private static final String EMPTY_NAME_MESSAGE = "가격은 비어있을 수 없습니다.";
+    private static final String INVALID_PRICE_MESSAGE = "가격은 0보다 크거다 같아야 합니다.";
 
     private final BigDecimal price;
 
-    public MenuPrice(final BigDecimal price) {
+     Price(final BigDecimal price) {
         if (null == price) {
             throw new IllegalArgumentException(EMPTY_NAME_MESSAGE);
         }
@@ -19,16 +19,25 @@ public class MenuPrice {
         this.price = price;
     }
 
+    public Price multiply(long quantity) {
+        return new Price(price.multiply(BigDecimal.valueOf(quantity)));
+    }
+
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
         if (null == o || getClass() != o.getClass()) return false;
-        final MenuPrice menuPrice = (MenuPrice) o;
+        final Price menuPrice = (Price) o;
         return price == null ? menuPrice.price == null : price.compareTo(menuPrice.price) == 0;
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(price);
+    }
+
+    @Override
+    public int compareTo(final Price o) {
+        return price.subtract(o.price).intValue();
     }
 }
--- src/test/java/kitchenpos/menus/tobe/domain/PriceTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/PriceTest.java
@@ -13,14 +13,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 - [x] 메뉴의 가격은 0보다 크거나 같아야 한다.
 - [x] 메뉴의 가격이 올바르지 않으면 등록할 수 없다. (Null)
  */
-class MenuPriceTest {
+class PriceTest {
     @DisplayName("메뉴의 가격은 0보다 크거나 같아야 한다.")
     @Test
     void registerWithLessThanZero() {
-        final String INVALID_PRICE_MESSAGE = "메뉴 가격은 0보다 크거다 같아야 합니다.";
+        final String INVALID_PRICE_MESSAGE = "가격은 0보다 크거다 같아야 합니다.";
         final BigDecimal price = BigDecimal.valueOf(-1L);
 
-        assertThatThrownBy(() -> new MenuPrice(price))
+        assertThatThrownBy(() -> new Price(price))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_PRICE_MESSAGE);
     }
@@ -29,9 +29,9 @@ class MenuPriceTest {
     @NullSource
     @ParameterizedTest
     void registerWithEmptyName(final BigDecimal price) {
-        final String EMPTY_NAME_MESSAGE = "메뉴 가격은 비어있을 수 없습니다.";
+        final String EMPTY_NAME_MESSAGE = "가격은 비어있을 수 없습니다.";
 
-        assertThatThrownBy(() -> new MenuPrice(price))
+        assertThatThrownBy(() -> new Price(price))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(EMPTY_NAME_MESSAGE);
     }
