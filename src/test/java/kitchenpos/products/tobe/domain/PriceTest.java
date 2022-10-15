85c3aa61843c4cca8a7c3baa520ba154b049fb81
benggrae <kbh0581@gmail.com>
2022-10-15 15:58:34 +0900

    test: price 테스트 구문 변경

--- src/test/java/kitchenpos/products/tobe/domain/PriceTest.java
+++ src/test/java/kitchenpos/products/tobe/domain/PriceTest.java
@@ -3,15 +3,17 @@ package kitchenpos.products.tobe.domain;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PriceTest {
 
-    @DisplayName("가격은 0원 이상이어야 한다")
-    @Test
-    void price() {
+    @DisplayName("가격은 0원 미만이면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3})
+    void price(int price) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> Price.from(-3)
+                .isThrownBy(() -> Price.from(price)
         );
     }
 }
