757cf977f85e9b05fcc5e7d01b0d21e559945836
lehdqlsl <lehdqlsl@naver.com>
2022-10-16 13:11:19 +0900

    test(Menu): 테스트코드 작성.

--- src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -14,6 +15,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
       - 메뉴의 가격은 0원 이상이어야 한다.
     - 메뉴에 속한 상품 금액의 합은 메뉴의 가격보다 크거나 같아야 한다.
     - 메뉴는 특정 메뉴 그룹에 속해야 한다.
+    - 메뉴의 가격을 변경할 수 있다.
+    - 메뉴의 가격이 올바르지 않으면 변경할 수 없다.
+    - 메뉴를 노출할 수 있다.
+    - 메뉴의 가격이 메뉴에 속한 상품 금액의 합보다 높을 경우 메뉴를 노출할 수 없다.
+    - 메뉴를 숨길 수 있다.
+    - 메뉴의 목록을 조회할 수 있다.
  */
 public class MenuTest {
 
@@ -25,7 +32,6 @@ public class MenuTest {
         final DisplayedName name = new DisplayedName("치킨", profanities);
         final Price price = new Price(20000);
         final Long menuGroupId = 1L;
-
         MenuProduct menuProductId1 = new MenuProduct(1L, 10000, 1);
         MenuProduct menuProductId2 = new MenuProduct(2L, 15000, 1);
 
@@ -34,11 +40,10 @@ public class MenuTest {
 
     @DisplayName("메뉴에 속한 상품 금액의 합은 메뉴의 가격보다 작으면 예외가 발생한다.")
     @Test
-    public void price_exception() {
+    public void price() {
         final DisplayedName name = new DisplayedName("치킨", profanities);
         final Price price = new Price(20000);
         final Long menuGroupId = 1L;
-
         MenuProduct menuProductId1 = new MenuProduct(1L, 10000, 1);
         MenuProduct menuProductId2 = new MenuProduct(2L, 5000, 1);
 
@@ -46,9 +51,52 @@ public class MenuTest {
                 -> new Menu(name, price, menuGroupId, List.of(menuProductId1, menuProductId2)));
     }
 
-    @DisplayName("메뉴는 특정 메뉴 그룹에 속해야 한다.")
+    @DisplayName("메뉴의 가격을 변경할 수 있다.")
     @Test
-    void name() {
+    void changePrice() {
+        Menu menu = createMenu();
 
+        assertDoesNotThrow(() -> menu.changePrice(new Price(19000)));
     }
+
+    @DisplayName("메뉴를 노출할 수 있다.")
+    @Test
+    public void display() {
+        Menu menu = createMenu();
+
+        assertThat(menu.isDisplayed()).isTrue();
+    }
+
+    @DisplayName("메뉴를 숨길 수 있다.")
+    @Test
+    public void hide() {
+        Menu menu = createMenu();
+
+        menu.hide();
+
+        assertThat(menu.isDisplayed()).isFalse();
+    }
+
+    @DisplayName("메뉴의 가격이 메뉴에 속한 상품 금액의 합보다 높을 경우 메뉴를 노출할 수 없다.")
+    @Test
+    public void display_exception() {
+        Menu menu = createMenu();
+
+        menu.changePrice(new Price(26000));
+
+        assertThrows(IllegalArgumentException.class, () -> menu.display());
+    }
+
+    private Menu createMenu() {
+        final DisplayedName name = new DisplayedName("치킨", profanities);
+        final Price price = new Price(20000);
+        final Long menuGroupId = 1L;
+
+        MenuProduct menuProductId1 = new MenuProduct(1L, 10000, 1);
+        MenuProduct menuProductId2 = new MenuProduct(2L, 15000, 1);
+
+        Menu menu = new Menu(name, price, menuGroupId, List.of(menuProductId1, menuProductId2));
+        return menu;
+    }
+
 }
