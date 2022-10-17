b3a050c10205fcbccf3d2500cdbbb6d0a7f1b0c2
이정수 <lsecret@naver.com>
2022-10-17 19:36:08 +0900

    refactor: 주문 테이블 도메인 리팩토링

---
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
@@ -0,0 +1,91 @@
+package kitchenpos.eatinorders.tobe.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
+
+@Table(name = "order_table")
+@Entity
+public class OrderTable {
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Embedded
+    @Column(name = "name", nullable = false)
+    private OrderTableName name;
+
+    @Embedded
+    @Column(name = "number_of_guests", nullable = false)
+    private NumberOfGuests numberOfGuests;
+
+    @Column(name = "occupied", nullable = false)
+    private boolean occupied;
+
+    protected OrderTable() {
+    }
+
+    public OrderTable(OrderTableName name) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.numberOfGuests = new NumberOfGuests(0);
+        this.occupied = false;
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public OrderTableName name() {
+        return name;
+    }
+
+    public String nameValue() {
+        return this.name.name();
+    }
+
+    public NumberOfGuests numberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public int numberOfGuestsValue() {
+        return numberOfGuests.numberOfGuests();
+    }
+
+    public boolean occupied() {
+        return occupied;
+    }
+
+    public void sit() {
+        this.occupied = true;
+    }
+
+    public void clear() {
+        this.numberOfGuests = new NumberOfGuests(0);
+        this.occupied = false;
+    }
+
+    public void changeNumberOfGuest(int numberOfGuest) {
+        emptyTableCheck();
+        this.numberOfGuests.changeNumberOfGuest(numberOfGuest);
+    }
+
+    private void emptyTableCheck() {
+        if (!this.occupied) {
+            throw new IllegalStateException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderTable that = (OrderTable) o;
+        return occupied == that.occupied && Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(numberOfGuests, that.numberOfGuests);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, numberOfGuests, occupied);
+    }
+}
---
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/NumberOfGuestsTest.java
@@ -0,0 +1,35 @@
+package kitchenpos.eatinorders.tobe.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NumberOfGuestsTest {
+
+    @Test
+    @DisplayName("방문 고객 수를 생성한다.")
+    void createNumberOfGuests() {
+        // given
+        int guests = 0;
+
+        // when
+        NumberOfGuests numberOfGuests = new NumberOfGuests(guests);
+
+        // then
+        assertThat(numberOfGuests).isEqualTo(new NumberOfGuests(guests));
+    }
+
+    @ParameterizedTest
+    @DisplayName("상품 가격이 0보다 작을 경우 Exception을 발생시킨다.")
+    @ValueSource(ints = {-1})
+    void createMenuPrice(int numberOfGuests) {
+        // when
+        // then
+        assertThatThrownBy(() -> new NumberOfGuests(numberOfGuests))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
