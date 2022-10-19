66c7a8cf21dde35079b1dad540c39a5df7ccd48d
JunGu Kang <chr0m3@users.noreply.github.com>
2022-10-19 23:57:02 +0900

    Price 추가

---
+++ src/main/java/kitchenpos/common/price/Price.java
@@ -0,0 +1,51 @@
+package kitchenpos.common.price;
+
+import java.math.BigDecimal;
+
+/**
+ * <h1>가격</h1>
+ * <ul>
+ *   <li>가격은 음수일 수 없다.</li>
+ * </ul>
+ */
+public class Price {
+
+    public final BigDecimal value;
+
+    public Price(BigDecimal value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Price는 null일 수 없습니다");
+        }
+        if (value.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price는 음수일 수 없습니다");
+        }
+        this.value = value;
+    }
+
+    public Price(long value) {
+        this(BigDecimal.valueOf(value));
+    }
+
+    public Price(double value) {
+        this(BigDecimal.valueOf(value));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Price price = (Price) o;
+
+        return value.equals(price.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
