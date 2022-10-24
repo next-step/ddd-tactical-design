2c7035e664d10156fd51d7057177e0b58e078ea7
JunGu Kang <chr0m3@users.noreply.github.com>
2022-10-24 15:52:54 +0900

    불필요해진 과거 코드 제거

--- src/main/java/kitchenpos/menu/infra/JpaMenuGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package kitchenpos.menu.infra;
-
-import java.util.UUID;
-import kitchenpos.menu.domain.MenuGroup;
-import kitchenpos.menu.domain.MenuGroupRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaMenuGroupRepository extends MenuGroupRepository, JpaRepository<MenuGroup, UUID> {
-}
