6defaec089466dc616b4fc136f8be3e814ba8a68
sonjuwon <logicalstark1105@gmail.com>
2022-10-24 21:03:44 +0900

    feat(Order): 배달 주문 테스트 케이스 추가

--- src/main/java/kitchenpos/eatinorders/tobe/domain/Order.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/Order.java
@@ -1,10 +1,13 @@
 package kitchenpos.eatinorders.tobe.domain;
 
+import kitchenpos.deliveryorders.infra.KitchenridersClient;
 import kitchenpos.eatinorders.domain.OrderStatus;
 import kitchenpos.eatinorders.domain.OrderType;
 import org.aspectj.weaver.ast.Or;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 public class Order {
     private static final String NOT_WAITING_MESSAGE = "대기 상태에만 수락 상태로 변경할 수 있습니다.";
@@ -12,6 +15,7 @@ public class Order {
     private static final String NOT_SERVED_MESSAGE = "서빙 완료 상태에서만 완료 상태로 변경할 수 있습니다.";
     private static final String CAN_NOT_MINUS_QUANTITY_EXCEPT_EAT_IN = "매장 주문이 아니라면 주문 수량은 0보다 크거나 같아야 한다.";
 
+    private UUID id;
     private final OrderType orderType;
     private final List<OrderLineItem> orderLineItems;
     private OrderStatus orderStatus;
@@ -43,12 +47,29 @@ public class Order {
         this.orderTable = orderTable;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
     public OrderStatus getOrderStatus() {
         return orderStatus;
     }
 
     public OrderTable getOrderTable() {
         return orderTable;
+    }
+
+    public void accept(KitchenridersClient client, BigDecimal sum) {
+        if (orderStatus != OrderStatus.WAITING) {
+            throw new IllegalArgumentException(NOT_WAITING_MESSAGE);
+        }
+        client.requestDelivery(id, sum, deliveryAddress);
+
+        orderStatus = OrderStatus.ACCEPTED;
     }
 
     public void accept() {
@@ -71,5 +92,22 @@ public class Order {
         }
         orderStatus = OrderStatus.COMPLETED;
         orderTable.makeEmpty();
+    }
+
+    public void startDelivery() {
+        if (orderType != OrderType.DELIVERY) {
+            throw new IllegalArgumentException();
+        }
+        if (orderStatus != OrderStatus.SERVED) {
+            throw new IllegalArgumentException();
+        }
+        orderStatus = OrderStatus.DELIVERING;
+    }
+
+    public void completeDelivery() {
+        if (orderStatus != OrderStatus.DELIVERING) {
+            throw new IllegalArgumentException();
+        }
+        orderStatus = OrderStatus.DELIVERED;
     }
 }
