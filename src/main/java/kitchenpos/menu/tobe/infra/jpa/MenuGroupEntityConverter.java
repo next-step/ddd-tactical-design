1c460a0ccd8f6e82aa986446923ef5c00723a3d3
JunGu Kang <chr0m3@users.noreply.github.com>
2022-10-24 17:41:05 +0900

    Converter 인터페이스 제거

--- src/main/java/kitchenpos/menu/tobe/infra/jpa/MenuGroupEntityConverter.java
+++ src/main/java/kitchenpos/menu/tobe/infra/jpa/MenuGroupEntityConverter.java
@@ -2,7 +2,6 @@ package kitchenpos.menu.tobe.infra.jpa;
 
 import kitchenpos.common.name.NameFactory;
 import kitchenpos.menu.tobe.domain.entity.MenuGroup;
-import kitchenpos.util.converter.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +11,8 @@ public class MenuGroupEntityConverter {
     }
 
     @Component
-    public static class MenuGroupToMenuGroupEntityConverter
-        implements Converter<MenuGroup, MenuGroupEntity> {
+    public static class MenuGroupToMenuGroupEntityConverter {
 
-        @Override
         public MenuGroupEntity convert(final MenuGroup source) {
             final MenuGroupEntity menuGroupEntity = new MenuGroupEntity();
             menuGroupEntity.id = source.id;
@@ -25,8 +22,7 @@ public class MenuGroupEntityConverter {
     }
 
     @Component
-    public static class MenuGroupEntityToMenuGroupConverter
-        implements Converter<MenuGroupEntity, MenuGroup> {
+    public static class MenuGroupEntityToMenuGroupConverter {
 
         private final NameFactory nameFactory;
 
@@ -35,7 +31,6 @@ public class MenuGroupEntityConverter {
             this.nameFactory = nameFactory;
         }
 
-        @Override
         public MenuGroup convert(final MenuGroupEntity source) {
             return new MenuGroup(
                 source.id,
--- src/main/java/kitchenpos/util/converter/Converter.java
+++
@@ -1,6 +0,0 @@
-package kitchenpos.util.converter;
-
-public interface Converter<S, D> {
-
-    D convert(final S source);
-}
