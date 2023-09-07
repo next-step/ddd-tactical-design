67422b07f3ee339b5b5aee227c700a04311e6730
jehyeong <5909264@kbfg.com>
2023-09-07 13:03:02 +0900

    test : productPrice test 추가

---
+++ src/test/java/kitchenpos/products/domain/tobe/domain/ProductPriceTest.java
@@ -0,0 +1,56 @@
+package kitchenpos.products.domain.tobe.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ProductPriceTest {
+
+    @Test
+    void nullPrice() {
+        assertThatThrownBy(() -> ProductPrice.of(null))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void minusPrice() {
+        assertThatThrownBy(() -> ProductPrice.of(-1))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void changePrice() {
+        var price = ProductPrice.of(30_000L);
+        var newPrice = price.changePrice(30_001L);
+        assertThat(newPrice.isSamePrice(ProductPrice.of(30_001L))).isTrue();
+    }
+
+    @Test
+    void isGreaterThan1() {
+        var price = ProductPrice.of(30_000L);
+        assertThat(price.isGreaterThan(29_999L)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {30000, 30001})
+    void isGreaterThan2(long value) {
+        var price = ProductPrice.of(30_000L);
+        assertThat(price.isGreaterThan(value)).isFalse();
+    }
+
+    @Test
+    void isSamePrice() {
+        var price1 = ProductPrice.of(30_000L);
+        var price2 = ProductPrice.of(30_000L);
+        assertThat(price1.isSamePrice(price2)).isTrue();
+    }
+
+    @Test
+    void isSameObject() {
+        var price1 = ProductPrice.of(30_000L);
+        var price2 = ProductPrice.of(30_000L);
+        assertThat(price1).isEqualTo(price2);
+    }
+}
