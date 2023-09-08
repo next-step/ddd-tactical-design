ace2b22bf7188befc4838235edf7d276245e55e8
ryuhistory <ryuhistory@gmail.com>
2023-09-08 13:40:04 +0900

    test : display name 추가

--- src/test/java/kitchenpos/products/domain/tobe/domain/ProductPriceTest.java
+++ src/test/java/kitchenpos/products/domain/tobe/domain/ProductPriceTest.java
@@ -2,24 +2,28 @@ package kitchenpos.products.domain.tobe.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ProductPriceTest {
 
+    @DisplayName("상품 가격은 null을 가질 수 없다.")
     @Test
     void nullPrice() {
         assertThatThrownBy(() -> ProductPrice.of(null))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("상품가격은 0원 이상이어야 한다")
     @Test
     void minusPrice() {
         assertThatThrownBy(() -> ProductPrice.of(-1))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("상품가격을 변경하면, 변경된 가격을 확인 할 수 있다.")
     @Test
     void changePrice() {
         var price = ProductPrice.of(30_000L);
@@ -27,12 +31,14 @@ class ProductPriceTest {
         assertThat(newPrice.isSamePrice(ProductPrice.of(30_001L))).isTrue();
     }
 
+    @DisplayName("상품가격이 입력한 금액보다 크다")
     @Test
     void isGreaterThan1() {
         var price = ProductPrice.of(30_000L);
         assertThat(price.isGreaterThan(ProductPrice.of(29_999L))).isTrue();
     }
 
+    @DisplayName("상품가격이 입력한 가격과 같거나 크다.")
     @ParameterizedTest
     @ValueSource(longs = {30000, 30001})
     void isGreaterThan2(long value) {
@@ -40,6 +46,7 @@ class ProductPriceTest {
         assertThat(price.isGreaterThan(ProductPrice.of(value))).isFalse();
     }
 
+    @DisplayName("상품가격을 더하면 합계 상품 금액을 확인 할 수 있다.")
     @Test
     void addPrice() {
         var price1 = ProductPrice.of(30_000L);
@@ -47,6 +54,7 @@ class ProductPriceTest {
         assertThat(price1.add(price2)).isEqualTo(ProductPrice.of(40_000L));
     }
 
+    @DisplayName("상품가격이 동일한 가격을 가지고 있다.")
     @Test
     void isSamePrice() {
         var price1 = ProductPrice.of(30_000L);
@@ -54,6 +62,7 @@ class ProductPriceTest {
         assertThat(price1.isSamePrice(price2)).isTrue();
     }
 
+    @DisplayName("동일한 상품가격은 같은 객체로 간주한다.")
     @Test
     void isSameObject() {
         var price1 = ProductPrice.of(30_000L);
--- src/test/java/kitchenpos/products/domain/tobe/domain/ToBeProductTest.java
+++ src/test/java/kitchenpos/products/domain/tobe/domain/ToBeProductTest.java
@@ -5,29 +5,31 @@ import static org.assertj.core.api.Assertions.*;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ToBeProductTest {
 
+    private ToBeProduct product;
     private BigDecimal price;
 
     @BeforeEach
     void setUp() {
         price = BigDecimal.valueOf(30_000L);
+        product = new ToBeProduct("돈가스", price, false);
     }
 
+    @DisplayName("가격을 변경한다")
     @Test
     void changePrice() {
-
-        ToBeProduct product = new ToBeProduct("돈가스", price);
         product.changePrice(price);
         assertThat(product.isSamePrice(price))
             .isTrue();
     }
 
+    @DisplayName("상품 가격이 입력 금액보다 크다")
     @Test
     void isGreaterThanPrice() {
-        ToBeProduct product = new ToBeProduct("돈가스", price);
         assertThat(product.isGreaterThanPrice(BigDecimal.valueOf(29_999L)))
             .isTrue();
     }
