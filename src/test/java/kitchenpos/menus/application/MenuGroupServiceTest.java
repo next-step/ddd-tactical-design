e8154aaeb5d3c07e07d1a4cd4831ea13a3c02c83
jeunghoonsong <thdwmdgns@naver.com>
2023-09-08 13:01:10 +0900

    test: MenuGroup 테스트코드 합치기

--- src/test/java/kitchenpos/menus/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/menus/application/MenuGroupServiceTest.java
@@ -1,21 +1,22 @@
 package kitchenpos.menus.application;
 
-import kitchenpos.menus.domain.MenuGroup;
-import kitchenpos.menus.domain.MenuGroupRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-
-import java.util.List;
-
-import static kitchenpos.Fixtures.menuGroup;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
+import java.util.stream.Stream;
+import kitchenpos.menus.domain.MenuGroup;
+import kitchenpos.menus.domain.MenuGroupRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class MenuGroupServiceTest {
+
     private MenuGroupRepository menuGroupRepository;
     private MenuGroupService menuGroupService;
 
@@ -25,38 +26,66 @@ class MenuGroupServiceTest {
         menuGroupService = new MenuGroupService(menuGroupRepository);
     }
 
-    @DisplayName("메뉴 그룹을 등록할 수 있다.")
-    @Test
-    void create() {
-        final MenuGroup expected = createMenuGroupRequest("두마리메뉴");
-        final MenuGroup actual = menuGroupService.create(expected);
-        assertThat(actual).isNotNull();
+    @DisplayName("메뉴그룹의 이름은 반드시 있어야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void test1(String name) {
+        //given
+        MenuGroup request = new MenuGroup();
+        request.setName(name);
+
+        //when && then
+        assertThatThrownBy(
+            () -> menuGroupService.create(request)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴그룹을 등록할수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"메뉴그룹1", "메뉴그룹2", "메뉴그룹3", "메뉴 그룹 4", "메뉴%그룹@5"})
+    void test2(String name) {
+        //given
+        MenuGroup request = new MenuGroup();
+        request.setName(name);
+
+        //when
+        MenuGroup result = menuGroupService.create(request);
+
+        //then
         assertAll(
-            () -> assertThat(actual.getId()).isNotNull(),
-            () -> assertThat(actual.getName()).isEqualTo(expected.getName())
+            () -> assertThat(result.getId()).isNotNull(),
+            () -> assertThat(result.getName()).isEqualTo(request.getName())
         );
     }
 
-    @DisplayName("메뉴 그룹의 이름이 올바르지 않으면 등록할 수 없다.")
-    @NullAndEmptySource
+    @DisplayName("모든 메뉴그룹의 정보를 조회할수 있다.")
     @ParameterizedTest
-    void create(final String name) {
-        final MenuGroup expected = createMenuGroupRequest(name);
-        assertThatThrownBy(() -> menuGroupService.create(expected))
-            .isInstanceOf(IllegalArgumentException.class);
+    @MethodSource("test3MethodSource")
+    void test3(List<String> names) {
+        //given
+        for (String name : names) {
+            MenuGroup request = new MenuGroup();
+            request.setName(name);
+            menuGroupService.create(request);
+        }
+
+        //when
+        List<MenuGroup> menuGroups = menuGroupService.findAll();
+
+        //then
+        assertAll(
+            () -> assertThat(menuGroups).extracting("name")
+                .containsExactlyInAnyOrderElementsOf(names),
+            () -> assertThat(menuGroups).extracting("id")
+                .doesNotContainNull()
+        );
+
     }
 
-    @DisplayName("메뉴 그룹의 목록을 조회할 수 있다.")
-    @Test
-    void findAll() {
-        menuGroupRepository.save(menuGroup("두마리메뉴"));
-        final List<MenuGroup> actual = menuGroupService.findAll();
-        assertThat(actual).hasSize(1);
-    }
-
-    private MenuGroup createMenuGroupRequest(final String name) {
-        final MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setName(name);
-        return menuGroup;
+    static Stream<List<String>> test3MethodSource() {
+        return Stream.of(
+            List.of("메뉴그룹1", "MenuGroup2", "메#뉴$그%룹^3"),
+            List.of(" 메 뉴 그 룹 1 ", " 메 뉴 그 룹 2", "메 뉴 그 룹 3")
+        );
     }
 }
