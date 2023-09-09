c4d20be06f55d4e425eeaa6c7364bd0e483a245b
ryuhistory <ryuhistory@gmail.com>
2023-09-09 15:08:13 +0900

    test : product name 검증 테스트 추가

--- src/test/java/kitchenpos/products/domain/tobe/domain/ToBeProductTest.java
+++ src/test/java/kitchenpos/products/domain/tobe/domain/ToBeProductTest.java
@@ -7,6 +7,8 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class ToBeProductTest {
 
@@ -17,6 +19,21 @@ class ToBeProductTest {
     void setUp() {
         price = BigDecimal.valueOf(30_000L);
         product = new ToBeProduct("돈가스", price, false);
+    }
+
+    @DisplayName("상품이름이 빈값이나거 null 이면 등록할 수 없다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void newProduct(String name) {
+        assertThatThrownBy(() -> new ToBeProduct(name, price, false))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품이름은 비속어를 가질 수 없다.")
+    @Test
+    void profanityName() {
+        assertThatThrownBy(() -> new ToBeProduct("김밥", price, true))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("가격을 변경한다")
