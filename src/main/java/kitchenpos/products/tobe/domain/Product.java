dce5d5a8b79b8ec7ca3452c354c2f3402e06550b
jordy-torvalds <jordy-torvalds@kakao.com>
2023-09-10 21:47:45 +0900

    feat: to-be entity 선언

---
+++ src/main/java/kitchenpos/products/tobe/domain/Product.java
@@ -0,0 +1,25 @@
+package kitchenpos.products.tobe.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Table(name = "product")
+@Entity
+public class Product {
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    public Product() {
+    }
+}
