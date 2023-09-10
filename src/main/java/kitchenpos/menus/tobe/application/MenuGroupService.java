66069690ab046ba205cf9524eb011be5c9ec3a04
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-09-10 12:13:57 +0900

    refactor: menu group service layer change to use dto

--- src/main/java/kitchenpos/menus/tobe/application/MenuGroupService.java
+++ src/main/java/kitchenpos/menus/tobe/application/MenuGroupService.java
@@ -2,10 +2,13 @@ package kitchenpos.menus.tobe.application;
 
 import kitchenpos.menus.tobe.domain.MenuGroup;
 import kitchenpos.menus.tobe.domain.MenuGroupRepository;
+import kitchenpos.menus.tobe.ui.MenuGroupRequest;
+import kitchenpos.menus.tobe.ui.MenuGroupResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MenuGroupService {
@@ -16,12 +19,15 @@ public class MenuGroupService {
     }
 
     @Transactional
-    public MenuGroup create(final MenuGroup request) {
-        return menuGroupRepository.save(MenuGroup.of(request));
+    public MenuGroupResponse create(final MenuGroupRequest request) {
+        MenuGroup menuGroup = menuGroupRepository.save(MenuGroup.of(request));
+        return new MenuGroupResponse(menuGroup.getId(), menuGroup.getName());
     }
 
     @Transactional(readOnly = true)
-    public List<MenuGroup> findAll() {
-        return menuGroupRepository.findAll();
+    public List<MenuGroupResponse> findAll() {
+        return menuGroupRepository.findAll().stream()
+                .map(it -> new MenuGroupResponse(it.getId(), it.getName()))
+                .collect(Collectors.toList());
     }
 }
