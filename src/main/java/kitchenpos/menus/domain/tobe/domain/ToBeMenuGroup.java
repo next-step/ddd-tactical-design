ca586305c3b4bf697ffd1402e37df14d82ff5dc5
ryuhistory <ryuhistory@gmail.com>
2023-09-10 16:35:59 +0900

    feat : tebeMenuGroup 구현

---
+++ src/main/java/kitchenpos/menus/domain/tobe/domain/ToBeMenuGroup.java
@@ -0,0 +1,31 @@
+package kitchenpos.menus.domain.tobe.domain;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "menu_group")
+@Entity
+public class ToBeMenuGroup {
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public ToBeMenuGroup() {
+    }
+
+    public ToBeMenuGroup(String name) {
+        if (Objects.isNull(name) || name.isEmpty()) {
+            throw new IllegalArgumentException("메뉴 그룹 이름이 입력되야 합니다.");
+        }
+        this.id = UUID.randomUUID();
+        this.name = name;
+    }
+}
