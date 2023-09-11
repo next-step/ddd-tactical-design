513c52f17cdede5bb4b3a3cf099d926462f5b380
jeunghoonsong <thdwmdgns@naver.com>
2023-09-11 12:38:10 +0900

    feat: Equals & Hashcode

--- src/main/java/kitchenpos/products/tobe/domain/Price.java
+++ src/main/java/kitchenpos/products/tobe/domain/Price.java
@@ -1,6 +1,7 @@
 package kitchenpos.products.tobe.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
@@ -26,5 +27,22 @@ public class Price {
 
     public BigDecimal getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Price)) {
+            return false;
+        }
+        Price price1 = (Price) o;
+        return Objects.equals(price, price1.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
     }
 }
