780cb287f340a69f531aca44e6d16345971756f6
namhyun.sung <namhyun.sung@yanolja.com>
2023-09-11 14:17:37 +0900

    refactor(DisplayedName) : DisplayedName 객체 생성 및 적용

---
+++ src/main/java/kitchenpos/products/tobe/domain/DisplayedName.java
@@ -0,0 +1,32 @@
+package kitchenpos.products.tobe.domain;
+
+import kitchenpos.products.tobe.infra.PurgomalumClient;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class DisplayedName {
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    protected DisplayedName() {
+    }
+
+    public DisplayedName(final String name, PurgomalumClient purgomalumClient) {
+        validate(name, purgomalumClient);
+        this.name = name;
+    }
+
+
+    private void validate(final String name, PurgomalumClient purgomalumClient) {
+        if (Objects.isNull(name) || purgomalumClient.containsProfanity(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
