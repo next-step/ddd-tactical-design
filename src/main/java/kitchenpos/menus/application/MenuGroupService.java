7b1a7a2a8bdd421095b3d45a5cc394dbd88fe772
junlee <laonzenamoon@gmail.com>
2023-09-11 20:07:45 +0900

    refactor(#step1): 메뉴 그룹 API Spec 용도 model 추가

--- src/main/java/kitchenpos/menus/application/MenuGroupService.java
+++ src/main/java/kitchenpos/menus/application/MenuGroupService.java
@@ -2,12 +2,15 @@ package kitchenpos.menus.application;
 
 import kitchenpos.menus.domain.MenuGroup;
 import kitchenpos.menus.domain.MenuGroupRepository;
+import kitchenpos.menus.dto.MenuGroupCreateRequest;
+import kitchenpos.menus.dto.MenuGroupDetailResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class MenuGroupService {
@@ -18,7 +21,7 @@ public class MenuGroupService {
     }
 
     @Transactional
-    public MenuGroup create(final MenuGroup request) {
+    public MenuGroupDetailResponse create(final MenuGroupCreateRequest request) {
         final String name = request.getName();
         if (Objects.isNull(name) || name.isEmpty()) {
             throw new IllegalArgumentException();
@@ -26,11 +29,18 @@ public class MenuGroupService {
         final MenuGroup menuGroup = new MenuGroup();
         menuGroup.setId(UUID.randomUUID());
         menuGroup.setName(name);
-        return menuGroupRepository.save(menuGroup);
+        return toMenuGroupDetailResponse(menuGroupRepository.save(menuGroup));
     }
 
     @Transactional(readOnly = true)
-    public List<MenuGroup> findAll() {
-        return menuGroupRepository.findAll();
+    public List<MenuGroupDetailResponse> findAll() {
+        return menuGroupRepository.findAll()
+                .stream()
+                .map(this::toMenuGroupDetailResponse)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private MenuGroupDetailResponse toMenuGroupDetailResponse(MenuGroup menuGroup) {
+        return new MenuGroupDetailResponse(menuGroup.getId(), menuGroup.getName());
     }
 }
