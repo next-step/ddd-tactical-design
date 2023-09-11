e4b7936c77b36a540f5d7f804802029846752b9b
namhyun.sung <namhyun.sung@yanolja.com>
2023-09-11 14:41:45 +0900

    refactor(ProductController, ProductService) : ProductCreateResponse 적용

--- src/main/java/kitchenpos/products/tobe/application/ProductService.java
+++ src/main/java/kitchenpos/products/tobe/application/ProductService.java
@@ -5,6 +5,7 @@ import kitchenpos.products.tobe.domain.Product;
 import kitchenpos.products.tobe.domain.ProductRepository;
 import kitchenpos.products.tobe.infra.PurgomalumClient;
 import kitchenpos.products.tobe.ui.dto.ProductCreateRequest;
+import kitchenpos.products.tobe.ui.dto.ProductCreateResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +26,44 @@ public class ProductService {
     }
 
     @Transactional
-    public Product create(final ProductCreateRequest request) {
+    public ProductCreateResponse create(final ProductCreateRequest request) {
         final Product product = new Product(request.getName(), purgomalumClient, request.getPrice());
-        return productRepository.save(product);
+        Product saveProduct = productRepository.save(product);
+        return ProductCreateResponse.of(
+                saveProduct.getId(),
+                saveProduct.getName().getName(),
+                saveProduct.getPrice().getPrice())
+                ;
     }
+//
+//    @Transactional
+//    public Product changePrice(final UUID productId, final Product request) {
+//        final BigDecimal price = request.getPrice();
+//        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+//            throw new IllegalArgumentException();
+//        }
+//        final Product product = productRepository.findById(productId)
+//                .orElseThrow(NoSuchElementException::new);
+//        product.setPrice(price);
+//        final List<Menu> menus = menuRepository.findAllByProductId(productId);
+//        for (final Menu menu : menus) {
+//            BigDecimal sum = BigDecimal.ZERO;
+//            for (final MenuProduct menuProduct : menu.getMenuProducts()) {
+//                sum = sum.add(
+//                        menuProduct.getProduct()
+//                                .getPrice()
+//                                .multiply(BigDecimal.valueOf(menuProduct.getQuantity()))
+//                );
+//            }
+//            if (menu.getPrice().compareTo(sum) > 0) {
+//                menu.setDisplayed(false);
+//            }
+//        }
+//        return product;
+//    }
+//
+//    @Transactional(readOnly = true)
+//    public List<Product> findAll() {
+//        return productRepository.findAll();
+//    }
 }
--- src/main/java/kitchenpos/products/tobe/ui/ProductRestController.java
+++ src/main/java/kitchenpos/products/tobe/ui/ProductRestController.java
@@ -1,8 +1,8 @@
 package kitchenpos.products.tobe.ui;
 
 import kitchenpos.products.tobe.application.ProductService;
-import kitchenpos.products.tobe.domain.Product;
 import kitchenpos.products.tobe.ui.dto.ProductCreateRequest;
+import kitchenpos.products.tobe.ui.dto.ProductCreateResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +21,19 @@ public class ProductRestController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@RequestBody final ProductCreateRequest request) {
-        final Product response = productService.create(request);
+    public ResponseEntity<ProductCreateResponse> create(@RequestBody final ProductCreateRequest request) {
+        final ProductCreateResponse response = productService.create(request);
         return ResponseEntity.created(URI.create("/api/products/" + response.getId()))
                 .body(response);
     }
+
+//    @PutMapping("/{productId}/price")
+//    public ResponseEntity<Product> changePrice(@PathVariable final UUID productId, @RequestBody final Product request) {
+//        return ResponseEntity.ok(productService.changePrice(productId, request));
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<Product>> findAll() {
+//        return ResponseEntity.ok(productService.findAll());
+//    }
 }
