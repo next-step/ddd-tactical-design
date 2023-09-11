4e145f5ff12c1b0e074461ab799ff9d3eafef528
namhyun.sung <namhyun.sung@yanolja.com>
2023-09-11 14:10:47 +0900

    refactor(price) : Price 객체 생성 및 적용

---
+++ src/main/java/kitchenpos/products/tobe/domain/Price.java
@@ -0,0 +1,30 @@
+package kitchenpos.products.tobe.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+public class Price {
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    protected Price() {
+    }
+
+    public Price(BigDecimal price) {
+        validate(price);
+        this.price = price;
+    }
+
+    private void validate(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
---
+++ src/test/java/kitchenpos/products/tobe/domain/PriceTest.java
@@ -0,0 +1,21 @@
+package kitchenpos.products.tobe.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PriceTest {
+    @DisplayName("상품의 가격이 올바르지 않으면 등록할 수 없다.")
+    @ValueSource(strings = "-1000")
+    @NullSource
+    @ParameterizedTest
+    void create(final BigDecimal price) {
+        assertThatThrownBy(() -> new Price(price))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
