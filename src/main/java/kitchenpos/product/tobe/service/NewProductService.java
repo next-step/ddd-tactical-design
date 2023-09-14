6a4d3fd861450ec0e31a7c2bd33cfa7a28ee4952
Soora Oh <soora.oh@betalabs.kr>
2023-09-14 17:09:44 +0900

    feat: 가격 변경, 상품 생성 Usecase 기능 개발

--- src/main/java/kitchenpos/product/tobe/service/NewProductService.java
+++ src/main/java/kitchenpos/product/tobe/service/NewProductService.java
@@ -3,79 +3,81 @@ package kitchenpos.product.tobe.service;
 import kitchenpos.menu.domain.Menu;
 import kitchenpos.menu.domain.MenuProduct;
 import kitchenpos.menu.domain.MenuRepository;
+import kitchenpos.product.tobe.domain.port.inp.NewProductCreator;
+import kitchenpos.product.tobe.domain.port.inp.NewProductPriceChanger;
+import kitchenpos.product.tobe.domain.port.inp.NewProductPriceChangerCommand;
+import kitchenpos.product.tobe.domain.port.inp.NewProductCreatorCommand;
 import kitchenpos.product.tobe.domain.NewProduct;
-import kitchenpos.product.tobe.domain.NewProductRepository;
-import kitchenpos.profanity.domain.PurgomalumClient;
+import kitchenpos.product.tobe.domain.port.outp.NewProductRepository;
+import kitchenpos.profanity.domain.PurgomalumChecker;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.UUID;
 
 @Service
-public class NewProductService {
+@Transactional
+public class NewProductService implements NewProductCreator, NewProductPriceChanger {
     private final NewProductRepository newProductRepository;
     private final MenuRepository menuRepository;
-    private final PurgomalumClient purgomalumClient;
+    private final PurgomalumChecker purgomalumChecker;
 
     public NewProductService(
-        final NewProductRepository newProductRepository,
-        final MenuRepository menuRepository,
-        final PurgomalumClient purgomalumClient
+            final NewProductRepository newProductRepository,
+            final MenuRepository menuRepository,
+            final PurgomalumChecker purgomalumChecker
     ) {
         this.newProductRepository = newProductRepository;
         this.menuRepository = menuRepository;
-        this.purgomalumClient = purgomalumClient;
+        this.purgomalumChecker = purgomalumChecker;
     }
 
-    @Transactional
-    public NewProduct create(final NewProduct request) {
-        final BigDecimal price = request.getPrice();
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException();
-        }
-        final String name = request.getName();
-        if (Objects.isNull(name) || purgomalumClient.containsProfanity(name)) {
-            throw new IllegalArgumentException();
-        }
-        final NewProduct product = new NewProduct();
-        product.setId(UUID.randomUUID());
-        product.setName(name);
-        product.setPrice(price);
+    @Override
+    public NewProduct create(final NewProductCreatorCommand command) {
+        final NewProduct product = NewProduct.createWithoutProfanity(command.getName(), command.getPrice(), purgomalumChecker);
+
         return newProductRepository.save(product);
     }
 
-    @Transactional
-    public NewProduct changePrice(final UUID productId, final NewProduct request) {
-        final BigDecimal price = request.getPrice();
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException();
-        }
-        final NewProduct product = newProductRepository.findById(productId)
-            .orElseThrow(NoSuchElementException::new);
-        product.setPrice(price);
-        final List<Menu> menus = menuRepository.findAllByProductId(productId);
-        for (final Menu menu : menus) {
-            BigDecimal sum = BigDecimal.ZERO;
-            for (final MenuProduct menuProduct : menu.getMenuProducts()) {
-                sum = sum.add(
-                    menuProduct.getProduct()
-                        .getPrice()
-                        .multiply(BigDecimal.valueOf(menuProduct.getQuantity()))
-                );
-            }
-            if (menu.getPrice().compareTo(sum) > 0) {
-                menu.setDisplayed(false);
-            }
-        }
+    @Override
+    public NewProduct change(final NewProductPriceChangerCommand command) {
+        final NewProduct product = newProductRepository.findById(command.getProductId())
+                .orElseThrow(NoSuchElementException::new);
+
+        product.changePrice(command.getPrice());
+
+        /** 해당 관련 코드는 추후 MenuService로 이동 **/
+        applyDisplayable(command.getProductId());
+
         return product;
     }
 
-    @Transactional(readOnly = true)
-    public List<NewProduct> findAll() {
-        return newProductRepository.findAll();
+    private void applyDisplayable(final UUID productId) {
+        final List<Menu> menus = menuRepository.findAllByProductId(productId);
+
+        for (final Menu menu : menus) {
+            if (isNotDisplayable(menu)) {
+                menu.setDisplayed(false);
+            }
+        }
+    }
+
+    private boolean isNotDisplayable(final Menu menu) {
+        BigDecimal sum = sumOfPrice(menu.getMenuProducts());
+
+        return menu.getPrice().compareTo(sum) > 0;
+    }
+
+    private BigDecimal sumOfPrice(final List<MenuProduct> menuProducts) {
+        return menuProducts.stream()
+                .map(menuProduct ->
+                        menuProduct.getProduct()
+                                .getPrice()
+                                .multiply(BigDecimal.valueOf(menuProduct.getQuantity()))
+                )
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
---
+++ src/test/java/kitchenpos/product/tobe/domain/port/inp/NewProductCreatorTest.java
@@ -0,0 +1,59 @@
+package kitchenpos.product.tobe.domain.port.inp;
+
+import kitchenpos.product.tobe.domain.*;
+import kitchenpos.product.tobe.domain.port.outp.NewProductRepository;
+import kitchenpos.profanity.domain.PurgomalumChecker;
+import kitchenpos.support.BaseServiceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
+
+import static kitchenpos.fixture.ProductFixture.createProduct;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+public class NewProductCreatorTest extends BaseServiceTest {
+    private final NewProductCreator newProductCreator;
+    private final NewProductRepository newProductRepository;
+
+    @MockBean
+    private PurgomalumChecker purgomalumChecker;
+
+    public NewProductCreatorTest(final NewProductCreator newProductCreator, final NewProductRepository newProductRepository) {
+        this.newProductCreator = newProductCreator;
+        this.newProductRepository = newProductRepository;
+    }
+
+    @DisplayName("상품은 등록이 가능하다")
+    @Test
+    void test1() {
+        final NewProductCreatorCommand command = new NewProductCreatorCommand(new Name("product"), new Price(BigDecimal.valueOf(1000)));
+        given(purgomalumChecker.containsProfanity(command.getName().getValue())).willReturn(false);
+
+        final NewProduct createdNewProduct = newProductCreator.create(command);
+
+        final NewProduct foundNewProduct = newProductRepository.findAll().get(0);
+
+        assertAll(
+                () -> assertThat(createdNewProduct.getId()).isNotNull(),
+                () -> assertThat(createdNewProduct.getName()).isEqualTo(command.getName()),
+                () -> assertThat(createdNewProduct.getPrice()).isEqualTo(command.getPrice()),
+                () -> assertThat(foundNewProduct.getId()).isEqualTo(createdNewProduct.getId())
+        );
+    }
+
+    @DisplayName("상품의 이름에 비속어가 포함되면 안된다")
+    @Test
+    void test3() {
+        final NewProductCreatorCommand command = new NewProductCreatorCommand(new Name("비속어"), new Price(BigDecimal.valueOf(1000)));
+
+        when(purgomalumChecker.containsProfanity(command.getName().getValue())).thenReturn(true);
+
+        assertThatExceptionOfType(IllegalNewProductNameException.class)
+                .isThrownBy(() -> newProductCreator.create(command));
+    }
+}
