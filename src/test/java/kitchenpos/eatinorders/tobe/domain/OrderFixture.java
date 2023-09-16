1c0a4bb445b7a96aca90a656637384479a6f8808
jeunghoonsong <thdwmdgns@naver.com>
2023-09-16 20:32:36 +0900

    test(refactor): OrderFixture를 통한 생성 로직 분리

---
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/OrderFixture.java
@@ -0,0 +1,58 @@
+package kitchenpos.eatinorders.tobe.domain;
+
+import java.util.List;
+import java.util.UUID;
+
+public class OrderFixture {
+
+    public static Order order() {
+        return new Order(occupiedOrderTable(), orderLineItems());
+    }
+
+    public static Order order(OrderTable orderTable) {
+        return new Order(orderTable, orderLineItems());
+    }
+
+    public static Order order(OrderLineItems orderLineItems) {
+        return new Order(occupiedOrderTable(), orderLineItems);
+    }
+
+    public static Order order(OrderTable orderTable, OrderLineItems orderLineItems) {
+        return new Order(orderTable, orderLineItems);
+    }
+
+    public static Order acceptedOrder() {
+        Order order = OrderFixture.order();
+        order.accept();
+        return order;
+    }
+
+    public static Order servedOrder() {
+        Order order = acceptedOrder();
+        order.serve();
+        return order;
+    }
+
+    public static Order completedOrder() {
+        Order order = servedOrder();
+        order.complete();
+
+        return order;
+    }
+
+    public static OrderTable occupiedOrderTable() {
+        OrderTable orderTable = new OrderTable(new OrderTableName("name"));
+        orderTable.occupy();
+
+        return orderTable;
+    }
+
+    public static OrderLineItems orderLineItems() {
+        OrderLineItem orderLineItem1 = new kitchenpos.eatinorders.tobe.domain.OrderLineItem(UUID.randomUUID(),
+            new OrderLineItemQuantity(1));
+        OrderLineItem orderLineItem2 = new kitchenpos.eatinorders.tobe.domain.OrderLineItem(UUID.randomUUID(),
+            new OrderLineItemQuantity(1));
+
+        return new OrderLineItems(List.of(orderLineItem1, orderLineItem2));
+    }
+}
--- src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTest.java
@@ -1,69 +1,30 @@
 package kitchenpos.eatinorders.tobe.domain;
 
+import static kitchenpos.eatinorders.tobe.domain.OrderFixture.acceptedOrder;
+import static kitchenpos.eatinorders.tobe.domain.OrderFixture.completedOrder;
+import static kitchenpos.eatinorders.tobe.domain.OrderFixture.order;
+import static kitchenpos.eatinorders.tobe.domain.OrderFixture.servedOrder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
-import java.util.UUID;
 import kitchenpos.eatinorders.tobe.domain.Order.OrderStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrderTest {
 
-    private OrderTable orderTable;
-    private Order waitingOrder;
-    private Order acceptedOrder;
-    private Order servedOrder;
-    private Order completedOrder;
-
-    @BeforeEach
-    public void init() {
-        this.orderTable = new OrderTable(new OrderTableName("name"));
-        waitingOrder = createOrder();
-
-        acceptedOrder = createOrder();
-        acceptedOrder.accept();
-
-        servedOrder = createOrder();
-        servedOrder.accept();
-        servedOrder.serve();
-
-        completedOrder = createOrder();
-        completedOrder.accept();
-        completedOrder.serve();
-        completedOrder.complete();
-    }
-
-    private Order createOrder() {
-        OrderLineItem orderLineItem1 = new OrderLineItem(UUID.randomUUID(), new OrderLineItemQuantity(1));
-        OrderLineItems orderLineItems = new OrderLineItems(List.of(orderLineItem1));
-        OrderTable orderTable = new OrderTable(new OrderTableName("name"));
-        orderTable.occupy();
-
-        return new Order(orderTable, orderLineItems);
-    }
-
     @DisplayName("주문을 생성할수 있다")
     @Test
     void test1() {
-        //given
-        orderTable.occupy();
-        OrderLineItem orderLineItem1 = new OrderLineItem(UUID.randomUUID(), new OrderLineItemQuantity(1));
-        OrderLineItem orderLineItem2 = new OrderLineItem(UUID.randomUUID(), new OrderLineItemQuantity(1));
-        OrderLineItems orderLineItems = new OrderLineItems(List.of(orderLineItem1, orderLineItem2));
-
-        //when
-        Order order = new Order(orderTable, orderLineItems);
+        Order order = OrderFixture.order();
 
         //then
         assertAll(
             () -> assertThat(order.getId()).isNotNull(),
             () -> assertThat(order.getStatus()).isEqualTo(OrderStatus.WAITING),
             () -> assertThat(order.getOrderDateTime()).isNotNull(),
-            () -> assertThat(order.getOrderTable()).isEqualTo(orderTable),
             () -> assertThat(order.getOrderLineItems())
                 .extracting("order")
                 .containsExactlyInAnyOrder(order, order)
@@ -75,13 +36,10 @@ public class OrderTest {
     @Test
     void test2() {
         //given
-        OrderLineItem orderLineItem1 = new OrderLineItem(UUID.randomUUID(), new OrderLineItemQuantity(1));
-        OrderLineItem orderLineItem2 = new OrderLineItem(UUID.randomUUID(), new OrderLineItemQuantity(1));
-        OrderLineItems orderLineItems = new OrderLineItems(List.of(orderLineItem1, orderLineItem2));
-
+        OrderTable vacantTable = new OrderTable(new OrderTableName("name"));
         //when && then
         assertThatThrownBy(
-            () -> new Order(orderTable, orderLineItems)
+            () -> order(vacantTable)
         ).isInstanceOf(IllegalStateException.class)
             .hasMessage("주문테이블이 이용중이 아닙니다");
     }
@@ -90,12 +48,11 @@ public class OrderTest {
     @Test
     void test3() {
         //given
-        orderTable.occupy();
         OrderLineItems orderLineItems = new OrderLineItems(List.of());
 
         //when && then
         assertThatThrownBy(
-            () -> new Order(orderTable, orderLineItems)
+            () -> order(orderLineItems)
         ).isInstanceOf(IllegalArgumentException.class)
             .hasMessage("주문내역이 하나 이상 있어야 합니다");
     }
@@ -105,15 +62,15 @@ public class OrderTest {
     void test4() {
         assertAll(
             () -> assertThatThrownBy(
-                () -> acceptedOrder.accept()
+                () -> acceptedOrder().accept()
             ).isInstanceOf(IllegalStateException.class)
                 .hasMessage("접수 대기 중인 주문만 수락할수 있습니다"),
             () -> assertThatThrownBy(
-                () -> servedOrder.accept()
+                () -> servedOrder().accept()
             ).isInstanceOf(IllegalStateException.class)
                 .hasMessage("접수 대기 중인 주문만 수락할수 있습니다"),
             () -> assertThatThrownBy(
-                () -> completedOrder.accept()
+                () -> completedOrder().accept()
             ).isInstanceOf(IllegalStateException.class)
                 .hasMessage("접수 대기 중인 주문만 수락할수 있습니다")
         );
@@ -124,15 +81,11 @@ public class OrderTest {
     void test5() {
         assertAll(
             () -> assertThatThrownBy(
-                () -> waitingOrder.serve()
+                () -> servedOrder().serve()
             ).isInstanceOf(IllegalStateException.class)
                 .hasMessage("수락된 주문만 전달할수 있습니다"),
             () -> assertThatThrownBy(
-                () -> servedOrder.serve()
-            ).isInstanceOf(IllegalStateException.class)
-                .hasMessage("수락된 주문만 전달할수 있습니다"),
-            () -> assertThatThrownBy(
-                () -> completedOrder.serve()
+                () -> completedOrder().serve()
             ).isInstanceOf(IllegalStateException.class)
                 .hasMessage("수락된 주문만 전달할수 있습니다")
         );
@@ -143,15 +96,11 @@ public class OrderTest {
     void test6() {
         assertAll(
             () -> assertThatThrownBy(
-                () -> waitingOrder.complete()
+                () -> acceptedOrder().complete()
             ).isInstanceOf(IllegalStateException.class)
                 .hasMessage("전달된 주문만 완료할수 있습니다"),
             () -> assertThatThrownBy(
-                () -> acceptedOrder.complete()
-            ).isInstanceOf(IllegalStateException.class)
-                .hasMessage("전달된 주문만 완료할수 있습니다"),
-            () -> assertThatThrownBy(
-                () -> completedOrder.complete()
+                () -> completedOrder().complete()
             ).isInstanceOf(IllegalStateException.class)
                 .hasMessage("전달된 주문만 완료할수 있습니다")
         );
