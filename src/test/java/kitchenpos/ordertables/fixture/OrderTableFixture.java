0f618d53fa4ee98debb898d8d040fbc325ed2443
hiblue02 <hiblue02@gmail.com>
2023-09-16 10:38:03 +0900

    feat(주문, 주문테이블): 테스트 픽스처 분리

---
+++ src/test/java/kitchenpos/ordertables/fixture/OrderTableFixture.java
@@ -0,0 +1,17 @@
+package kitchenpos.ordertables.fixture;
+
+import kitchenpos.ordertables.domain.NumberOfGuest;
+import kitchenpos.ordertables.domain.OrderTable;
+import kitchenpos.ordertables.domain.OrderTableName;
+
+public class OrderTableFixture {
+    public static OrderTable orderTable() {
+        return orderTable(false, 0);
+    }
+
+    public static OrderTable orderTable(final boolean occupied, final int numberOfGuests) {
+        return new OrderTable(new OrderTableName("1번"),
+                new NumberOfGuest(numberOfGuests),
+                occupied);
+    }
+}
