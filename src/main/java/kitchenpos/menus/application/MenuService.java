5316f2f803d4acb80e2df5597d912f7443aa2567
namhyun.sung <namhyun.sung@yanolja.com>
2023-09-17 14:34:16 +0900

    refactor(menuService) : ProductRepository 주입 제거

--- src/main/java/kitchenpos/menus/application/MenuService.java
+++ src/main/java/kitchenpos/menus/application/MenuService.java
@@ -1,18 +1,14 @@
 package kitchenpos.menus.application;
 
 import kitchenpos.menus.domain.Menu;
-import kitchenpos.menus.domain.MenuProduct;
 import kitchenpos.menus.domain.MenuRepository;
 import kitchenpos.menus.ui.dto.request.MenuChangePriceRequest;
 import kitchenpos.menus.ui.dto.request.MenuCreateRequest;
 import kitchenpos.menus.ui.dto.response.MenuResponse;
-import kitchenpos.products.domain.Product;
-import kitchenpos.products.domain.ProductRepository;
 import kitchenpos.products.domain.PurgomalumClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -20,18 +16,15 @@ import java.util.UUID;
 @Service
 public class MenuService {
     private final MenuRepository menuRepository;
-    private final ProductRepository productRepository;
     private final PurgomalumClient purgomalumClient;
     private final MenuValidator validator;
 
     public MenuService(
             final MenuRepository menuRepository,
-            final ProductRepository productRepository,
             final PurgomalumClient purgomalumClient,
             final MenuValidator validator
     ) {
         this.menuRepository = menuRepository;
-        this.productRepository = productRepository;
         this.purgomalumClient = purgomalumClient;
         this.validator = validator;
     }
@@ -59,15 +52,7 @@ public class MenuService {
     public MenuResponse display(final UUID menuId) {
         final Menu menu = menuRepository.findById(menuId)
                 .orElseThrow(NoSuchElementException::new);
-        BigDecimal sum = BigDecimal.ZERO;
-        for (final MenuProduct menuProduct : menu.getMenuProducts()) {
-            final Product product = productRepository.findById(menuProduct.getProductId())
-                    .orElseThrow(NoSuchElementException::new);
-            sum = sum.add(product.multiplyPrice(menuProduct.getQuantity()).getValue());
-        }
-        if (menu.getPrice().compareTo(sum) > 0) {
-            throw new IllegalStateException();
-        }
+        validator.validateChangePrice(menu.getMenuProducts(), menu.getPrice());
         menu.show();
         return MenuResponse.from(menu);
     }
