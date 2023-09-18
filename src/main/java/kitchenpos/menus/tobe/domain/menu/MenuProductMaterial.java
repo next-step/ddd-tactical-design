b85b7ccca8def8f6c8888cf08564186cf36af1ca
mypark <mypark@miridih.com>
2023-09-19 08:41:54 +0900

    refactor(step2): MenuProductMaterial에 equals and hashCode 추가

--- src/main/java/kitchenpos/menus/tobe/domain/menu/MenuProductMaterial.java
+++ src/main/java/kitchenpos/menus/tobe/domain/menu/MenuProductMaterial.java
@@ -1,5 +1,6 @@
 package kitchenpos.menus.tobe.domain.menu;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class MenuProductMaterial {
@@ -17,5 +18,18 @@ public class MenuProductMaterial {
 
     public long getQuantity() {
         return this.quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuProductMaterial that = (MenuProductMaterial) o;
+        return quantity == that.quantity && Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, quantity);
     }
 }
