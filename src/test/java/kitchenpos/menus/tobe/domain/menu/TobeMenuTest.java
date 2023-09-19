1e727da5607b95aee0b9ffdaf1658d2bf466d6f2
saerang <lsg10119@gamail.com>
2023-09-19 20:14:26 +0900

    test(menu): 메뉴 테스트코드 추가

---
+++ src/test/java/kitchenpos/menus/tobe/domain/menu/TobeMenuTest.java
@@ -0,0 +1,40 @@
+package kitchenpos.menus.tobe.domain.menu;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static kitchenpos.TobeFixtures.menu;
+import static kitchenpos.TobeFixtures.menuProduct;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TobeMenuTest {
+
+    @DisplayName("메뉴 노출")
+    @Test
+    void display() {
+        TobeMenu menu = menu(19_000L, false, menuProduct());
+        menu.display();
+
+        assertThat(menu.isDisplayed()).isTrue();
+    }
+
+    @DisplayName("메뉴 숨김")
+    @Test
+    void hide() {
+        TobeMenu menu = menu(19_000L, false, menuProduct());
+        menu.hide();
+
+        assertThat(menu.isDisplayed()).isFalse();
+    }
+
+    @DisplayName("메뉴 숨김")
+    @Test
+    void updatePrice() {
+        TobeMenu menu = menu(19_000L, false, menuProduct());
+        menu.updatePrice(new MenuPrice(BigDecimal.valueOf(20_000L)));
+
+        assertThat(menu.isDisplayed()).isFalse();
+    }
+}
