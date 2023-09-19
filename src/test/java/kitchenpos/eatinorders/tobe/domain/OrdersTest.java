7e6a7663aa586f2e09f4af832271d68aad656c31
jeunghoonsong <thdwmdgns@naver.com>
2023-09-19 11:54:08 +0900

    test: Orders의 불변리스트에 대한 테스트 추가

--- src/test/java/kitchenpos/eatinorders/tobe/domain/OrdersTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/OrdersTest.java
@@ -3,6 +3,7 @@ package kitchenpos.eatinorders.tobe.domain;
 import static kitchenpos.eatinorders.tobe.domain.OrderFixture.completedOrder;
 import static kitchenpos.eatinorders.tobe.domain.OrderFixture.servedOrder;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
@@ -30,4 +31,26 @@ class OrdersTest {
         );
     }
 
+    @DisplayName("전체 주문 리스트는 불변리스트로 반환된다")
+    @Test
+    void test2() {
+        //given
+        Order order1 = completedOrder();
+        Order order2 = completedOrder();
+
+        //when
+        Orders orders = new Orders(List.of(order1));
+        List<Order> ordersList = orders.getOrders();
+
+        //then
+        assertAll(
+            () -> assertThatThrownBy(
+                () -> ordersList.remove(0)
+            ).isInstanceOf(UnsupportedOperationException.class),
+            () -> assertThatThrownBy(
+                () -> ordersList.add(order2)
+            ).isInstanceOf(UnsupportedOperationException.class)
+        );
+
+    }
 }
