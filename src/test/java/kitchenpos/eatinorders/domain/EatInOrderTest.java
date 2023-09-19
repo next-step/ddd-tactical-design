f39757f8ec1b2cdfcb7c1d9b904c72aeff8799ae
hiblue02 <hiblue02@gmail.com>
2023-09-20 00:45:52 +0900

    test(매장 주문): 매장 주문 생성 private 메소드 생성

--- src/test/java/kitchenpos/eatinorders/domain/EatInOrderTest.java
+++ src/test/java/kitchenpos/eatinorders/domain/EatInOrderTest.java
@@ -22,11 +22,8 @@ class EatInOrderTest {
     @DisplayName("매장 주문 생성 ")
     @Test
     void create() {
-        EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
-        OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
-        EatInOrder response = new EatInOrder(eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
-
-        assertThat(response.getStatus().isWaiting()).isTrue();
+        EatInOrder eatInOrder = createEatInOrder(EatInOrderStatus.WAITING);
+        assertThat(eatInOrder.getStatus().isWaiting()).isTrue();
     }
 
     @DisplayName("매장 주문 접수")
@@ -35,24 +32,20 @@ class EatInOrderTest {
         @DisplayName("[성공] 대기 상태인 매장 주문만 접수할 수 있다. ")
         @Test
         void accept1() {
-            EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
-            OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
-            EatInOrder response = new EatInOrder(EatInOrderStatus.WAITING, eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
+            EatInOrder eatInOrder = createEatInOrder(EatInOrderStatus.WAITING);
 
-            response.accept();
+            eatInOrder.accept();
 
-            assertThat(response.getStatus().isAccepted()).isTrue();
+            assertThat(eatInOrder.getStatus().isAccepted()).isTrue();
         }
 
         @DisplayName("[실패] 매장 주문이 대기 상태가 아니면 접수할 수 없다.")
         @EnumSource(value = EatInOrderStatus.class, names = "WAITING", mode = EnumSource.Mode.EXCLUDE)
         @ParameterizedTest
         void accept2(final EatInOrderStatus status) {
-            EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
-            OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
-            EatInOrder response = new EatInOrder(status, eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
+            EatInOrder eatInOrder = createEatInOrder(status);
 
-            assertThatThrownBy(response::accept)
+            assertThatThrownBy(eatInOrder::accept)
                     .isInstanceOf(EatInOrderException.class)
                     .hasMessage(EatInOrderErrorCode.IS_NOT_WAITING.getMessage());
         }
@@ -65,24 +58,20 @@ class EatInOrderTest {
         @DisplayName("[성공] 접수된 매장 주문만 서빙될 수 있다. ")
         @Test
         void serve1() {
-            EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
-            OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
-            EatInOrder response = new EatInOrder(EatInOrderStatus.ACCEPTED, eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
+            EatInOrder eatInOrder = createEatInOrder(EatInOrderStatus.ACCEPTED);
 
-            response.serve();
+            eatInOrder.serve();
 
-            assertThat(response.getStatus().isServed()).isTrue();
+            assertThat(eatInOrder.getStatus().isServed()).isTrue();
         }
 
         @DisplayName("[실패] 접수된 매장 주문만 서빙될 수 있다. ")
         @EnumSource(value = EatInOrderStatus.class, names = "ACCEPTED", mode = EnumSource.Mode.EXCLUDE)
         @ParameterizedTest
         void serve2(final EatInOrderStatus status) {
-            EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
-            OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
-            EatInOrder response = new EatInOrder(status, eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
+            EatInOrder eatInOrder = createEatInOrder(status);
 
-            assertThatThrownBy(response::serve)
+            assertThatThrownBy(eatInOrder::serve)
                     .isInstanceOf(EatInOrderException.class)
                     .hasMessage(EatInOrderErrorCode.IS_NOT_ACCEPTED.getMessage());
         }
@@ -95,27 +84,29 @@ class EatInOrderTest {
         @DisplayName("[성공] 서빙된 매장 주문만 완료될 수 있다. ")
         @Test
         void complete1() {
-            EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
-            OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
-            EatInOrder response = new EatInOrder(EatInOrderStatus.SERVED, eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
+            EatInOrder eatInOrder = createEatInOrder(EatInOrderStatus.SERVED);
 
-            response.complete();
+            eatInOrder.complete();
 
-            assertThat(response.getStatus().isCompleted()).isTrue();
+            assertThat(eatInOrder.getStatus().isCompleted()).isTrue();
         }
 
         @DisplayName("[실패] 서빙된 매장 주문만 완료될 수 있다. ")
         @EnumSource(value = EatInOrderStatus.class, names = "SERVED", mode = EnumSource.Mode.EXCLUDE)
         @ParameterizedTest
         void complete2(final EatInOrderStatus status) {
-            EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
-            OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
-            EatInOrder response = new EatInOrder(status, eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
+            EatInOrder eatInOrder = createEatInOrder(status);
 
-            assertThatThrownBy(response::complete)
+            assertThatThrownBy(eatInOrder::complete)
                     .isInstanceOf(EatInOrderException.class)
                     .hasMessage(EatInOrderErrorCode.IS_NOT_SERVED.getMessage());
         }
 
+    }
+
+    private EatInOrder createEatInOrder(EatInOrderStatus status) {
+        EatInOrderLineItems eatInOrderLineItems = orderLineItems(orderLineItem());
+        OrderTable orderTable = OrderTableFixture.orderTable(true, 4);
+        return new EatInOrder(status, eatInOrderLineItems, new OrderTableId(orderTable.getIdValue()));
     }
 }
