406cb7c75b8617448a8e5925a613184f23672a35
saerang <lsg10119@gamail.com>
2023-09-21 09:42:20 +0900

    test(menu): 테스트코드 추가

--- src/test/java/kitchenpos/menus/tobe/domain/menu/TobeMenuTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/menu/TobeMenuTest.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
 import static kitchenpos.TobeFixtures.menu;
 import static kitchenpos.TobeFixtures.menuProduct;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TobeMenuTest {
 
@@ -36,5 +37,14 @@ class TobeMenuTest {
         menu.updatePrice(new MenuPrice(BigDecimal.valueOf(20_000L)));
 
         assertThat(menu.getPrice()).isEqualTo(new MenuPrice(BigDecimal.valueOf(20_000L)));
+    }
+
+    @DisplayName("가격 변경 시 상품 금액의 합보다 메뉴의 가격이 작을 수 없다.")
+    @Test
+    void updatePrice01() {
+        TobeMenu menu = menu(19_000L, false, menuProduct());
+        assertThatThrownBy(() -> menu.updatePrice(new MenuPrice(BigDecimal.valueOf(65_000L))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("메뉴에 속한 상품 금액의 합은 메뉴의 가격보다 크거나 같아야 합니다.");
     }
 }
