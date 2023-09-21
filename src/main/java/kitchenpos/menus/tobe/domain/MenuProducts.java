6628852c86ed489b78cc5d44e6842a687086394c
soosue <yunsoo0510@daum.net>
2023-09-21 17:05:10 +0900

    feat: menus: MenuProducts 구현

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProducts.java
@@ -0,0 +1,28 @@
+package kitchenpos.menus.tobe.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class MenuProducts {
+    private final List<MenuProduct> menuProducts;
+
+    public MenuProducts(List<MenuProduct> menuProducts) {
+        if (menuProducts.size() < 1) {
+            throw new IllegalArgumentException(String.format("메뉴에 속한 상품은 1개 이상이어야 합니다. 현재 값: %s", menuProducts.size()));
+        }
+        this.menuProducts = menuProducts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuProducts that = (MenuProducts) o;
+        return Objects.equals(menuProducts, that.menuProducts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuProducts);
+    }
+}
---
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuProductsTest.java
@@ -0,0 +1,51 @@
+package kitchenpos.menus.tobe.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class MenuProductsTest {
+    @Test
+    void MenuProducts는_MenuProduct를_1개_이상_갖는다() {
+        assertDoesNotThrow(() -> new MenuProducts(List.of(new MenuProduct(UUID.randomUUID(), new Quantity(0)))));
+    }
+
+    @Test
+    void MenuProducts는_MenuProduct가_0개일_수_없다() {
+        assertThatThrownBy(() -> new MenuProducts(List.of()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("메뉴에 속한 상품은 1개 이상이어야 합니다. 현재 값: 0");
+    }
+
+    @Test
+    void MenuProducts_동등성_비교() {
+        List<MenuProduct> menuProducts = List.of(new MenuProduct(UUID.randomUUID(), new Quantity(0)));
+        List<MenuProduct> otherMenuProducts = List.of(new MenuProduct(UUID.randomUUID(), new Quantity(0)));
+
+        MenuProducts actual = new MenuProducts(menuProducts);
+
+        assertThat(actual.equals(actual)).isTrue();
+
+        assertThat(actual.equals(null)).isFalse();
+        assertThat(actual.equals(5)).isFalse();
+
+        assertThat(actual.equals(new MenuProducts(menuProducts))).isTrue();
+        assertThat(actual.equals(new MenuProducts(otherMenuProducts))).isFalse();
+    }
+
+    @Test
+    void MenuProducts_hashCode() {
+        List<MenuProduct> menuProducts = List.of(new MenuProduct(UUID.randomUUID(), new Quantity(0)));
+        List<MenuProduct> otherMenuProducts = List.of(new MenuProduct(UUID.randomUUID(), new Quantity(0)));
+        
+        MenuProducts actual = new MenuProducts(menuProducts);
+
+        assertThat(actual.hashCode()).isEqualTo(new MenuProducts(menuProducts).hashCode());
+        assertThat(actual.hashCode()).isNotEqualTo(new MenuProducts(otherMenuProducts).hashCode());
+    }
+}
