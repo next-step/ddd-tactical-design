2fdb127e731ab2a7f8cc51a2ef5bdb19eea51ece
soosue <yunsoo0510@daum.net>
2023-09-21 16:02:11 +0900

    feat: menus: DisplayedName 구현

---
+++ src/main/java/kitchenpos/menus/tobe/domain/DisplayedName.java
@@ -0,0 +1,30 @@
+package kitchenpos.menus.tobe.domain;
+
+import java.util.Objects;
+
+public class DisplayedName {
+    private final String name;
+    public DisplayedName(String name, PurgomalumClient purgomalumClient) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException(String.format("이름은 없거나 빈 값일 수 없습니다. 현재 값: %s", name));
+        }
+        if (purgomalumClient.containsProfanity(name)) {
+            throw new IllegalArgumentException(String.format("이름은 욕설을 포함할 수 없습니다. 현재 값: %s", name));
+        }
+
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DisplayedName that = (DisplayedName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/main/java/kitchenpos/menus/tobe/domain/PurgomalumClient.java
@@ -0,0 +1,5 @@
+package kitchenpos.menus.tobe.domain;
+
+public interface PurgomalumClient {
+    boolean containsProfanity(String text);
+}
