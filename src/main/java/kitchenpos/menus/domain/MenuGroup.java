dd6727d5d54a3409567ca75cf34ed5f3aff77bef
soo <yunsoo0510@daum.net>
2023-09-22 20:12:46 +0900

    refactor: MenuFixture 수정

--- src/main/java/kitchenpos/menus/domain/MenuGroup.java
+++ src/main/java/kitchenpos/menus/domain/MenuGroup.java
@@ -32,6 +32,10 @@ public class MenuGroup {
         return id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/kitchenpos/menus/service/MenuFixture.java
+++ src/test/java/kitchenpos/menus/service/MenuFixture.java
@@ -1,22 +1,32 @@
 package kitchenpos.menus.service;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+import kitchenpos.menus.domain.DisplayedName;
 import kitchenpos.menus.domain.Menu;
 import kitchenpos.menus.domain.MenuGroup;
+import kitchenpos.menus.domain.MenuPricePolicy;
 import kitchenpos.menus.domain.MenuProduct;
+import kitchenpos.menus.domain.MenuProducts;
+import kitchenpos.menus.domain.Price;
+import kitchenpos.menus.tobe.domain.FakePurgomalumClient;
 import kitchenpos.products.domain.Product;
 
 public class MenuFixture {
-    private final Menu menu;
+    private UUID id;
+    private String name;
+    private long price;
+    private boolean displayed;
+    private UUID menuGroupId;
+    private String menuGroupName;
+    private List<MenuProduct> menuProducts;
 
     public MenuFixture() {
-        menu = new Menu();
-        menu.setId(UUID.randomUUID());
-        menu.setName("내일의 치킨");
-        menu.setPrice(new BigDecimal(15000));
+        id = UUID.randomUUID();
+        name = "내일의 치킨";
+        price = 15000L;
+        menuGroupId = null;
     }
 
     public static MenuFixture builder() {
@@ -36,43 +46,48 @@ public class MenuFixture {
     }
 
     public MenuFixture name(String name) {
-        menu.setName(name);
-        return this;
-    }
-
-    public MenuFixture price(BigDecimal price) {
-        menu.setPrice(price);
+        this.name = name;
         return this;
     }
 
     public MenuFixture price(long price) {
-        return price(new BigDecimal(price));
+        this.price = price;
+        return this;
     }
 
     public MenuFixture menuGroup(MenuGroup menuGroup) {
-        menu.setMenuGroup(menuGroup);
+        menuGroupId = null;
+        menuGroupName = null;
         if (menuGroup != null) {
-            menu.setMenuGroupId(menuGroup.getId());
+            menuGroupId = menuGroup.getId();
+            menuGroupName = menuGroup.getName();
         }
         return this;
     }
 
     public MenuFixture menuProduct(MenuProduct menuProduct) {
-        menu.setMenuProducts(List.of(menuProduct));
+        menuProducts = List.of(menuProduct);
         return this;
     }
 
     public MenuFixture menuProducts(List<MenuProduct> menuProducts) {
-        menu.setMenuProducts(menuProducts);
+        this.menuProducts = menuProducts;
         return this;
     }
 
     public MenuFixture displayed(boolean displayed) {
-        menu.setDisplayed(displayed);
+        this.displayed = displayed;
         return this;
     }
 
     public Menu build() {
-        return menu;
+        return new Menu(
+                id,
+                new DisplayedName(name, new FakePurgomalumClient()),
+                new Price(price),
+                new MenuGroup(menuGroupId, menuGroupName),
+                new MenuProducts(menuProducts), new MenuPricePolicy()
+        );
     }
+
 }
