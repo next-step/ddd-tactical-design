474e81b4202d41d278fa8244820c5fe1ae5db7db
soo <yunsoo0510@daum.net>
2023-09-22 19:47:13 +0900

    test: MenuGroupServiceTest 수정

--- src/main/java/kitchenpos/menus/application/dto/CreateMenuGroupRequest.java
+++ src/main/java/kitchenpos/menus/application/dto/CreateMenuGroupRequest.java
@@ -3,6 +3,13 @@ package kitchenpos.menus.application.dto;
 public class CreateMenuGroupRequest {
     private String name;
 
+    public CreateMenuGroupRequest() {
+    }
+
+    public CreateMenuGroupRequest(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/kitchenpos/menus/service/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/menus/service/MenuGroupServiceTest.java
@@ -5,8 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import kitchenpos.menus.application.CreateMenuGroupRequest;
 import kitchenpos.menus.application.MenuGroupService;
-import kitchenpos.menus.domain.MenuGroup;
 
 public class MenuGroupServiceTest {
     private final MenuGroupService menuGroupService = new MenuGroupService(null);
@@ -14,9 +14,7 @@ public class MenuGroupServiceTest {
     @ParameterizedTest
     @NullAndEmptySource
     void 메뉴그룹_생성__실패_이름이_null_and_empty(String nullAndEmpty) {
-        MenuGroup menuGroup = MenuGroupFixture.builder()
-                .name(nullAndEmpty)
-                .build();
+        CreateMenuGroupRequest menuGroup = new CreateMenuGroupRequest(nullAndEmpty);
 
         assertThatThrownBy(() -> menuGroupService.create(menuGroup))
                 .isInstanceOf(IllegalArgumentException.class);
