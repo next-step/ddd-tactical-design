ac8477699d0a80f31c06d216c417f6e5bb55558c
김경록 <kyeongrok.kim@kurlycorp.com>
2023-09-26 09:12:54 +0900

    refactor: MenuProduct 객체 생성 테스트 추가

--- src/test/java/kitchenpos/menu/tobe/domain/MenuProductTest.java
+++ src/test/java/kitchenpos/menu/tobe/domain/MenuProductTest.java
@@ -1,14 +1,43 @@
 package kitchenpos.menu.tobe.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 import kitchenpos.Fixtures;
 import kitchenpos.product.tobe.domain.Product;
+import kitchenpos.product.tobe.domain.ProductPrice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MenuProductTest {
+
+    @DisplayName("메뉴 상품을 생성한다")
+    @Test
+    void testInitMenuProduct() {
+        // given
+        ProductPrice price = new ProductPrice(BigDecimal.valueOf(10_000L));
+        int quantity = 1;
+
+        // when // then
+        assertDoesNotThrow(() -> new MenuProduct(UUID.randomUUID(), price, quantity));
+    }
+
+    @DisplayName("갯수가 0 미만이면 메뉴 상품을 생성할 수 없다")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3})
+    void testInitMenuProductIfQuantityIsNegative(int quantity) {
+        // given
+        ProductPrice price = new ProductPrice(BigDecimal.valueOf(10_000L));
+
+        // when // then
+        assertThatThrownBy(() -> new MenuProduct(UUID.randomUUID(), price, quantity))
+            .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("메뉴 상품의 총 가격을 구한다")
     @Test
