c68742e0e171be8f2c3839ac1346a9738aaba9b0
김경록 <kyeongrok.kim@kurlycorp.com>
2023-09-27 20:08:11 +0900

    refactor: 메뉴 상품 가격 수정 기능 테스트 추가

--- src/test/java/kitchenpos/menu/tobe/domain/MenuProductsTest.java
+++ src/test/java/kitchenpos/menu/tobe/domain/MenuProductsTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import java.math.BigDecimal;
 import java.util.List;
 import kitchenpos.Fixtures;
+import kitchenpos.product.tobe.domain.ProductPrice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -46,5 +47,18 @@ class MenuProductsTest {
 
         // then
         assertThat(actual).isEqualTo(new MenuPrice(BigDecimal.valueOf(25_000L)));
+    }
+
+    @DisplayName("메뉴 상품의 가격을 수정한다")
+    @Test
+    void testChangeMenuProductPrice() {
+        // given
+        var menuProduct = Fixtures.menuProduct();
+
+        // when
+        menuProduct.changePrice(MenuPrice.of(BigDecimal.ONE));
+
+        // then
+        assertThat(menuProduct.getPrice()).isEqualTo(new ProductPrice(BigDecimal.ONE));
     }
 }
