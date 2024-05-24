a667ca04213e6c2761b0d6f06c0900ec52cd7caf
KimJangHoon <jhkim0@meissa.ai>
2024-05-24 09:14:50 +0900

    [feat] impl basic test

---
+++ src/test/java/kitchenpos/products/application/tobe/ProductTest.java
@@ -0,0 +1,20 @@
+package kitchenpos.products.application.tobe;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+public class ProductTest {
+    @DisplayName("상품의 가격이 올바르지 않으면 변경할 수 없다.")
+    @ValueSource(strings = "-1000")
+    @NullSource
+    @ParameterizedTest
+    void kk(final BigDecimal price) {
+        Assertions.assertEquals(0, 1);
+    }
+}
