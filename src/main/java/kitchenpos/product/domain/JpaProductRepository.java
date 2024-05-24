61cd88d85fdf4e03354d8a246f1f3e4bf8c97c3f
hvoiunq <hyojunghan1016@gmail.com>
2024-05-24 23:52:16 +0900

    refactor(package) : 과제2 패키지 구조 적용

---
+++ src/main/java/kitchenpos/product/domain/JpaProductRepository.java
@@ -0,0 +1,8 @@
+package kitchenpos.product.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
+}
