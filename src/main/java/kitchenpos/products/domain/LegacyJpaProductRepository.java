e363ec5e34f062b205149eb736a412c7c1965337
yuseongan <dhaorktm125@naver.com>
2024-05-24 22:16:49 +0900

    fix(invalid reference of product entity in jpaProductRepository)

--- src/main/java/kitchenpos/products/domain/LegacyJpaProductRepository.java
+++ src/main/java/kitchenpos/products/domain/LegacyJpaProductRepository.java
@@ -1,10 +1,8 @@
 package kitchenpos.products.domain;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-@Primary
 public interface LegacyJpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
 }
--- src/main/java/kitchenpos/products/tobe/domain/JpaProductRepository.kt
+++ src/main/java/kitchenpos/products/tobe/domain/JpaProductRepository.kt
@@ -1,8 +1,5 @@
 package kitchenpos.products.tobe.domain
 
-import kitchenpos.products.domain.Product
-import kitchenpos.products.domain.ProductRepository
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface JpaProductRepository : JpaRepository<Product, Long>, ProductRepository {
-}
+interface JpaProductRepository : JpaRepository<Product, Long>, ProductRepository
