8fb14e67c63a0e97f7a26e808a498fedc94894d5
hvoiunq <hyojunghan1016@gmail.com>
2024-05-25 22:36:16 +0900

    0단계 - 기본 코드 준비 (#256)

---
+++ src/main/java/kitchenpos/order/domain/JpaOrderRepository.java
@@ -0,0 +1,8 @@
+package kitchenpos.order.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID> {
+}
---
+++ src/test/java/kitchenpos/menu/domain/InMemoryMenuGroupRepository.java
@@ -0,0 +1,23 @@
+package kitchenpos.menu.domain;
+
+import java.util.*;
+
+public class InMemoryMenuGroupRepository implements MenuGroupRepository {
+    private final Map<UUID, MenuGroup> menuGroups = new HashMap<>();
+
+    @Override
+    public MenuGroup save(final MenuGroup menuGroup) {
+        menuGroups.put(menuGroup.getId(), menuGroup);
+        return menuGroup;
+    }
+
+    @Override
+    public Optional<MenuGroup> findById(final UUID id) {
+        return Optional.ofNullable(menuGroups.get(id));
+    }
+
+    @Override
+    public List<MenuGroup> findAll() {
+        return new ArrayList<>(menuGroups.values());
+    }
+}
