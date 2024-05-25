d6a502b9f5933b128014a5732ac3351aaa5ef46d
jkshin0602 <sjk6437@gmail.com>
2024-05-25 10:23:39 +0900

    feat: 트랜잭션 제거

--- src/main/java/kitchenpos/menus/tode/adapter/DefaultMenuReader.kt
+++ src/main/java/kitchenpos/menus/tode/adapter/DefaultMenuReader.kt
@@ -4,10 +4,8 @@ import kitchenpos.menus.domain.Menu
 import kitchenpos.menus.domain.MenuRepository
 import kitchenpos.menus.tode.port.MenuReader
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
-@Transactional
 @Component
 class DefaultMenuReader(
     private val menuRepository: MenuRepository,
--- src/main/java/kitchenpos/products/tobe/adpater/DefaultProductReader.kt
+++ src/main/java/kitchenpos/products/tobe/adpater/DefaultProductReader.kt
@@ -3,14 +3,11 @@ package kitchenpos.products.tobe.adpater
 import kitchenpos.products.domain.Product
 import kitchenpos.products.domain.ProductRepository
 import kitchenpos.products.tobe.port.ProductReader
-import org.springframework.stereotype.Component
 import java.util.*
 
-@Component
 class DefaultProductReader(
     private val productRepository: ProductRepository,
 ) : ProductReader {
     override fun findProductById(productId: UUID): Product =
-        productRepository.findById(productId)
-            .orElseThrow { NoSuchElementException() }
+        productRepository.findById(productId).orElseThrow { NoSuchElementException() }
 }
