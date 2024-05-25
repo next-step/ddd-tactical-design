2b3bec67418d77fe510d68012274b4844006198c
yuseongan <dhaorktm125@naver.com>
2024-05-25 23:06:31 +0900

    implement(append equals and hashcode to product)

--- src/main/java/kitchenpos/products/tobe/domain/Product.kt
+++ src/main/java/kitchenpos/products/tobe/domain/Product.kt
@@ -24,4 +24,18 @@ class Product(
     fun changePrice(price: ProductPrice) {
         this.price = price
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Product
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
 }
