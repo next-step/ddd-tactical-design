6567a99eac13361700e25d9df992bbf8d3aa14e3
doorisopen <doorwasopen@gmail.com>
2024-05-26 16:04:54 +0900

    refactor(kitchenpos): 상품 도메인 계층 리펙터링

---
+++ src/main/java/kitchenpos/domain/product/tobe/domain/Product.java
@@ -0,0 +1,64 @@
+package kitchenpos.domain.product.tobe.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Table(name = "product")
+@Entity
+public class Product {
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    protected Product() {
+    }
+
+    public Product(UUID id, String name, BigDecimal price) {
+        validatePrice(price);
+        validateName(name);
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void changePrice(BigDecimal price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateName(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
