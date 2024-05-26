d7028f972cb0c2d5f07eeabeb49655afc1e49e1c
TaeWoong Lee <doorwasopen@gmail.com>
2024-05-26 15:27:14 +0900

    refactor(kitchenpos): 이전 미션 코드 가져오기 (#259)

---
+++ src/test/java/kitchenpos/domain/order/InMemoryOrderRepository.java
@@ -0,0 +1,35 @@
+package kitchenpos.domain.order;
+
+import kitchenpos.domain.order.domain.Order;
+import kitchenpos.domain.order.domain.OrderRepository;
+import kitchenpos.domain.order.domain.OrderStatus;
+import kitchenpos.domain.order.domain.eatIn.OrderTable;
+
+import java.util.*;
+
+public class InMemoryOrderRepository implements OrderRepository {
+    private final Map<UUID, Order> orders = new HashMap<>();
+
+    @Override
+    public Order save(final Order order) {
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    @Override
+    public Optional<Order> findById(final UUID id) {
+        return Optional.ofNullable(orders.get(id));
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return new ArrayList<>(orders.values());
+    }
+
+    @Override
+    public boolean existsByOrderTableAndStatusNot(final OrderTable orderTable, final OrderStatus status) {
+        return orders.values()
+            .stream()
+            .anyMatch(order -> order.getOrderTable().equals(orderTable) && order.getStatus() != status);
+    }
+}
