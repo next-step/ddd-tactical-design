603223efbf61576cad5ad429317aeefe19d518a8
ohjeung <jeung@docfriends.com>
2024-05-27 11:05:27 +0900

    refactor(product) : 비속어 클래스 의존성 낮추기 위해 분리

--- src/main/java/kitchenpos/products/tobe/application/ProductService.java
+++ src/main/java/kitchenpos/products/tobe/application/ProductService.java
@@ -3,14 +3,13 @@ package kitchenpos.products.tobe.application;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.UUID;
 import kitchenpos.menus.domain.Menu;
 import kitchenpos.menus.domain.MenuProduct;
 import kitchenpos.menus.domain.MenuRepository;
-import kitchenpos.products.infra.PurgomalumClient;
 import kitchenpos.products.tobe.domain.Product;
 import kitchenpos.products.tobe.domain.ProductRepository;
+import kitchenpos.products.tobe.infra.PurgomalumClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,18 +31,15 @@ public class ProductService {
 
     @Transactional
     public Product create(final String name, final BigDecimal price) {
-        return productRepository.save(Product.create(name, price, purgomalumClient));
+        purgomalumClient.containsProfanity(name);
+        return productRepository.save(Product.create(name, price));
     }
 
     @Transactional
-    public Product changePrice(final UUID productId, final Product request) {
-        final BigDecimal price = request.getPrice();
-        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException();
-        }
+    public Product changePrice(final UUID productId, final BigDecimal price) {
         final Product product = productRepository.findById(productId)
             .orElseThrow(NoSuchElementException::new);
-        product.setPrice(price);
+        product.setChangePrice(price);
         final List<Menu> menus = menuRepository.findAllByProductId(productId);
         for (final Menu menu : menus) {
             BigDecimal sum = BigDecimal.ZERO;
