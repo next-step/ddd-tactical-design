50cbd6478901a0336f5f46d77b55f67242327a8f
OhJeUng <bioman3238@gmail.com>
2024-05-27 23:41:12 +0900

    style : 줄바꿈

--- src/main/java/kitchenpos/products/tobe/application/ProductCreationResponseDto.java
+++ src/main/java/kitchenpos/products/tobe/application/ProductCreationResponseDto.java
@@ -1,15 +1,17 @@
 package kitchenpos.products.tobe.application;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import kitchenpos.products.tobe.domain.Product;
-import kitchenpos.products.tobe.domain.ProductPrice;
 
 public class ProductCreationResponseDto {
     private final UUID id;
     private final String name;
-    private final ProductPrice price;
+    private final BigDecimal price;
 
-    public ProductCreationResponseDto(UUID id, String name, ProductPrice price) {
+    public ProductCreationResponseDto(UUID id,
+                                      String name,
+                                      BigDecimal price) {
         this.id = id;
         this.name = name;
         this.price = price;
