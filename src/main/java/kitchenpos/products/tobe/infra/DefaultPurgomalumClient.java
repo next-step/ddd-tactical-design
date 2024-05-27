dc7047eac77219a494afe1c0ec0a669d0016d8e4
ohjeung <jeung@docfriends.com>
2024-05-27 11:05:27 +0900

    refactor(product) : 비속어 클래스 의존성 낮추기 위해 분리

---
+++ src/main/java/kitchenpos/products/tobe/infra/DefaultPurgomalumClient.java
@@ -0,0 +1,31 @@
+package kitchenpos.products.tobe.infra;
+
+import java.net.URI;
+import java.util.Objects;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class DefaultPurgomalumClient implements PurgomalumClient {
+    private final RestTemplate restTemplate;
+
+    public DefaultPurgomalumClient(final RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Override
+    public boolean containsProfanity(final String text) {
+        if (Objects.isNull(text) || text.length() <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        final URI url = UriComponentsBuilder.fromUriString("https://www.purgomalum.com/service/containsprofanity")
+            .queryParam("text", text)
+            .build()
+            .toUri();
+        return Boolean.parseBoolean(restTemplate.getForObject(url, String.class));
+    }
+}
---
+++ src/main/java/kitchenpos/products/tobe/infra/PurgomalumClient.java
@@ -0,0 +1,5 @@
+package kitchenpos.products.tobe.infra;
+
+public interface PurgomalumClient {
+    boolean containsProfanity(String text);
+}
