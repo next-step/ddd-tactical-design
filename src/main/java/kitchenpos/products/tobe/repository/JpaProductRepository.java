ae714ca36c759e79c62640ba3465100f8a54c0f7
OhJeUng <bioman3238@gmail.com>
2024-05-27 23:40:50 +0900

    fix(product) : 레파지토리 패키지 생성

--- src/main/java/kitchenpos/products/tobe/repository/JpaProductRepository.java
+++ src/main/java/kitchenpos/products/tobe/repository/JpaProductRepository.java
@@ -1,6 +1,8 @@
-package kitchenpos.products.tobe.domain;
+package kitchenpos.products.tobe.repository;
 
 import java.util.UUID;
+
+import kitchenpos.products.tobe.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
