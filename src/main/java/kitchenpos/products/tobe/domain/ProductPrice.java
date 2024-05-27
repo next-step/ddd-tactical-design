4cc48ed07519c6d1e0967b68471a38cea49e52d8
ohjeung <jeung@docfriends.com>
2024-05-27 11:32:08 +0900

    refactor(product) : Price 값 객체 사용

---
+++ src/main/java/kitchenpos/products/tobe/domain/ProductPrice.java
@@ -0,0 +1,28 @@
+package kitchenpos.products.tobe.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+public class ProductPrice {
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    protected ProductPrice() {}
+
+    protected ProductPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setPrice(final BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.price = price;
+    }
+
+}
