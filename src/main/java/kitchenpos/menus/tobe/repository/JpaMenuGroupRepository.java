ff457661c045cfc6e04c1b72a2636f91c06b4ea5
ohjeung <jeung@docfriends.com>
2024-05-29 16:56:58 +0900

    style(menuGroup) : Jpa 인터페이스 infra 영역으로 이동

--- src/main/java/kitchenpos/menus/tobe/repository/JpaMenuGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package kitchenpos.menus.tobe.repository;
-
-import java.util.UUID;
-
-import kitchenpos.menus.tobe.domain.MenuGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaMenuGroupRepository extends MenuGroupRepository, JpaRepository<MenuGroup, UUID> {
-}
