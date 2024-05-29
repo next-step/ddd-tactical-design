331ca52acacc000eb7f965f1b9af2bca30336567
OhJeUng <bioman3238@gmail.com>
2024-05-30 00:19:40 +0900

    refactor(product) : ProductRequest 생성자 추가

--- src/main/java/kitchenpos/products/tobe/application/ProductRequest.java
+++ src/main/java/kitchenpos/products/tobe/application/ProductRequest.java
@@ -3,8 +3,13 @@ package kitchenpos.products.tobe.application;
 import java.math.BigDecimal;
 
 public class ProductRequest {
-    private String name;
-    private BigDecimal price;
+    private final String name;
+    private final BigDecimal price;
+
+    public ProductRequest(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
 
     public String getName() {
         return name;
