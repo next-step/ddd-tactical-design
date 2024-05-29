4d1e391d54f9d8db9a90fba8cf7a9d0c9e56f535
ohjeung <jeung@docfriends.com>
2024-05-29 18:58:11 +0900

    ----------작업중------------

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuProduct.java
@@ -0,0 +1,37 @@
+package kitchenpos.menus.tobe.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import java.util.UUID;
+import kitchenpos.products.domain.Product;
+
+@Table(name = "menu_product")
+@Entity
+public class MenuProduct {
+    @Column(name = "seq")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long seq;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(
+        name = "product_id",
+        columnDefinition = "binary(16)",
+        foreignKey = @ForeignKey(name = "fk_menu_product_to_product")
+    )
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private long quantity;
+
+    public MenuProduct() {
+    }
+}
