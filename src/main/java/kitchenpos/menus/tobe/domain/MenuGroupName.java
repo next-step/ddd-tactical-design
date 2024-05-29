a617458f5ad2800fb2ae928104e4c7b7b6854729
ohjeung <jeung@docfriends.com>
2024-05-29 18:58:11 +0900

    ----------작업중------------

--- src/main/java/kitchenpos/menus/tobe/domain/MenuGroupName.java
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuGroupName.java
@@ -24,4 +24,17 @@ public class MenuGroupName {
     public String getName() {
         return name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuGroupName that = (MenuGroupName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
