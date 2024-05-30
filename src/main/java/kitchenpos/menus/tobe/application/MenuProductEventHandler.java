d3edda46cfcae77fa316852c66df34b21dfec806
whale0928 <rlagusrl928@gmail.com>
2024-05-31 02:43:34 +0900

    fix: add Transactional annotation

--- src/main/java/kitchenpos/menus/tobe/application/MenuProductEventHandler.java
+++ src/main/java/kitchenpos/menus/tobe/application/MenuProductEventHandler.java
@@ -3,7 +3,10 @@ package kitchenpos.menus.tobe.application;
 import kitchenpos.menus.domain.Menu;
 import kitchenpos.menus.domain.MenuRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
 @Component
 public class MenuProductEventHandler {
@@ -14,6 +17,7 @@ public class MenuProductEventHandler {
         this.menuRepository = menuRepository;
     }
 
+    @Transactional(propagation = REQUIRES_NEW)
     @TransactionalEventListener
     public void productPriceChangeEvent(ProductPriceChangeEvent productEvent) {
         menuRepository
