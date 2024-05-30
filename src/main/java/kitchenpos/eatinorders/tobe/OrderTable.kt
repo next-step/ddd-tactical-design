65a7da1a6d2f13de12e97a8ad9bb3471f950a927
jkshin0602 <sjk6437@gmail.com>
2024-05-30 21:09:31 +0900

    feat: 주문 테이블 도메인 생성 및 테스트 작성

---
+++ src/main/java/kitchenpos/eatinorders/tobe/OrderTable.kt
@@ -0,0 +1,34 @@
+package kitchenpos.eatinorders.tobe
+
+import java.util.*
+
+class OrderTable(
+    val name: String,
+) {
+    init {
+        require(name.isNotEmpty())
+    }
+
+    val id = UUID.randomUUID()
+    var occupied: Boolean = false
+    var numberOfGuests: Int = NUMBER_OF_GUEST_INITIAL_VALUE
+
+    fun sit() {
+        this.occupied = true
+    }
+
+    fun clear() {
+        this.numberOfGuests = NUMBER_OF_GUEST_INITIAL_VALUE
+        this.occupied = false
+    }
+
+    fun changeNumberOfGuests(numberOfGuests: Int) {
+        check(numberOfGuests >= MINIMUM_NUMBER_OF_GUEST)
+        this.numberOfGuests = numberOfGuests
+    }
+
+    companion object {
+        private const val MINIMUM_NUMBER_OF_GUEST = 0
+        private const val NUMBER_OF_GUEST_INITIAL_VALUE = 0
+    }
+}
