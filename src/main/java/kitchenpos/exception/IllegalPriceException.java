2c2730fa02a2128cd94da0e9ea0357a452256ce0
hvoiunq <hyojunghan1016@gmail.com>
2024-05-31 10:20:14 +0900

    test(product) : product 테스트코드 추가

--- src/main/java/kitchenpos/exception/IllegalPriceException.java
+++ src/main/java/kitchenpos/exception/IllegalPriceException.java
@@ -3,10 +3,6 @@ package kitchenpos.exception;
 import java.math.BigDecimal;
 
 public class IllegalPriceException extends IllegalArgumentException {
-    public IllegalPriceException(BigDecimal price) {
-        super("잘못된 가격입니다. : " + price);
-    }
-
     public IllegalPriceException(String s) {
         super(s);
     }
