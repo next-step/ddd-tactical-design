b292951e0d51a7588da9ca6d4d924593456406b6
안주형 <ajh1016@midasin.com>
2024-06-02 02:20:36 +0900

    Feat: product bc에서 발생한 이벤트 수신하는 로직 생성

---
+++ src/main/java/kitchenpos/menu/tobe/application/event/ProductPriceChangedListener.java
@@ -0,0 +1,36 @@
+package kitchenpos.menu.tobe.application.event;
+
+
+import kitchenpos.menu.tobe.domain.menu.Menu;
+import kitchenpos.menu.tobe.domain.menu.MenuRepository;
+import kitchenpos.common.event.publisher.ProductPriceChangedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class ProductPriceChangedListener {
+    private final MenuRepository menuRepository;
+
+    public ProductPriceChangedListener(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+
+    @EventListener
+    @Transactional
+    public void handle(ProductPriceChangedEvent event) {
+        List<Menu> menus = menuRepository.findAllByProductId(event.productId());
+
+        for (Menu menu : menus) {
+            try {
+                menu.changePrice(event.price());
+            } catch (IllegalArgumentException e) {
+                menu.hide();
+            }
+        }
+
+        menuRepository.saveAll(menus);
+    }
+}
