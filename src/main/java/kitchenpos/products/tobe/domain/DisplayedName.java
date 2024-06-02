b1e0653147fd4d28deed5b6551f2597530eecb2a
writer0713 <writer0713@naver.com>
2024-06-02 09:19:12 +0900

    feat: 상품 리팩토링 - Product 라는 이름으로 Entity 를 두개 등록 못한다고 하여 우선 'TobeProduct' 로 생성 - 관련된 VO 인 DisplayedName / Price 생성 - 비속어 검사를 위한 Profanities interface 추가

---
+++ src/main/java/kitchenpos/products/tobe/domain/DisplayedName.java
@@ -0,0 +1,38 @@
+package kitchenpos.products.tobe.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class DisplayedName {
+
+    @Column(name = "name", nullable = false)
+    private final String name;
+
+    private DisplayedName(String name) {
+        this.name = name;
+    }
+
+    public static DisplayedName of(String name) {
+        return new DisplayedName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DisplayedName that = (DisplayedName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
