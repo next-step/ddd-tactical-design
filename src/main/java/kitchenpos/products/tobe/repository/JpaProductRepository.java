ecf9cddada5501bd7f38aa261dad4fc22be7b2c5
OhJeUng <bioman3238@gmail.com>
2024-06-02 21:58:05 +0900

    chore(product) : tobe 코드 삭제

--- src/main/java/kitchenpos/products/tobe/repository/JpaProductRepository.java
+++
@@ -1,9 +0,0 @@
-package kitchenpos.products.tobe.repository;
-
-import java.util.UUID;
-
-import kitchenpos.products.tobe.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
-}
--- src/main/java/kitchenpos/products/tobe/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package kitchenpos.products.tobe.repository;
-
-import kitchenpos.products.tobe.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProductRepository extends JpaRepository<Product, UUID> {}
