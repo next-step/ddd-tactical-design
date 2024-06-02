54fedc107eef4494636413d6ee38fb7698644d34
whale0928 <rlagusrl928@gmail.com>
2024-06-03 06:13:48 +0900

    feat: tobe menu entity implement

---
+++ src/main/java/kitchenpos/menus/tobe/domain/TobeMenu.java
@@ -0,0 +1,102 @@
+package kitchenpos.menus.tobe.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Table(name = "menu")
+@Entity
+public class TobeMenu {
+
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Embedded
+    @Column(name = "name", nullable = false)
+    private DisplayName name;
+
+    @Embedded
+    @Column(name = "price", nullable = false)
+    private MenuPrice menuPrice;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "menu_group_id", columnDefinition = "binary(16)")
+    private TobeMenuGroup menuGroup;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "displayed", nullable = false)
+    private Displayed displayed = Displayed.DISPLAYED;
+
+    @Embedded
+    private MenuProducts menuProducts;
+
+    protected TobeMenu() {
+    }
+
+    public TobeMenu(UUID id, DisplayName name, MenuPrice menuPrice, TobeMenuGroup menuGroup, Displayed displayed, MenuProducts menuProducts) {
+
+        if (menuPrice.price().compareTo(menuProducts.getTotalPrice()) > 0) {
+            throw new IllegalArgumentException("메뉴의 가격은 메뉴에 속한 상품의 가격보다 적거나 같아야합니다.");
+        }
+
+        this.id = id;
+        this.name = name;
+        this.menuPrice = menuPrice;
+        this.menuGroup = menuGroup;
+        this.displayed = displayed;
+        this.menuProducts = menuProducts;
+    }
+
+    /**
+     * 메뉴의 가격을 변경한다
+     *
+     * @param price the price
+     */
+    public void changePrice(BigDecimal price) {
+        this.menuPrice = MenuPrice.of(price);
+        updateDisplayStatusOnPriceChange();
+    }
+
+    /**
+     * 상품의 가격이 변경될 경우 메뉴의 가격을 체크 해야한다.
+     */
+    public void updateDisplayStatusOnPriceChange() {
+        if (menuPrice.price().compareTo(menuProducts.getTotalPrice()) > 0) {
+            this.displayed = Displayed.HIDDEN;
+        }
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public DisplayName name() {
+        return name;
+    }
+
+    public BigDecimal price() {
+        return menuPrice.price();
+    }
+
+    public TobeMenuGroup menuGroup() {
+        return menuGroup;
+    }
+
+    public Displayed displayed() {
+        return displayed;
+    }
+
+    public MenuProducts menuProducts() {
+        return menuProducts;
+    }
+}
---
+++ src/test/java/kitchenpos/menus/tobe/domain/TobeMenuTest.java
@@ -0,0 +1,92 @@
+package kitchenpos.menus.tobe.domain;
+
+import kitchenpos.products.application.FakePurgomalumClient;
+import kitchenpos.products.infra.PurgomalumClient;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TobeMenuTest {
+
+    private PurgomalumClient purgomalumClient;
+
+    @BeforeEach
+    void setUp() {
+        purgomalumClient = new FakePurgomalumClient();
+    }
+
+    @Test
+    @org.junit.jupiter.api.DisplayName("메뉴의 가격은 메뉴상품의 가격합보다 적거나 같아야한다.")
+    void create() {
+        // given
+        DisplayName name = DisplayName.of("후라이드 치킨", purgomalumClient);
+        MenuPrice menuPrice = MenuPrice.of(BigDecimal.valueOf(21000L));
+        TobeMenuGroup menuGroup = TobeMenuGroup.create("후라이드 치킨 세트");
+        Displayed displayed = Displayed.DISPLAYED;
+
+        final BigDecimal price = BigDecimal.valueOf(10000L);
+        MenuProducts menuProducts = MenuProducts.of(
+                TobeMenuProduct.create(UUID.randomUUID(), price, 1L),
+                TobeMenuProduct.create(UUID.randomUUID(), price, 1L)
+        );
+
+        // when
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new TobeMenu(UUID.randomUUID(), name, menuPrice, menuGroup, displayed, menuProducts))
+                .withMessage("메뉴의 가격은 메뉴에 속한 상품의 가격보다 적거나 같아야합니다.");
+    }
+
+    @Test
+    @org.junit.jupiter.api.DisplayName("상품의 가격이 변경된 후 메뉴상품과 비교 후  Displayed 상태가 변경될 수 있다.")
+    void updateDisplayStatusOnPriceChange() {
+        // given
+        DisplayName name = DisplayName.of("후라이드 치킨", purgomalumClient);
+        MenuPrice menuPrice = MenuPrice.of(BigDecimal.valueOf(20000L));
+        TobeMenuGroup menuGroup = TobeMenuGroup.create("후라이드 치킨 세트");
+        Displayed displayed = Displayed.DISPLAYED;
+
+        final BigDecimal price = BigDecimal.valueOf(10000L);
+        MenuProducts menuProducts = MenuProducts.of(
+                TobeMenuProduct.create(UUID.randomUUID(), price, 1L),
+                TobeMenuProduct.create(UUID.randomUUID(), price, 1L)
+        );
+
+        TobeMenu menu = new TobeMenu(UUID.randomUUID(), name, menuPrice, menuGroup, displayed, menuProducts);
+
+        // when
+        menu.changePrice(BigDecimal.valueOf(21000L));
+        menu.updateDisplayStatusOnPriceChange();
+
+        // then
+        assertEquals(Displayed.HIDDEN, menu.displayed());
+    }
+
+    @Test
+    @org.junit.jupiter.api.DisplayName("메뉴의 가격을 변경할 수 있다.")
+    void changePrice() {
+        // given
+        DisplayName name = DisplayName.of("후라이드 치킨", purgomalumClient);
+        MenuPrice menuPrice = MenuPrice.of(BigDecimal.valueOf(20000L));
+        TobeMenuGroup menuGroup = TobeMenuGroup.create("후라이드 치킨 세트");
+        Displayed displayed = Displayed.DISPLAYED;
+
+        final BigDecimal price = BigDecimal.valueOf(10000L);
+        MenuProducts menuProducts = MenuProducts.of(
+                TobeMenuProduct.create(UUID.randomUUID(), price, 1L),
+                TobeMenuProduct.create(UUID.randomUUID(), price, 1L)
+        );
+
+        TobeMenu menu = new TobeMenu(UUID.randomUUID(), name, menuPrice, menuGroup, displayed, menuProducts);
+
+        // when
+        menu.changePrice(BigDecimal.valueOf(19000L));
+
+        // then
+        assertEquals(BigDecimal.valueOf(19000L), menu.price());
+    }
+
+}
