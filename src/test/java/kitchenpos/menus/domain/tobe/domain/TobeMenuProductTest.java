06243fcf08a20971c72690bb9b1782933fe033c5
writer0713 <writer0713@naver.com>
2024-06-03 15:26:02 +0900

    test: TobeMenuProductTest 추가

---
+++ src/test/java/kitchenpos/menus/domain/tobe/domain/TobeMenuProductTest.java
@@ -0,0 +1,47 @@
+package kitchenpos.menus.domain.tobe.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+class TobeMenuProductTest {
+
+    @DisplayName("메뉴에 속한 상품의 수량은 0 이상이어야 한다.")
+    @Test
+    void case_1() {
+        // given
+        var quantity = -1;
+
+        // when
+        // then
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> createMenuProductWithQuantity(quantity));
+    }
+
+    @DisplayName("메뉴에 속한 상품의 가격은 0 이상이어야 한다.")
+    @Test
+    void case_2() {
+        // given
+        var price = -1;
+
+        // when
+        // then
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> createMenuProductWithPrice(price));
+    }
+
+    private TobeMenuProduct createMenuProductWithQuantity(int quantity) {
+        var productId = UUID.randomUUID();
+        var price = 10_000;
+        return new TobeMenuProduct(quantity, price, productId);
+    }
+
+    private TobeMenuProduct createMenuProductWithPrice(int price) {
+        var productId = UUID.randomUUID();
+        var quantity = 1;
+        return new TobeMenuProduct(quantity, price, productId);
+    }
+
+}
