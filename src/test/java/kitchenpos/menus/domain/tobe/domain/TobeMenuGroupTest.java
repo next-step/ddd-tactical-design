d4cd312736be7698b43d5cbba2c935963ba6b141
writer0713 <writer0713@naver.com>
2024-06-03 15:03:50 +0900

    test: MenuGroup 테스트 추가

---
+++ src/test/java/kitchenpos/menus/domain/tobe/domain/TobeMenuGroupTest.java
@@ -0,0 +1,25 @@
+package kitchenpos.menus.domain.tobe.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.UUID;
+
+class TobeMenuGroupTest {
+
+    @DisplayName("메뉴 그룹의 이름은 비워 둘 수 없다.")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void case_1(String name) {
+        // given
+
+        // when
+        // then
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> new TobeMenuGroup(UUID.randomUUID(), name));
+
+    }
+
+}
