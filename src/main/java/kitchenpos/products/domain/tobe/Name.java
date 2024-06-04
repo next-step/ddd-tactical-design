2eda9b83f41552636043751a2d1b20861455e844
hoa0217 <gjwjdghk123@naver.com>
2024-06-04 16:59:19 +0900

    refactor(kitchenpos-product): Name/Price equal, hashCode 메서드 생성

--- src/main/java/kitchenpos/products/domain/tobe/Name.java
+++ src/main/java/kitchenpos/products/domain/tobe/Name.java
@@ -25,5 +25,22 @@ public class Name {
     public String getName() {
         return name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
 
--- src/main/java/kitchenpos/products/domain/tobe/Price.java
+++ src/main/java/kitchenpos/products/domain/tobe/Price.java
@@ -25,4 +25,21 @@ public class Price {
     public BigDecimal getPrice() {
         return price;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Price price1 = (Price) o;
+        return Objects.equals(price, price1.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
 }
