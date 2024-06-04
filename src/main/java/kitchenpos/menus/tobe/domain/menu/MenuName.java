86dea0fa3b258e8b4997490db2681be2f3958429
OhJeUng <bioman3238@gmail.com>
2024-06-05 00:42:27 +0900

    feat(menu) : 메뉴 엔티티 수정

--- src/main/java/kitchenpos/menus/tobe/domain/menu/MenuName.java
+++ src/main/java/kitchenpos/menus/tobe/domain/menu/MenuName.java
@@ -1,4 +1,4 @@
-package kitchenpos.menus.tobe.domain;
+package kitchenpos.menus.tobe.domain.menu;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
@@ -6,37 +6,37 @@ import java.util.Objects;
 
 @Embeddable
 public class MenuName {
-    @Column(name = "name", nullable = false)
-    private String name;
+  @Column(name = "name", nullable = false)
+  private String name;
 
-    protected MenuName() {}
+  protected MenuName() {}
 
-    protected MenuName(String name, PurgomalumClient purgomalumClient) {
-        if (Objects.isNull(name) || name.isEmpty()) {
-            throw new IllegalArgumentException("메뉴 이름을 입력해주세요.");
-        }
-
-        if (purgomalumClient.containsProfanity(name)) {
-            throw new IllegalArgumentException("메뉴 이름에 비속어가 들어갈 수 없습니다.");
-        }
-
-        this.name = name;
+  protected MenuName(String name, PurgomalumClient purgomalumClient) {
+    if (Objects.isNull(name) || name.isEmpty()) {
+      throw new IllegalArgumentException("메뉴 이름을 입력해주세요.");
     }
 
-    public String getName() {
-        return name;
+    if (purgomalumClient.containsProfanity(name)) {
+      throw new IllegalArgumentException("메뉴 이름에 비속어가 들어갈 수 없습니다.");
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MenuName menuName = (MenuName) o;
-        return Objects.equals(name, menuName.name);
-    }
+    this.name = name;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    MenuName menuName = (MenuName) o;
+    return Objects.equals(name, menuName.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
 }
