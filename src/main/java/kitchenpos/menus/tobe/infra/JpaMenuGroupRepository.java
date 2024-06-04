853dad772ebd1c2244338b1403359f6c1b9865d5
OhJeUng <bioman3238@gmail.com>
2024-06-05 00:47:51 +0900

    chore(menu) : infra 패키지 이동

--- src/main/java/kitchenpos/menus/tobe/infra/JpaMenuGroupRepository.java
+++ src/main/java/kitchenpos/menus/tobe/infra/JpaMenuGroupRepository.java
@@ -2,8 +2,9 @@ package kitchenpos.menus.tobe.infra;
 
 import java.util.UUID;
 
-import kitchenpos.menus.tobe.domain.MenuGroup;
+import kitchenpos.menus.tobe.domain.menugroup.MenuGroup;
+import kitchenpos.menus.tobe.domain.menugroup.MenuGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaMenuGroupRepository extends MenuGroupRepository, JpaRepository<MenuGroup, UUID> {
-}
+public interface JpaMenuGroupRepository
+    extends MenuGroupRepository, JpaRepository<MenuGroup, UUID> {}
