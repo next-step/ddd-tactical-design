bc582af1cb2aa05f4147027e4dda3325ae3608da
Jivebread <spark@troy.edu>
2024-06-05 00:07:58 +0900

    feature(MenuGroup): Equals & Hash 생성

--- src/main/java/kitchenpos/menus/domain/tobe/menugroup/MenuGroupName.java
+++ src/main/java/kitchenpos/menus/domain/tobe/menugroup/MenuGroupName.java
@@ -33,4 +33,17 @@ public class MenuGroupName {
   public String getName() {
     return name;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    MenuGroupName that = (MenuGroupName) o;
+    return Objects.equals(name, that.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
 }
