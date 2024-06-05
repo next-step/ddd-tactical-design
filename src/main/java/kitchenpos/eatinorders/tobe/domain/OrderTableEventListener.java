12d5d543b481c74cb490aa45c33050c4e15129f1
kst <kst@osstem.com>
2024-06-05 17:55:41 +0900

    del( event ) 이벤트 관련 클래스 정리 및 삭제

--- src/main/java/kitchenpos/eatinorders/tobe/domain/OrderTableEventListener.java
+++
@@ -1,8 +0,0 @@
-package kitchenpos.eatinorders.tobe.domain;
-
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class OrderTableEventListener {
-}
