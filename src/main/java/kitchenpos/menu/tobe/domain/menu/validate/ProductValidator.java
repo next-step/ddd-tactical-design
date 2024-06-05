e02983e9fa8c0522bf7512291d1e7909d84e762f
안주형 <ajh1016@midasin.com>
2024-06-06 00:39:17 +0900

    Refactor: 변수 추출로 가독성 향상

--- src/main/java/kitchenpos/menu/tobe/domain/menu/validate/ProductValidator.java
+++ src/main/java/kitchenpos/menu/tobe/domain/menu/validate/ProductValidator.java
@@ -5,6 +5,7 @@ import kitchenpos.menu.tobe.domain.menu.ProductApiClient;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class ProductValidator {
@@ -15,11 +16,11 @@ public class ProductValidator {
     }
 
     public void validateProductExistence(List<MenuProduct> menuProducts) {
-        long actualProductCount = productApiClient.countProductsByIds(
-                menuProducts.stream()
-                        .map(MenuProduct::getProductId)
-                        .toList()
-        );
+        List<UUID> productIds = menuProducts.stream()
+                .map(MenuProduct::getProductId)
+                .toList();
+
+        long actualProductCount = productApiClient.countProductsByIds(productIds);
 
         if (actualProductCount != menuProducts.size()) {
             throw new IllegalArgumentException("메뉴에 속한 상품의 갯수가 실제 상품과 일치하지 않습니다.");
