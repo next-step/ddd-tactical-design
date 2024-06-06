b63eb1194c03d7a828f1a2b73a24227341b33f70
writer0713 <writer0713@naver.com>
2024-06-06 20:39:47 +0900

    test: TobeProductServiceTest 추가

--- src/test/java/kitchenpos/products/tobe/application/TobeProductServiceTest.kt
+++ src/test/java/kitchenpos/products/tobe/application/TobeProductServiceTest.kt
@@ -1,9 +1,11 @@
 package kitchenpos.products.tobe.application
 
+import kitchenpos.products.tobe.domain.Price
 import kitchenpos.products.tobe.domain.TobeProduct
 import kitchenpos.products.tobe.domain.TobeProductRepository
 import kitchenpos.products.tobe.infra.InMemoryTobeProductRepository
 import kitchenpos.share.domain.FakeProfanities
+import kitchenpos.shared.domain.Profanities
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -20,20 +22,58 @@ class TobeProductServiceTest {
         sut = TobeProductService(tobeProductRepository = tobeProductRepository)
     }
 
-    @DisplayName("save product to DB")
+    @DisplayName("상품 등록")
     @Test
     fun case_1() {
         // given
-        val id = UUID.randomUUID()
-        val name = "후라이드"
-        val price = 10_000
-        val profanities = FakeProfanities("비속어")
-        val request = TobeProduct(id, name, price, profanities)
+        val request = createProduct()
 
         // when
         val savedProduct = sut.create(request)
 
         // then
         assertThat(savedProduct).isNotNull()
+    }
+
+    @DisplayName("상품 가격 변경")
+    @Test
+    fun case_2() {
+        // given
+        val savedProduct = tobeProductRepository.save(createProduct())
+        val price = 15_000
+        val productId = savedProduct.id
+        val changePriceRequest = createProduct(price = price)
+
+        // when
+        val changedProduct = sut.changePrice(productId, changePriceRequest)
+
+        // then
+        assertThat(changedProduct).isNotNull()
+        assertThat(changedProduct.id).isEqualTo(productId)
+        assertThat(changedProduct.price).isEqualTo(Price.of(price))
+    }
+
+    @DisplayName("상품의 목록을 조회할 수 있다.")
+    @Test
+    fun case_3() {
+        // given
+        tobeProductRepository.save(createProduct(name = "후라이드"))
+        tobeProductRepository.save(createProduct(name = "양념"))
+
+        // when
+        val products = sut.findAll()
+
+        // then
+        assertThat(products).isNotEmpty
+        assertThat(products.size).isEqualTo(2)
+    }
+
+    private fun createProduct(
+        id: UUID = UUID.randomUUID(),
+        name: String = "후라이드",
+        price: Int = 10_000,
+        profanities: Profanities = FakeProfanities("비속어"),
+    ): TobeProduct {
+        return TobeProduct(id, name, price, profanities)
     }
 }
