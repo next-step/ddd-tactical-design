2e3e4e4bbe33d9974c21e8d0fc28b9a0b92338da
hoa0217 <gjwjdghk123@naver.com>
2024-06-06 12:13:55 +0900

    refactor(kitchenpos-product): final 추가

--- src/main/java/kitchenpos/products/ui/dto/ProductCreateRequest.java
+++ src/main/java/kitchenpos/products/ui/dto/ProductCreateRequest.java
@@ -1,21 +1,20 @@
 package kitchenpos.products.ui.dto;
 
 import java.math.BigDecimal;
-import kitchenpos.products.domain.PurgomalumClient;
 import kitchenpos.products.domain.tobe.Product;
 
 public class ProductCreateRequest {
 
-    private String name;
+    private final String name;
 
-    private BigDecimal price;
+    private final BigDecimal price;
 
     public ProductCreateRequest(String name, BigDecimal price) {
         this.name = name;
         this.price = price;
     }
 
-    public Product to(){
+    public Product to() {
         return new Product(name, price);
     }
 
