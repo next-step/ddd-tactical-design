321217a2e15f10de49dfda96ab3f5294db050901
writer0713 <writer0713@naver.com>
2024-06-06 21:53:33 +0900

    test: TobeMenuGroupServiceTest 에 @Transactional 추가

--- src/test/java/kitchenpos/menus/tobe/application/TobeMenuGroupServiceTest.kt
+++ src/test/java/kitchenpos/menus/tobe/application/TobeMenuGroupServiceTest.kt
@@ -2,13 +2,14 @@ package kitchenpos.menus.tobe.application
 
 import kitchenpos.menus.tobe.domain.menugroup.TobeMenuGroup
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
+@Transactional
 @SpringBootTest
 class TobeMenuGroupServiceTest
     @Autowired
