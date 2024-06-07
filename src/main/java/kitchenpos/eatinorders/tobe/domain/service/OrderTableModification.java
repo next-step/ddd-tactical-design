22d0891f53fb30335eaade3b8ff9ac982e9b304d
pawoo0211 <pawoo0211@naver.com>
2024-06-08 04:04:49 +0900

    chore: 이벤트 핸들링 시 로깅 처리

--- src/main/java/kitchenpos/eatinorders/tobe/domain/service/OrderTableModification.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/service/OrderTableModification.java
@@ -3,12 +3,15 @@ package kitchenpos.eatinorders.tobe.domain.service;
 import kitchenpos.eatinorders.tobe.domain.entity.CompletedOrderEvent;
 import kitchenpos.eatinorders.tobe.domain.entity.OrderTable;
 import kitchenpos.eatinorders.tobe.domain.repository.OrderTableRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import java.util.NoSuchElementException;
 
 @Component
 public class OrderTableModification {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderTableModification.class);
     private final OrderTableRepository tableRepository;
 
     public OrderTableModification(OrderTableRepository tableRepository) {
@@ -17,7 +20,7 @@ public class OrderTableModification {
 
     @EventListener
     public void setEmptyTable(CompletedOrderEvent event) {
-        System.out.println("주문 테이블 빈 테이블 설정");
+        LOGGER.info("주문이 완료 되어 빈 테이블 설정을 진행합니다.");
         OrderTable table = tableRepository.findById(event.getOrderTableId())
                 .orElseThrow(() -> new NoSuchElementException());
         table.clear();
--- src/main/java/kitchenpos/eatinorders/tobe/domain/service/UpdateMenuInOrders.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/service/UpdateMenuInOrders.java
@@ -3,6 +3,8 @@ package kitchenpos.eatinorders.tobe.domain.service;
 import kitchenpos.eatinorders.tobe.domain.entity.EatInOrder;
 import kitchenpos.eatinorders.tobe.domain.repository.EatInOrderRepository;
 import kitchenpos.menus.tobe.domain.entity.ChangedMenuEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 @Component
 public class UpdateMenuInOrders {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UpdateMenuInOrders.class);
     private final EatInOrderRepository orderRepository;
 
     public UpdateMenuInOrders(EatInOrderRepository orderRepository) {
@@ -18,7 +21,7 @@ public class UpdateMenuInOrders {
 
     @EventListener
     public void updateMenu(ChangedMenuEvent menuEvent) {
-        System.out.println("메뉴가 변경됐습니다.");
+        LOGGER.info("메뉴가 변경되어 주문 항목을 확인합니다.");
 
         List<EatInOrder> orders = orderRepository.findAllByMenuId(menuEvent.getMenuId());
 
