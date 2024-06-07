ad542aeddb65bf91991c544da0888a2f6e1bbfc0
OhJeUng <bioman3238@gmail.com>
2024-06-08 01:14:06 +0900

    test(menu) : 상품 숨김 처리, 테스트 방법 모색중

--- src/test/java/kitchenpos/products/tobe/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/products/tobe/application/ProductServiceTest.java
@@ -8,13 +8,17 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
-import kitchenpos.menus.application.InMemoryMenuRepository;
-import kitchenpos.menus.domain.MenuRepository;
+import kitchenpos.menus.tobe.MenuFixtures;
+import kitchenpos.menus.tobe.application.InMemoryMenuRepository;
+import kitchenpos.menus.tobe.domain.menu.Menu;
+import kitchenpos.menus.tobe.domain.menu.MenuRepository;
 import kitchenpos.products.application.ProductRequest;
 import kitchenpos.products.application.ProductResponse;
 import kitchenpos.products.application.ProductService;
+import kitchenpos.products.domain.Product;
 import kitchenpos.products.domain.ProductRepository;
 import kitchenpos.products.domain.PurgomalumClient;
+import kitchenpos.products.tobe.ProductFixtures;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,7 +26,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -31,13 +37,16 @@ class ProductServiceTest {
   private MenuRepository menuRepository;
   private PurgomalumClient purgomalumClient;
   private ProductService productService;
+  @Mock private ApplicationEventPublisher applicationEventPublisher;
 
   @BeforeEach
   void setUp() {
     productRepository = new InMemoryProductRepository();
     menuRepository = new InMemoryMenuRepository();
     purgomalumClient = new FakePurgomalumClient();
-    productService = new ProductService(productRepository, menuRepository, purgomalumClient);
+    productService =
+        new ProductService(
+            productRepository, menuRepository, purgomalumClient, applicationEventPublisher);
   }
 
   @DisplayName("상품을 등록할 수 있다.")
@@ -47,10 +56,9 @@ class ProductServiceTest {
     final ProductResponse actual = productService.create(expected);
     assertThat(actual).isNotNull();
     assertAll(
-            () -> assertThat(actual.getId()).isNotNull(),
-            () -> assertThat(actual.getName()).isEqualTo(expected.getName()),
-            () -> assertThat(actual.getPrice()).isEqualTo(expected.getPrice())
-    );
+        () -> assertThat(actual.getId()).isNotNull(),
+        () -> assertThat(actual.getName()).isEqualTo(expected.getName()),
+        () -> assertThat(actual.getPrice()).isEqualTo(expected.getPrice()));
   }
 
   @DisplayName("상품의 가격이 올바르지 않으면 등록할 수 없다.")
@@ -60,7 +68,7 @@ class ProductServiceTest {
   void create(final BigDecimal price) {
     final ProductRequest expected = new ProductRequest("후라이드", price);
     assertThatThrownBy(() -> productService.create(expected))
-            .isInstanceOf(IllegalArgumentException.class);
+        .isInstanceOf(IllegalArgumentException.class);
   }
 
   @DisplayName("상품의 이름이 올바르지 않으면 등록할 수 없다.")
@@ -70,13 +78,14 @@ class ProductServiceTest {
   void create(final String name) {
     final ProductRequest expected = new ProductRequest(name, BigDecimal.valueOf(16_000L));
     assertThatThrownBy(() -> productService.create(expected))
-            .isInstanceOf(IllegalArgumentException.class);
+        .isInstanceOf(IllegalArgumentException.class);
   }
 
   @DisplayName("상품의 가격을 변경할 수 있다.")
   @Test
   void changePrice() {
-    final UUID productId = productRepository.save(product("후라이드", 16_000L, purgomalumClient)).getId();
+    final UUID productId =
+        productRepository.save(ProductFixtures.product("후라이드", 16_000L, purgomalumClient)).getId();
     final ProductRequest expected = new ProductRequest("", BigDecimal.valueOf(15_000L));
     final ProductResponse actual = productService.changePrice(productId, expected);
     assertThat(actual.getPrice()).isEqualTo(expected.getPrice());
@@ -87,27 +96,28 @@ class ProductServiceTest {
   @NullSource
   @ParameterizedTest
   void changePrice(final BigDecimal price) {
-    final UUID productId = productRepository.save(product("후라이드", 16_000L, purgomalumClient)).getId();
+    final UUID productId =
+        productRepository.save(ProductFixtures.product("후라이드", 16_000L, purgomalumClient)).getId();
     final ProductRequest expected = new ProductRequest("", price);
     assertThatThrownBy(() -> productService.changePrice(productId, expected))
-            .isInstanceOf(IllegalArgumentException.class);
+        .isInstanceOf(IllegalArgumentException.class);
   }
 
-/*  @DisplayName("상품의 가격이 변경될 때 메뉴의 가격이 메뉴에 속한 상품 금액의 합보다 크면 메뉴가 숨겨진다.")
+  @DisplayName("상품의 가격이 변경될 때 메뉴의 가격이 메뉴에 속한 상품 금액의 합보다 크면 메뉴가 숨겨진다.")
   @Test
   void changePriceInMenu() {
-    final Product product = productRepository.save(product("후라이드", 16_000L, purgomalumClient));
-    final Menu menu = menuRepository.save(menu(19_000L, true, menuProduct(product, 2L)));
+    final Product product = productRepository.save(ProductFixtures.product());
+    final Menu menu = menuRepository.save(MenuFixtures.menu());
     final ProductRequest expected = new ProductRequest("", BigDecimal.valueOf(8_000L));
     productService.changePrice(product.getId(), expected);
     assertThat(menuRepository.findById(menu.getId()).get().isDisplayed()).isFalse();
-  }*/
+  }
 
   @DisplayName("상품의 목록을 조회할 수 있다.")
   @Test
   void findAll() {
-    productRepository.save(product("후라이드", 16_000L, purgomalumClient));
-    productRepository.save(product("양념치킨", 16_000L, purgomalumClient));
+    productRepository.save(ProductFixtures.product("후라이드", 16_000L, purgomalumClient));
+    productRepository.save(ProductFixtures.product("양념치킨", 16_000L, purgomalumClient));
     final List<ProductResponse> actual = productService.findAll();
     assertThat(actual).hasSize(2);
   }
