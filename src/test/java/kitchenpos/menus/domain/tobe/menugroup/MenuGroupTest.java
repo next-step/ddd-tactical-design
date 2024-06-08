6c0ad63c46f76929263ec5e91695c4364bcf357d
hoa0217 <gjwjdghk123@naver.com>
2024-06-09 03:50:01 +0900

    test(kitchenpos-product): MenuGroupTest 추가

---
+++ src/test/java/kitchenpos/menus/domain/tobe/menugroup/MenuGroupTest.java
@@ -0,0 +1,33 @@
+package kitchenpos.menus.domain.tobe.menugroup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("MenuGroup")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class MenuGroupTest {
+
+    @Test
+    void 이름이_null인_MenuGroup을_생성하면_예외를_던진다() {
+        assertThatThrownBy(() -> new MenuGroup((String) null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름이_비어있는_MenuGroup을_생성하면_예외를_던진다() {
+        assertThatThrownBy(() -> new MenuGroup(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void MenuGroup을_생성한다() {
+        MenuGroup actual = new MenuGroup("치킨");
+
+        assertThat(actual.getId()).isNotNull();
+    }
+}
