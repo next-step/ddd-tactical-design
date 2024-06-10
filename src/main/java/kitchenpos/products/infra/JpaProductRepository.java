20e1a38737af403d51d2dde052184977359a1cfe
sjhello <sjhello235@gmail.com>
2024-06-10 23:29:14 +0900

    refactor: infra 계층으로 이동

--- src/main/java/kitchenpos/products/infra/JpaProductRepository.java
+++ src/main/java/kitchenpos/products/infra/JpaProductRepository.java
@@ -1,5 +1,7 @@
-package kitchenpos.products.tobe.domain;
+package kitchenpos.products.infra;
 
+import kitchenpos.products.tobe.domain.Product;
+import kitchenpos.products.tobe.domain.ProductRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
