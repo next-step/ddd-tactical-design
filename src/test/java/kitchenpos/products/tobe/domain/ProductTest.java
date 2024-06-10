6afc31a589ccb4f26e1f549e4f969e68cd1298e8
sjhello <sjhello235@gmail.com>
2024-06-10 23:42:29 +0900

    refactor: ProductTest에 있는 ProductName에 대한 테스트를 분리한다

--- src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
+++ src/test/java/kitchenpos/products/tobe/domain/ProductTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ProductTest {
 
@@ -16,13 +15,6 @@ class ProductTest {
         Product product = new Product("후라이드", BigDecimal.valueOf(10_000L));
         assertThat(product.getName()).isEqualTo(new ProductName("후라이드"));
         assertThat(product.getPrice()).isEqualTo(new ProductPrice(BigDecimal.valueOf(10_000L)));
-    }
-
-    @Test
-    @DisplayName("상품의 이름이 null이면 IllegalArgumentException이 발생한다")
-    void fail() {
-        assertThatThrownBy(() -> new Product(null, BigDecimal.valueOf(10_000L)))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
