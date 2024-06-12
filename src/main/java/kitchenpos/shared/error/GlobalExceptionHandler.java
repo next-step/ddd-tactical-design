da145364331a2d967e37cbb7ab2e2aaf9c7efcd0
sjhello <sjhello235@gmail.com>
2024-06-12 23:39:12 +0900

    refactor: 서비스에서 발생할 수 있는 표준 Exception을 핸들링 한다.
     - 추후에는 커스텀 예외를 생성한다.

--- src/main/java/kitchenpos/shared/error/GlobalExceptionHandler.java
+++ src/main/java/kitchenpos/shared/error/GlobalExceptionHandler.java
@@ -12,10 +12,16 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException e) {
+    public ResponseEntity<Object> handleNoSuchElementException() {
         ProductErrorCode errorCode = ProductErrorCode.PRODUCT_NOT_FOUND;
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        return ResponseEntity.status(errorCode.getStatus())
                 .body(createErrorResponse(errorCode.name(), errorCode.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(createErrorResponse("BAD_REQUEST", "잘못된 요청입니다"));
     }
 
     private ErrorResponse createErrorResponse(String code, String message) {
