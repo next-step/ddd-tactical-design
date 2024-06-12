c3397af72c7dbac8bfa598df3bf432b6b6e162c2
sjhello <sjhello235@gmail.com>
2024-06-12 23:33:39 +0900

    refactor: ErrorCode에 HttpStatus 속성을 추가

--- src/main/java/kitchenpos/products/ui/error/ProductErrorCode.java
+++ src/main/java/kitchenpos/products/ui/error/ProductErrorCode.java
@@ -1,18 +1,26 @@
 package kitchenpos.products.ui.error;
 
 import kitchenpos.shared.error.ErrorCode;
+import org.springframework.http.HttpStatus;
 
 import static kitchenpos.products.ui.error.ProductErrorMessages.PRODUCT_NOT_FOUND_MESSAGE;
 
 public enum ProductErrorCode implements ErrorCode {
 
-    PRODUCT_NOT_FOUND(PRODUCT_NOT_FOUND_MESSAGE);
+    PRODUCT_NOT_FOUND(PRODUCT_NOT_FOUND_MESSAGE, HttpStatus.NOT_FOUND);
 
-    ProductErrorCode(String message) {
+    ProductErrorCode(String message, HttpStatus httpStatus) {
         this.message = message;
+        this.httpStatus = httpStatus;
     }
 
     private final String message;
+    private final HttpStatus httpStatus;
+
+    @Override
+    public HttpStatus getStatus() {
+        return this.httpStatus;
+    }
 
     @Override
     public String getMessage() {
--- src/main/java/kitchenpos/shared/error/ErrorCode.java
+++ src/main/java/kitchenpos/shared/error/ErrorCode.java
@@ -1,6 +1,9 @@
 package kitchenpos.shared.error;
 
+import org.springframework.http.HttpStatus;
+
 public interface ErrorCode {
     String name();
+    HttpStatus getStatus();
     String getMessage();
 }
