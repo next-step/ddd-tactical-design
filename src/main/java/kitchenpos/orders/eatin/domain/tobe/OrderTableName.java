324233fe7b6140711bdb030c8a86b23ae9923928
허정화 <gjwjdghk123@naver.com>
2024-06-20 17:26:15 +0900

    refactor(OrderTableName): 주문테이블의 이름은 없거나 비어있을 수 없다.

---
+++ src/main/java/kitchenpos/orders/eatin/domain/tobe/OrderTableName.java
@@ -0,0 +1,37 @@
+package kitchenpos.orders.eatin.domain.tobe;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class OrderTableName {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    protected OrderTableName() {
+    }
+
+    public OrderTableName(String name) {
+        if (Objects.isNull(name) || name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderTableName that = (OrderTableName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
