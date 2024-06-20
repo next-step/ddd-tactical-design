59771a5d850542a6662934db199df7dadd295823
허정화 <gjwjdghk123@naver.com>
2024-06-20 17:30:21 +0900

    refactor(OrderTable): 주문테이블 클래스를 새로 생성한다.
    - VO(NumberOfGuest, OrderTableName)을 갖는다.

---
+++ src/main/java/kitchenpos/orders/eatin/domain/tobe/OrderTable.java
@@ -0,0 +1,54 @@
+package kitchenpos.orders.eatin.domain.tobe;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table(name = "order_table")
+@Entity
+public class OrderTable {
+
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Embedded
+    private OrderTableName name;
+
+    @Embedded
+    private NumberOfGuests numberOfGuests;
+
+    @Column(name = "occupied", nullable = false)
+    private boolean occupied;
+
+    protected OrderTable() {
+    }
+
+    public OrderTable(String name) {
+        this.id = UUID.randomUUID();
+        this.name = new OrderTableName(name);
+        this.numberOfGuests = new NumberOfGuests(0);
+        this.occupied = false;
+    }
+
+    public void sit() {
+        this.occupied = true;
+    }
+
+    public void clear() {
+        this.numberOfGuests = new NumberOfGuests(0);
+        this.occupied = false;
+    }
+
+    public void changeNumberOfGuests(NumberOfGuests numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean hasGuest() {
+        return numberOfGuests.getNumberOfGuests() > 0;
+    }
+}
