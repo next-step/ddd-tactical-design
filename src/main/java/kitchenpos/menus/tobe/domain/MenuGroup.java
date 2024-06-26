9edd4b55593ed2acfd594ed87f6ac32989ac9831
gutenLee <3bnm1128@naver.com>
2024-06-26 11:30:05 +0900

    refactor : menuGroup.name Name 타입으로 변경

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuGroup.java
@@ -0,0 +1,42 @@
+package kitchenpos.menus.tobe.domain;
+
+import jakarta.persistence.*;
+import kitchenpos.products.tobe.Name;
+
+import java.util.UUID;
+
+@Table(name = "menu_group")
+@Entity
+public class MenuGroup {
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    @Embedded
+    private Name name;
+
+    public MenuGroup() {
+    }
+
+    public MenuGroup(UUID id, Name name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(final Name name) {
+        this.name = name;
+    }
+}
