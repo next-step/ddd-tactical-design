4097bfbd8ce6918ef934e825de4edcc5a7073c14
wotjd4305 <wotjd4305@naver.com>
2025-02-17 00:21:47 +0900

    feat : (ddd-tactical-design) step1. Product Price 임베디드 객체추가

---
+++ src/main/java/kitchenpos/products/tobe/domain/Product.java
@@ -0,0 +1,46 @@
+package kitchenpos.products.tobe.domain;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table(name = "product")
+@Entity
+public class Product {
+    @Column(name = "id", columnDefinition = "binary(16)")
+    @Id
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Embedded
+    private Price price;
+
+    public Product() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    public void setPrice(Price price) {
+        this.price = price;
+    }
+}
