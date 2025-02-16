404269772e287018555faf7a975db3b7919d0bd3
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-02-16 19:50:58 +0900

    feat(kitchen pos) : Name, Price equals & hashcode 추가

--- src/main/java/kitchenpos/common/domain/Name.java
+++ src/main/java/kitchenpos/common/domain/Name.java
@@ -29,4 +29,21 @@ public class Name {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
 }
