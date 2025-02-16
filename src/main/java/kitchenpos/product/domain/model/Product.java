bc1cf8c43750931c27509d8e6cccb5d7c18135e5
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-02-16 16:24:57 +0900

    refactor(kitchen pos) : sql script 가 작동하도록 DeliveryOrder 주석 처리

--- src/main/java/kitchenpos/product/domain/model/Product.java
+++ src/main/java/kitchenpos/product/domain/model/Product.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 @Table(name = "product")
@@ -33,6 +34,25 @@ public class Product {
 
     protected Product() {
 
+    }
+
+    public Product(String name, BigDecimal price) {
+        validateName(name);
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
+    }
+
+    private void validateName(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public UUID getId() {
