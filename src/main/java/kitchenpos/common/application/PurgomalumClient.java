8ddc5b3340e266312dd4aacb7935aa1521ca7adc
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-02-17 21:15:43 +0900

    fix(kitchen pos) : 필요 없는 애노테이션 제거

--- src/main/java/kitchenpos/common/application/PurgomalumClient.java
+++ src/main/java/kitchenpos/common/application/PurgomalumClient.java
@@ -1,8 +1,5 @@
 package kitchenpos.common.application;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public interface PurgomalumClient {
     boolean containsProfanity(String text);
 }
