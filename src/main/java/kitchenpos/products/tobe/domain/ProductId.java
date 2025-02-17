41ab52fe9d39d95c30b72376a35a1faf1f3f7f90
skdev0619 <skdev0619@naver.com>
2025-02-17 13:23:49 +0900

    ProductId 값 객체 추가

---
+++ src/main/java/kitchenpos/products/tobe/domain/ProductId.java
@@ -0,0 +1,33 @@
+package kitchenpos.products.tobe.domain;
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Embeddable
+public class ProductId implements Serializable {
+    private UUID id;
+
+    protected ProductId() {
+    }
+
+    public ProductId(UUID id) {
+        Objects.requireNonNull(id, "product id는 필수 입력 항목입니다");
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductId productId = (ProductId) o;
+        return Objects.equals(id, productId.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/test/java/kitchenpos/products/tobe/domain/ProductIdTest.java
@@ -0,0 +1,20 @@
+package kitchenpos.products.tobe.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ProductIdTest {
+
+    @DisplayName("상품 key에 null이 입력되면 예외가 발생한다")
+    @NullSource
+    @ParameterizedTest
+    void notNull(UUID id) {
+        assertThatThrownBy(() -> new ProductId(id))
+                .isInstanceOf(NullPointerException.class);
+    }
+}
