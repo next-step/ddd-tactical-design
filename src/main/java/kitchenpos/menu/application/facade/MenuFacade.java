55d09018d735a978e8fb7593ec6a33d5246fdd61
restinbeat <thek226@gmail.com>
2025-02-18 13:59:58 +0900

    refactor: *facade -> @component

--- src/main/java/kitchenpos/menu/application/facade/MenuFacade.java
+++ src/main/java/kitchenpos/menu/application/facade/MenuFacade.java
@@ -1,5 +1,21 @@
 package kitchenpos.menu.application.facade;
 
+import kitchenpos.menu.domain.service.MenuGroupService;
+import kitchenpos.menu.domain.service.MenuPurgomalumClient;
+import kitchenpos.menu.domain.service.MenuService;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MenuFacade {
 
+    private final MenuService menuService;
+    private final MenuGroupService menuGroupService;
+    private final MenuPurgomalumClient purgomalumClient;
+
+    public MenuFacade(MenuService menuService, MenuGroupService menuGroupService,
+        MenuPurgomalumClient purgomalumClient) {
+        this.menuService = menuService;
+        this.menuGroupService = menuGroupService;
+        this.purgomalumClient = purgomalumClient;
+    }
 }
--- src/main/java/kitchenpos/order/common/application/facade/OrderFacade.java
+++ src/main/java/kitchenpos/order/common/application/facade/OrderFacade.java
@@ -3,8 +3,9 @@ package kitchenpos.order.common.application.facade;
 import kitchenpos.order.delivery.application.DeliveryUsecase;
 import kitchenpos.order.eatin.application.EatinUsecase;
 import kitchenpos.order.takeout.application.TakeoutUsecase;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class OrderFacade {
     private final TakeoutUsecase takeoutUsecase;
     private final DeliveryUsecase deliveryUsecase;
