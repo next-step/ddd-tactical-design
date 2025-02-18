b58bc867002c22c3fba3f1fa27030a008b6263fc
suzhanlee <wlscww@kakao.com>
2025-02-18 10:54:43 +0900

    feat(kitchen pos) : MenuProductQuantity 생성

---
+++ src/main/java/kitchenpos/menu/domain/model/MenuProductQuantity.java
@@ -0,0 +1,43 @@
+package kitchenpos.menu.domain.model;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class MenuProductQuantity {
+    private static final String MENU_PRODUCT_QUANTITY_CREATION_EXCEPTION = "메뉴 상품의 수량은 0보다 커야 합니다!";
+
+    private final Long value;
+
+    public MenuProductQuantity(long value) {
+        validateMenuProductQuantity(value);
+        this.value = value;
+    }
+
+    private void validateMenuProductQuantity(long value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(MENU_PRODUCT_QUANTITY_CREATION_EXCEPTION);
+        }
+    }
+
+    protected MenuProductQuantity() {
+        this.value = null;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuProductQuantity that = (MenuProductQuantity) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
