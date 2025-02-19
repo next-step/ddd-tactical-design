d624867fae5c006001fc8e69ec8f96339f3ee737
restinbeat <thek226@gmail.com>
2025-02-19 11:01:39 +0900

    refactor: 상품 등록 정책 분리

---
+++ src/main/java/kitchenpos/product/domain/service/DefaultProductCreatePolicy.java
@@ -0,0 +1,24 @@
+package kitchenpos.product.domain.service;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import kitchenpos.global.exception.ErrorCode;
+import kitchenpos.product.domain.model.ProductNameValidator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultProductCreatePolicy implements ProductCreatePolicy {
+
+    @Override
+    public BigDecimal validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException(ErrorCode.PRODUCT_PRICE_NOT_ALLOWED.toString());
+        }
+        return price;
+    }
+
+    @Override
+    public String validateName(String productName, ProductPurgomalumClient purgomalumClient) {
+        return new ProductNameValidator(productName, purgomalumClient).name();
+    }
+}
