7c4a56651daa0818c6a1607d381399d2140068d0
suzhanlee <wlscww@kakao.com>
2025-02-20 14:37:45 +0900

    test(kitchen pos) : EatInOrderRestController 테스트 코드 추가

--- src/test/java/kitchenpos/TestFixtureFactory.java
+++ src/test/java/kitchenpos/TestFixtureFactory.java
@@ -89,7 +89,7 @@ public class TestFixtureFactory {
     }
 
     public static OrderTable createUsingOrderTable() {
-        return new OrderTable("사용 중인 테이블", 4, true);
+        return new OrderTable(UUID.randomUUID(), "사용 중인 테이블", 4, true);
     }
 
     public static Product createProduct(String name, long price) {
