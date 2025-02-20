5230f646cae88e7690dc1b23fb5d36791fcb21f3
restinbeat <thek226@gmail.com>
2025-02-21 08:29:24 +0900

    refactor: 불필요 제거

--- src/main/java/kitchenpos/product/domain/service/ProductCreatePolicy.java
+++
@@ -1,9 +0,0 @@
-package kitchenpos.product.domain.service;
-
-import java.math.BigDecimal;
-
-public interface ProductCreatePolicy {
-    BigDecimal validatePrice(BigDecimal price);
-
-    String validateName(String productName, ProductPurgomalumClient purgomalumClient);
-}
