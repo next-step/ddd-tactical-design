3e805bf6f4f1f0421175fd6fbd10705fdf66a4f5
restinbeat <thek226@gmail.com>
2025-02-21 15:44:52 +0900

    refactor: 계산 검증 제거하고 단순 숨김 처리만

--- src/test/java/kitchenpos/menu/domain/service/FakeMenuUpdatePolicy.java
+++ src/test/java/kitchenpos/menu/domain/service/FakeMenuUpdatePolicy.java
@@ -1,14 +1,13 @@
 package kitchenpos.menu.domain.service;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 import kitchenpos.menu.domain.entity.Menu;
-import kitchenpos.menu.domain.repository.InMemoryMenuRepository;
 import kitchenpos.menu.domain.repository.MenuRepository;
 
 public class FakeMenuUpdatePolicy implements MenuUpdatePolicy {
-    private MenuRepository menuRepository = new InMemoryMenuRepository();
+
+    private final MenuRepository menuRepository;
 
     public FakeMenuUpdatePolicy(MenuRepository menuRepository) {
         this.menuRepository = menuRepository;
@@ -18,16 +17,10 @@ public class FakeMenuUpdatePolicy implements MenuUpdatePolicy {
     public void hideMenu(UUID productId) {
         List<Menu> menus = menuRepository.findAllByProductId(productId);
 
-        for (Menu menu : menus) {
-            BigDecimal sum = BigDecimal.ZERO;
-            for (var menuProduct : menu.getMenuProducts()) {
-                sum = sum.add(
-                    menuProduct.getProduct()
-                        .getPrice()
-                        .multiply(BigDecimal.valueOf(menuProduct.getQuantity()))
-                );
-            }
-            menu.setDisplayed(menu.getPrice().compareTo(sum) <= 0);
-        }
+        menus.forEach(menu -> {
+            menu.setDisplayed(false);
+            menuRepository.save(menu);
+        });
     }
 }
+
