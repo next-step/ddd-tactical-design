bcba2d2a5b3aba4e1fa80c90c52f0da6805efb89
woozi <kwj1270@naver.com>
2025-02-22 22:18:14 +0900

    test(step2): 메뉴 상품의 가격은 0원 이상이어한다.

--- src/test/java/kitchenpos/menus/tobe/MenuProduct.java
+++ src/test/java/kitchenpos/menus/tobe/MenuProduct.java
@@ -1,4 +1,15 @@
 package kitchenpos.menus.tobe;
 
 public class MenuProduct {
+    private final Long seq;
+    private final long price;
+    private final long quantity;
+    private final long productId;
+
+    public MenuProduct(final Long seq, final long price, final long quantity, final long productId) {
+        this.seq = seq;
+        this.price = price;
+        this.quantity = quantity;
+        this.productId = productId;
+    }
 }
---
+++ src/test/java/kitchenpos/menus/tobe/MenuProductTest.java
@@ -0,0 +1,20 @@
+package kitchenpos.menus.tobe;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MenuProductTest {
+
+    @DisplayName("메뉴 상품의 가격은 0원 이상이어한다.")
+    @ParameterizedTest(name = "메뉴 상품 가격 : `{0}`")
+    @ValueSource(strings = {"-1", "-1000", "-100000"})
+    void createWithNegativePrice(final long price) {
+
+        assertThatThrownBy(
+                () -> new MenuProduct(null, price, 1L, 1L)
+        ).isInstanceOf(InvalidMenuProductPricePeriodException.class);
+    }
+}
