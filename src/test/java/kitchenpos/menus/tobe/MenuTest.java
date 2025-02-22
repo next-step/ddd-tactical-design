321455626bae45dfd9a67c7c5e9dc48472d0fb35
woozi <kwj1270@naver.com>
2025-02-23 00:36:53 +0900

    test(step2): 메뉴의 가격이 모든 메뉴 상품 금액의 총합보다 높은 경우 메뉴를 노출할 수 없다.

--- src/test/java/kitchenpos/menus/tobe/MenuTest.java
+++ src/test/java/kitchenpos/menus/tobe/MenuTest.java
@@ -117,4 +117,26 @@ public class MenuTest {
                 () -> menu.changePrice(changedMenuPrice)
         ).isExactlyInstanceOf(InvalidMenuPriceException.class);
     }
+
+    @DisplayName("메뉴의 가격이 모든 메뉴 상품 금액의 총합보다 높은 경우 메뉴를 노출할 수 없다.")
+    @CsvSource(value = {"1_000:1:1_001:1_000:1:2_000", "2_000:1:2_001:2_000:2:6_000", "1_000:3:1_001:3_000:2:9_000"}, delimiter = ':')
+    @ParameterizedTest(name = """
+                {index}. 첫 번째 메뉴 상품 가격 : `{0}`, 첫 번째 메뉴 상품 수량 : `{1}`, 변경할 첫 번째 메뉴 상품 가격 : `{2}`, 
+                두 번째 메뉴 상품 가격 : `{3}`, 두 번째 메뉴 상품 수량 : `{4}`, 메뉴 가격 : `{5}`
+            """)
+    void displayWithInvalidPrice(final long firstPrice, final long firstQuantity, final long changedFirstPrice,
+                                 final long secondPrice, final long secondQuantity,
+                                 final long menuPrice) {
+        final MenuProduct firstMenuProduct = new MenuProduct(1L, firstPrice, firstQuantity, 1L);
+        final List<MenuProduct> menuProducts = List.of(
+                firstMenuProduct,
+                new MenuProduct(2L, secondPrice, secondQuantity, 2L)
+        );
+        final Menu menu = new Menu(null, "메뉴", profanities, menuPrice, menuGroup, menuProducts, false);
+        final Menu changedMenu = menu.changedProductPrice(1L, changedFirstPrice);
+
+        assertThatThrownBy(
+                () -> changedMenu.display()
+        ).isExactlyInstanceOf(IllegalStateException.class);
+    }
 }
