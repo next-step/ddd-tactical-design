671cf09b7d522d503d80467952e42ea33346acb4
woozi <kwj1270@naver.com>
2025-02-22 17:59:07 +0900

    test(step2): 메뉴 그룹 이름이 null 이거나 빈 문자열일 경우 메뉴 그룹을 생성할 수 없다.

---
+++ src/test/java/kitchenpos/menus/tobe/MenuGroupTest.java
@@ -0,0 +1,22 @@
+package kitchenpos.menus.tobe;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MenuGroupTest {
+
+    @DisplayName("메뉴 그룹 이름이 null 이거나 빈 문자열일 경우 메뉴 그룹을 생성할 수 없다.")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void create(final String name) {
+        final UUID uuid = UUID.randomUUID();
+        assertThatThrownBy(
+                () -> new MenuGroup(uuid, name)
+        ).isInstanceOf(InvalidMenuGroupNameException.class);
+    }
+}
