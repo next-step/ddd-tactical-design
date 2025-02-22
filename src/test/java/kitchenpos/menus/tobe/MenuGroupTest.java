ab08b2df03aa5c69036fa2d44084cf9393c11253
woozi <kwj1270@naver.com>
2025-02-22 18:09:54 +0900

    test(step2): 메뉴 그룹을 생성할 수 있다.

--- src/test/java/kitchenpos/menus/tobe/MenuGroupTest.java
+++ src/test/java/kitchenpos/menus/tobe/MenuGroupTest.java
@@ -4,9 +4,11 @@ import kitchenpos.products.tobe.domain.exception.InvalidMenuGroupNameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MenuGroupTest {
@@ -19,5 +21,16 @@ public class MenuGroupTest {
 
         assertThatThrownBy(() -> new MenuGroup(uuid, name)
         ).isInstanceOf(InvalidMenuGroupNameException.class);
+    }
+
+
+    @DisplayName("메뉴 그룹을 생성할 수 있다.")
+    @ValueSource(strings = {"후라이드치킨", "양념치킨", "허니콤보"})
+    @ParameterizedTest(name = "{index}. 메뉴 그룹 이름: `{0}`")
+    void create(final String name) {
+        final UUID id = UUID.randomUUID();
+        final MenuGroup menuGroup = new MenuGroup(id, name);
+
+        assertThat(menuGroup).isEqualTo(new MenuGroup(id, name));
     }
 }
