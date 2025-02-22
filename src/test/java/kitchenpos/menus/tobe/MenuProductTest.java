f71815158aadffd363004dba580e10dfa40c93c2
woozi <kwj1270@naver.com>
2025-02-22 22:26:24 +0900

    test(step2): 메뉴 상품의 수량은 0개 이상이어야 한다.

--- src/test/java/kitchenpos/menus/tobe/MenuProductTest.java
+++ src/test/java/kitchenpos/menus/tobe/MenuProductTest.java
@@ -10,11 +10,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class MenuProductTest {
 
     @DisplayName("메뉴 상품의 가격은 0원 이상이어한다.")
-    @ParameterizedTest(name = "메뉴 상품 가격 : `{0}`")
+    @ParameterizedTest(name = "{index}. 메뉴 상품 가격 : `{0}`")
     @ValueSource(strings = {"-1", "-1000", "-100000"})
     void createWithNegativePrice(final long price) {
         assertThatThrownBy(
                 () -> new MenuProduct(null, price, 1L, 1L)
         ).isInstanceOf(InvalidMenuProductPricePeriodException.class);
+    }
+
+    @DisplayName("메뉴 상품의 수량은 0개 이상이어야 한다.")
+    @ParameterizedTest(name = "{index}. 메뉴 상품 수량 : `{0}`")
+    @ValueSource(strings = {"-1", "-1000", "-100000"})
+    void createWithNegativeQuantity(final long quantity) {
+        assertThatThrownBy(
+                () -> new MenuProduct(null, 1_000L, quantity, 1L)
+        ).isInstanceOf(InvalidMenuProductQuantityException.class);
     }
 }
