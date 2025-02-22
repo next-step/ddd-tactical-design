4c8d457e907d648b38d7db2a66d0bda42c3c867f
skdev0619 <skdev0619@naver.com>
2025-02-22 22:44:21 +0900

    MenuGroupId, MenuGroupName 값 객체 추가

---
+++ src/main/java/kitchenpos/menus/tobe/domain/MenuGroupId.java
@@ -0,0 +1,36 @@
+package kitchenpos.menus.tobe.domain;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Embeddable
+public class MenuGroupId {
+    private UUID id;
+
+    public static MenuGroupId generate() {
+        return new MenuGroupId(UUID.randomUUID());
+    }
+
+    protected MenuGroupId() {
+    }
+
+    public MenuGroupId(final UUID id) {
+        Objects.requireNonNull(id, "menuGroup id는 필수 입력 항목입니다");
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuGroupId that = (MenuGroupId) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
