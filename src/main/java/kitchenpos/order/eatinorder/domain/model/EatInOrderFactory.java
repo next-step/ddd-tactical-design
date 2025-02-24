9a5a39ba5f21408aadca8ffac3cbb357e707f805
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-02-24 21:52:49 +0900

    refactor(kitchen pos) : EatInOrderFactory 팩토리 메소드 애노테이션 수정

--- src/main/java/kitchenpos/order/eatinorder/domain/model/EatInOrderFactory.java
+++ src/main/java/kitchenpos/order/eatinorder/domain/model/EatInOrderFactory.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
-import kitchenpos.order.common.model.OrderLineItemValidator;
 import kitchenpos.order.common.model.OrderLineItem;
+import kitchenpos.order.common.model.OrderLineItemValidator;
 import kitchenpos.order.eatinorder.domain.repository.OrderTableRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class EatInOrderFactory {
 
     private final OrderLineItemValidator orderLineItemValidator;
