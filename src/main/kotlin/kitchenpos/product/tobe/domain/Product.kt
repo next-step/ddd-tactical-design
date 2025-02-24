0108bbe18e4196f54bb4279b56e3f77d3ab5765c
hoon25 <cchoon95@hanmail.net>
2025-02-24 20:44:49 +0900

    feat(Product): price 관련 domain 추가

--- src/main/kotlin/kitchenpos/product/tobe/domain/Product.kt
+++ src/main/kotlin/kitchenpos/product/tobe/domain/Product.kt
@@ -22,8 +22,10 @@ class Product(
     @Column(name = "id", columnDefinition = "binary(16)")
     @Id
     var id: UUID? = null,
+
     @Embedded
     var productName: ProductName,
+
     @Column(name = "price", nullable = false)
     var price: BigDecimal,
 ) {
@@ -31,6 +33,5 @@ class Product(
     init {
         require(price >= BigDecimal.ZERO) { "상품 가격은 0원 이상이어야 합니다." }
     }
-
 
 }
