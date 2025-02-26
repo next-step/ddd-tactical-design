26da49ce5eb3a8de3139bc5646f8d05e82b8aed8
restinbeat <thek226@gmail.com>
2025-02-26 14:02:03 +0900

    refactor: jparepo id 변경

--- src/main/java/kitchenpos/menu/infrastructure/persistence/JpaMenuRepository.java
+++ src/main/java/kitchenpos/menu/infrastructure/persistence/JpaMenuRepository.java
@@ -1,15 +1,15 @@
 package kitchenpos.menu.infrastructure.persistence;
 
 import java.util.List;
-import java.util.UUID;
 import kitchenpos.menu.domain.entity.Menu;
+import kitchenpos.menu.domain.model.MenuId;
 import kitchenpos.menu.domain.repository.MenuRepository;
 import kitchenpos.product.domain.model.ProductId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface JpaMenuRepository extends MenuRepository, JpaRepository<Menu, UUID> {
+public interface JpaMenuRepository extends MenuRepository, JpaRepository<Menu, MenuId> {
 
     @Query("select m from Menu m join MenuProduct mp where mp.productId = :productId")
     @Override
--- src/main/java/kitchenpos/product/infrastructure/persistence/JpaProductRepository.java
+++ src/main/java/kitchenpos/product/infrastructure/persistence/JpaProductRepository.java
@@ -1,10 +1,10 @@
 package kitchenpos.product.infrastructure.persistence;
 
-import java.util.UUID;
 import kitchenpos.product.domain.entity.Product;
+import kitchenpos.product.domain.model.ProductId;
 import kitchenpos.product.domain.repository.ProductRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
+public interface JpaProductRepository extends ProductRepository, JpaRepository<Product, ProductId> {
 
 }
