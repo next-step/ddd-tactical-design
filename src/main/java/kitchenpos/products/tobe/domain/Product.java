8214e2f3f6417f1eddb23365d66505a2f04fc089
devifancy <fancy.junyongmoon@gmail.com>
2025-02-26 21:32:34 +0900

    chore: 사용하지 않는 import, 필드 제거

--- src/main/java/kitchenpos/products/tobe/domain/Product.java
+++ src/main/java/kitchenpos/products/tobe/domain/Product.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import kitchenpos.common.infra.PurgomalumClient;
 
 import java.math.BigDecimal;
 import java.util.Objects;
--- src/test/java/kitchenpos/fixtures/ProductFixtures.java
+++ src/test/java/kitchenpos/fixtures/ProductFixtures.java
@@ -1,33 +1,13 @@
 package kitchenpos.fixtures;
 
-import static java.math.BigDecimal.valueOf;
 import kitchenpos.products.tobe.domain.Product;
 import kitchenpos.products.tobe.domain.ProductName;
 import kitchenpos.products.tobe.domain.ProductPrice;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public class ProductFixtures {
 
-    public static final UUID 후라이드치킨_ID = UUID.randomUUID();
-    public static final UUID 양념치킨_ID = UUID.randomUUID();
-
-    public static final String 후라이드치킨_이름 = "후라이드치킨";
-    public static final String 양념치킨_이름 = "양념치킨";
-
-    public static final BigDecimal 후라이드치킨_가격 = valueOf(16000);
-    public static final BigDecimal 양념치킨_가격 = valueOf(17000);
-
-    public static Product 후라이드치킨() {
-        Product product = new Product(new ProductName(후라이드치킨_이름), new ProductPrice(후라이드치킨_가격));
-        return product;
-    }
-
-    public static Product 양념치킨() {
-        Product product = new Product(new ProductName(양념치킨_이름), new ProductPrice(양념치킨_가격));
-        return product;
-    }
     public static Product createProduct(final String name, final BigDecimal price) {
         Product product = new Product(new ProductName(name), new ProductPrice(price));
         return product;
