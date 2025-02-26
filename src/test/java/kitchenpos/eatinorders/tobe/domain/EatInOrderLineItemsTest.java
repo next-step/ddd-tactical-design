94f20e5943f3a1795367dbf70171c8590c00ccfd
woozi <kwj1270@naver.com>
2025-02-27 02:03:18 +0900

    test(step3): 메뉴의 가격과 주문 항목 메뉴의 가격이 불일치하면 false 를 반환한다.

--- src/test/java/kitchenpos/eatinorders/tobe/domain/EatInOrderLineItemsTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/EatInOrderLineItemsTest.java
@@ -16,13 +16,23 @@ class EatInOrderLineItemsTest {
     @CsvSource(value = {"1000:1000", "10_000:10_000", "16_000:16_000"}, delimiter = ':')
     @ParameterizedTest(name = "{index}. 메뉴 가격: {0}, 주문 항목 메뉴 가격: {1}")
     void isSamePrice(final int menuPrice, final int expectedPrice) {
-        // given
         final UUID menuId = UUID.randomUUID();
         final EatInOrderLineItemMenu eatInOrderLineItemMenu = new EatInOrderLineItemMenu(menuId, "후라이드치킨", menuPrice);
         final EatInOrderLineItem eatInOrderLineItem = new EatInOrderLineItem(1L, eatInOrderLineItemMenu, 1);
         final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(List.of(eatInOrderLineItem));
 
-        // when
         assertThat(eatInOrderLineItems.isSamePrice(menuId, expectedPrice)).isTrue();
+    }
+
+    @DisplayName("메뉴의 가격과 주문 항목 메뉴의 가격이 불일치하면 false 를 반환한다.")
+    @CsvSource(value = {"1000:1001", "10_000:9_999", "0:1"}, delimiter = ':')
+    @ParameterizedTest(name = "{index}. 메뉴 가격: {0}, 주문 항목 메뉴 가격: {1}")
+    void isNotSamePrice(final int menuPrice, final int expectedPrice) {
+        final UUID menuId = UUID.randomUUID();
+        final EatInOrderLineItemMenu eatInOrderLineItemMenu = new EatInOrderLineItemMenu(menuId, "후라이드치킨", menuPrice);
+        final EatInOrderLineItem eatInOrderLineItem = new EatInOrderLineItem(1L, eatInOrderLineItemMenu, 1);
+        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(List.of(eatInOrderLineItem));
+
+        assertThat(eatInOrderLineItems.isSamePrice(menuId, expectedPrice)).isFalse();
     }
 }
