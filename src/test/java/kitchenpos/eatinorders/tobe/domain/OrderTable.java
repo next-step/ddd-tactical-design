bef7b842c1a9e1fcce52741885fab0b64d0644b9
woozi <kwj1270@naver.com>
2025-02-27 00:19:52 +0900

    refactor(step3): 주문 테이블 이름이 없거나 비어있으면 생성할 수 없다.

--- src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/OrderTable.java
@@ -1,18 +1,20 @@
 package kitchenpos.eatinorders.tobe.domain;
 
-import java.util.Objects;
+import kitchenpos.eatinorders.tobe.domain.vo.OrderTableName;
+
 import java.util.UUID;
 
 public class OrderTable {
     private UUID id;
-    private String name;
+    private OrderTableName name;
     private int numberOfGuests;
     private boolean occupied;
 
     public OrderTable(final UUID id, final String name, final int numberOfGuests, final boolean occupied) {
-        if(Objects.isNull(name) || name.isBlank()) {
-            throw new IllegalArgumentException();
-        }
+        this(id, new OrderTableName(name), numberOfGuests, occupied);
+    }
+
+    public OrderTable(final UUID id, final OrderTableName name, final int numberOfGuests, final boolean occupied) {
         this.id = id;
         this.name = name;
         this.numberOfGuests = numberOfGuests;
---
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/vo/OrderTableName.java
@@ -0,0 +1,27 @@
+package kitchenpos.eatinorders.tobe.domain.vo;
+
+import java.util.Objects;
+
+public class OrderTableName {
+    private final String value;
+
+    public OrderTableName(final String value) {
+        if(Objects.isNull(value) || value.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final OrderTableName that = (OrderTableName) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
