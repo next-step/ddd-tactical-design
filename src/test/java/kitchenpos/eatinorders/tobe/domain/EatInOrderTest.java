f76336f10f4afa1e2a1e4210773dd93969b4fcf6
woozi <kwj1270@naver.com>
2025-03-01 02:38:43 +0900

    test(step3): 수락중인 매장 주문을 주문 서빙으로 변경한다.

--- src/test/java/kitchenpos/eatinorders/tobe/domain/EatInOrderTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/EatInOrderTest.java
@@ -172,4 +172,25 @@ public class EatInOrderTest {
         assertThatThrownBy(eatInOrder::served)
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("수락중인 매장 주문을 주문 서빙으로 변경한다.")
+    @Test
+    void serve() {
+        final UUID firstMenuId = UUID.randomUUID();
+        final UUID secondMenuId = UUID.randomUUID();
+        final EatInOrderLineItem firstEatInOrderLineItem = new EatInOrderLineItem(1L, new EatInOrderLineItemMenu(firstMenuId, "후라이드 치킨", 16_000), 1);
+        final EatInOrderLineItem secondEatInOrderLineItem = new EatInOrderLineItem(1L, new EatInOrderLineItemMenu(secondMenuId, "양념 치킨", 16_000), 1);
+        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(firstEatInOrderLineItem, secondEatInOrderLineItem);
+        final EatInOrderMenus eatInOrderMenus = new DefaultEatInOrderMenus(
+                new DefaultEatInOrderMenu(firstMenuId, 16_000, true),
+                new DefaultEatInOrderMenu(secondMenuId, 16_000, true)
+        );
+        final EatInOrderTable eatInOrderTable = new DefaultEatInOrderTable(UUID.randomUUID(), true);
+
+        final EatInOrder eatInOrder = new EatInOrder(eatInOrderLineItems, eatInOrderMenus, eatInOrderTable);
+        eatInOrder.accepted();
+        eatInOrder.served();
+
+        assertThat(eatInOrder.status()).isEqualTo(EatInOrderStatus.SERVED);
+    }
 }
