50f3b723dc52596ffee767e6f4e3ad8c47624d02
woozi <kwj1270@naver.com>
2025-03-01 11:25:37 +0900

    feat(step3): 매장 주문 전체 시나리오 서비스 정리

--- src/test/java/kitchenpos/eatinorders/tobe/application/EatInOrderService.java
+++ src/test/java/kitchenpos/eatinorders/tobe/application/EatInOrderService.java
@@ -2,11 +2,16 @@ package kitchenpos.eatinorders.tobe.application;
 
 import kitchenpos.eatinorders.tobe.application.dto.CreateEatInOrderCommand;
 import kitchenpos.eatinorders.tobe.application.dto.CreateEatInOrderLineItemCommand;
-import kitchenpos.eatinorders.tobe.domain.*;
+import kitchenpos.eatinorders.tobe.domain.InMemoryEatInOrderMenuRepository;
+import kitchenpos.eatinorders.tobe.domain.InMemoryEatInOrderRepository;
+import kitchenpos.eatinorders.tobe.domain.InMemoryEatInOrderTableRepository;
 import kitchenpos.eatinorders.tobe.domain.order.*;
+import kitchenpos.eatinorders.tobe.domain.order.vo.EatInOrderId;
 import kitchenpos.eatinorders.tobe.domain.order.vo.Quantity;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 public class EatInOrderService {
 
@@ -20,6 +25,38 @@ public class EatInOrderService {
         final EatInOrder eatInOrder = eatInOrder(command, eatInOrderMenus, orderTable);
         return eatInOrderRepository.save(eatInOrder);
     }
+
+    public EatInOrder accept(final UUID orderId) {
+        final EatInOrder order = eatInOrderRepository.findById(new EatInOrderId(orderId))
+                .orElseThrow(NoSuchElementException::new);
+        order.accepted();
+        return order;
+    }
+
+    public EatInOrder serve(final UUID orderId) {
+        final EatInOrder order = eatInOrderRepository.findById(new EatInOrderId(orderId))
+                .orElseThrow(NoSuchElementException::new);
+        order.served();
+        return order;
+    }
+
+    public EatInOrder complete(final UUID orderId) {
+        final EatInOrder order = eatInOrderRepository.findById(new EatInOrderId(orderId))
+                .orElseThrow(NoSuchElementException::new);
+        order.completed();
+//        order.setStatus(OrderStatus.COMPLETED);
+//        final OrderTable orderTable = order.getOrderTable();
+//        if (!eatInOrderRepository.existsByOrderTableAndStatusNot(orderTable, OrderStatus.COMPLETED)) {
+//            orderTable.setNumberOfGuests(0);
+//            orderTable.setOccupied(false);
+//        }
+        return order;
+    }
+
+    public List<EatInOrder> findAll() {
+        return eatInOrderRepository.findAll();
+    }
+
 
     private EatInOrder eatInOrder(final CreateEatInOrderCommand command, final EatInOrderMenus eatInOrderMenus, final EatInOrderTable orderTable) {
         final List<CreateEatInOrderLineItemCommand> createEatInOrderLineItemCommands = command.lineItems();
