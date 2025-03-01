ab5acc3a1722f04b28267d4ec9c52e544aeabb07
woozi <kwj1270@naver.com>
2025-03-01 18:53:26 +0900

    test(step3): 주문 항목 목록, 주문 항목 메뉴와 메뉴의 개수가 일치하지 않으면 매장 주문을 생성할 수 없다.

--- src/test/java/kitchenpos/eatinorders/tobe/domain/EatInOrderLineItemsTest.java
+++ src/test/java/kitchenpos/eatinorders/tobe/domain/EatInOrderLineItemsTest.java
@@ -2,10 +2,12 @@ package kitchenpos.eatinorders.tobe.domain;
 
 import kitchenpos.eatinorders.tobe.domain.order.EatInOrderLineItem;
 import kitchenpos.eatinorders.tobe.domain.order.EatInOrderLineItems;
+import kitchenpos.eatinorders.tobe.domain.order.menu.DefaultEatInOrderMenu;
+import kitchenpos.eatinorders.tobe.domain.order.menu.DefaultEatInOrderMenus;
+import kitchenpos.eatinorders.tobe.domain.order.menu.EatInOrderMenus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.List;
@@ -17,54 +19,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("주문 항목 목록 테스트")
 class EatInOrderLineItemsTest {
 
-    @DisplayName("메뉴의 가격과 주문 항목 메뉴의 가격이 일치하면 true 를 반환한다.")
-    @CsvSource(value = {"1000:1000", "10_000:10_000", "16_000:16_000"}, delimiter = ':')
-    @ParameterizedTest(name = "{index}. 메뉴 가격: {0}, 주문 항목 메뉴 가격: {1}")
-    void isSamePrice(final int menuPrice, final int expectedPrice) {
-        final UUID menuId = UUID.randomUUID();
-        final EatInOrderLineItem eatInOrderLineItem = new EatInOrderLineItem(menuId, "후라이드치킨", menuPrice, 1);
-        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(List.of(eatInOrderLineItem));
-
-        assertThat(eatInOrderLineItems.isSamePrice(menuId, expectedPrice)).isTrue();
-    }
-
-    @DisplayName("메뉴의 가격과 주문 항목 메뉴의 가격이 불일치하면 false 를 반환한다.")
-    @CsvSource(value = {"1000:1001", "10_000:9_999", "0:1"}, delimiter = ':')
-    @ParameterizedTest(name = "{index}. 메뉴 가격: {0}, 주문 항목 메뉴 가격: {1}")
-    void isNotSamePrice(final int menuPrice, final int expectedPrice) {
-        final UUID menuId = UUID.randomUUID();
-        final EatInOrderLineItem eatInOrderLineItem = new EatInOrderLineItem(menuId, "후라이드치킨", menuPrice, 1);
-        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(List.of(eatInOrderLineItem));
-
-        assertThat(eatInOrderLineItems.isSamePrice(menuId, expectedPrice)).isFalse();
-    }
-
-    @DisplayName("메뉴의 가격을 비교할 주문 항목의 메뉴가 없으면 예외를 발생한다.")
-    @CsvSource(value = {"1000", "10_000", "16_000"}, delimiter = ':')
-    @ParameterizedTest(name = "메뉴 가격: {0}")
-    void isSamePriceWithEmptyMenu(final int menuPrice) {
-        final UUID menuId = UUID.randomUUID();
-        final EatInOrderLineItem eatInOrderLineItem = new EatInOrderLineItem(menuId, "후라이드치킨", menuPrice, 1);
-        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(List.of(eatInOrderLineItem));
-
-        assertThatThrownBy(() -> eatInOrderLineItems.isSamePrice(UUID.randomUUID(), menuPrice))
-                .isExactlyInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("메뉴의 식별자 목록을 반환한다.")
-    @Test
-    void menuIds() {
-        final UUID firstMenuId = UUID.randomUUID();
-        final EatInOrderLineItem firstEatInOrderLineItem = new EatInOrderLineItem(firstMenuId, "후라이드치킨", 16_000, 1);
-        final UUID secondMenuId = UUID.randomUUID();
-        final EatInOrderLineItem secondEatInOrderLineItem = new EatInOrderLineItem(secondMenuId, "후라이드치킨", 16_000, 1);
-
-        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(List.of(firstEatInOrderLineItem, secondEatInOrderLineItem));
-        final List<UUID> actual = eatInOrderLineItems.menuIds();
-
-        assertThat(actual).containsExactlyInAnyOrder(firstMenuId, secondMenuId);
-    }
-
     @DisplayName("주문 항목의 개수를 반환한다.")
     @Test
     void size() {
@@ -74,7 +28,7 @@ class EatInOrderLineItemsTest {
         final UUID secondMenuId = UUID.randomUUID();
         final EatInOrderLineItem secondEatInOrderLineItem = new EatInOrderLineItem(secondMenuId, "후라이드치킨", 16_000, 1);
 
-        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(List.of(firstEatInOrderLineItem, secondEatInOrderLineItem));
+        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(firstEatInOrderLineItem, secondEatInOrderLineItem);
         final int actual = eatInOrderLineItems.size();
 
         assertThat(actual).isEqualTo(2);
@@ -85,6 +39,41 @@ class EatInOrderLineItemsTest {
     @NullAndEmptySource
     void createWithoutEatInOrderLineItems(final List<EatInOrderLineItem> eatInOrderLineItemList) {
         assertThatThrownBy(() -> new EatInOrderLineItems(eatInOrderLineItemList))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 항목 메뉴와 메뉴의 개수가 일치하지 않으면 매장 주문을 생성할 수 없다.")
+    @Test
+    void createWithDifferentMenuSize() {
+        final UUID firstMenuId = UUID.randomUUID();
+        final EatInOrderLineItem firstEatInOrderLineItem = new EatInOrderLineItem(firstMenuId, "후라이드치킨", 16_000, 1);
+
+        final UUID secondMenuId = UUID.randomUUID();
+        final EatInOrderLineItem secondEatInOrderLineItem = new EatInOrderLineItem(secondMenuId, "양념치킨", 16_000, 1);
+        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(firstEatInOrderLineItem, secondEatInOrderLineItem);
+
+        final EatInOrderMenus eatInOrderMenus = new DefaultEatInOrderMenus(new DefaultEatInOrderMenu(firstMenuId, 16_001, true));
+
+        assertThatThrownBy(() -> eatInOrderLineItems.ensureIntegrity(eatInOrderMenus))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 항목 메뉴와 메뉴의 가격이 일치하지 않으면 매장 주문을 생성할 수 없다.")
+    @Test
+    void createWithDifferentMenuPrice() {
+        final UUID firstMenuId = UUID.randomUUID();
+        final EatInOrderLineItem firstEatInOrderLineItem = new EatInOrderLineItem(firstMenuId, "후라이드치킨", 16_000, 1);
+
+        final UUID secondMenuId = UUID.randomUUID();
+        final EatInOrderLineItem secondEatInOrderLineItem = new EatInOrderLineItem(secondMenuId, "양념치킨", 16_000, 1);
+        final EatInOrderLineItems eatInOrderLineItems = new EatInOrderLineItems(firstEatInOrderLineItem, secondEatInOrderLineItem);
+
+        final EatInOrderMenus eatInOrderMenus = new DefaultEatInOrderMenus(
+                new DefaultEatInOrderMenu(firstMenuId, 16_000, true),
+                new DefaultEatInOrderMenu(secondMenuId, 16_001, true)
+        );
+
+        assertThatThrownBy(() -> eatInOrderLineItems.ensureIntegrity(eatInOrderMenus))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
