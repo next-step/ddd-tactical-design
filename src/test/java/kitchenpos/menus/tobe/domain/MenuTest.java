5c27fcdef3148ace9613bceb849b804740cffe51
devifancy <fancy.junyongmoon@gmail.com>
2025-03-02 20:52:07 +0900

    docs(Menu): 메뉴에 대한 요구사항 충족 및 README에 반영

--- src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
@@ -7,7 +7,6 @@ import kitchenpos.menus.tobe.domain.vo.MenuProducts;
 import kitchenpos.products.tobe.domain.Product;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
