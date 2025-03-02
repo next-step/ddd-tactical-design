a7a85138084f18d010524b28a09be3c8a1fc0a57
devifancy <fancy.junyongmoon@gmail.com>
2025-03-02 19:36:31 +0900

    test(Menu): 메뉴 가격은 포함된 상품들의 총 가격(각 상품 가격 × 수량)의 합보다 클 수 없다.

--- src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
+++ src/test/java/kitchenpos/menus/tobe/domain/MenuTest.java
@@ -19,3 +19,24 @@ class MenuTest {
                 .hasMessage("메뉴는 반드시 특정 메뉴 그룹에 속해야 합니다.");
     }
 
+    @Test
+    void 메뉴_가격은_포함된_상품들의_총_가격의_합보다_클_수_없다() {
+        // given
+        Product product1 = new Product("후라이드 치킨", valueOf(20_000));
+        Product product2 = new Product("양념 치킨", valueOf(22_000));
+
+        MenuGroup menuGroup = new MenuGroup("메인 메뉴");
+        Menu menu = new Menu(menuGroup, "치킨 세트", 50_000, true);
+
+        MenuProduct mp1 = new MenuProduct(menu, product1, 20_000, 1, product1.getId());
+        MenuProduct mp2 = new MenuProduct(menu, product2, 22_000, 1, product2.getId());
+        MenuProducts menuProducts = new MenuProducts(mp1, mp2);
+
+        // when & then
+        // 상품 총액은 20,000 + 22,000 = 42,000원, 메뉴 가격 50000원이므로 예외 발생해야 함.
+        assertThatThrownBy(() -> new Menu(menuGroup, "치킨 세트", 50_000, true, menuProducts))
+                .isInstanceOf(InvalidMenuPriceException.class)
+                .hasMessage("메뉴 가격은 포함된 상품들의 총 가격보다 클 수 없습니다.");
+    }
+
+}
