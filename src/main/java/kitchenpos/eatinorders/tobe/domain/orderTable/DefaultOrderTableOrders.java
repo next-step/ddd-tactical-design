ad0f88d50b04c0e626a5dbb844ec64e5ca5806b1
devfancy <fancy.junyongmoon@gmail.com>
2025-03-29 23:25:54 +0900

    chore: DefaultOrderTableOrders 클래스를 생성한 이유에 대해 주석 추가

--- src/main/java/kitchenpos/eatinorders/tobe/domain/orderTable/DefaultOrderTableOrders.java
+++ src/main/java/kitchenpos/eatinorders/tobe/domain/orderTable/DefaultOrderTableOrders.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+/**
+ * 주문 테이블의 도메인 객체(EatInOrderTable) 내부에서
+ * 완료되지 않은 주문이 존재하는지 여부를 판단하기 위한 도메인 서비스 구현체입니다.
+ *
+ * 응용 서비스가 아닌 도메인 계층에서 유효성 검증 책임을 갖기 위해
+ * OrderTableOrders 인터페이스로 추상화하고, 해당 구현을 여기서 수행합니다.
+ */
 @Component
 public class DefaultOrderTableOrders implements OrderTableOrders {
 
